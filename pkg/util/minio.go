2ea2adf656677a56eafee137a5b87f98fb50b666
gitlawr <lawrleegle@gmail.com>
2020-12-16 22:06:04 +0800

    init minio bucket if needed

--- pkg/util/minio.go
+++ pkg/util/minio.go
@@ -3,7 +3,6 @@ package util
 import (
 	"net/url"
 	"strings"
-	"sync"
 
 	"github.com/minio/minio-go/v6"
 	"github.com/rancher/harvester/pkg/config"
@@ -14,10 +13,6 @@ const (
 	BucketName           = "vm-images"
 	BucketLocation       = "us-east-1"
 	DownloadBucketPolicy = `{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"AWS":["*"]},"Action":["s3:GetBucketLocation","s3:ListBucket"],"Resource":["arn:aws:s3:::vm-images"]},{"Effect":"Allow","Principal":{"AWS":["*"]},"Action":["s3:GetObject"],"Resource":["arn:aws:s3:::vm-images/*"]}]}`
-)
-
-var (
-	once sync.Once
 )
 
 func NewMinioClient() (*minio.Client, error) {
@@ -36,13 +31,10 @@ func NewMinioClient() (*minio.Client, error) {
 	if err != nil {
 		return nil, err
 	}
-	once.Do(func() {
-		err = initBucket(client)
-	})
-	return client, err
+	return client, initBucketIfNotExist(client)
 }
 
-func initBucket(client *minio.Client) error {
+func initBucketIfNotExist(client *minio.Client) error {
 	exist, err := client.BucketExists(BucketName)
 	if err != nil {
 		return err
@@ -55,6 +47,13 @@ func initBucket(client *minio.Client) error {
 		}
 		logrus.Debugf("Successfully created bucket %s\n", BucketName)
 	}
+	policy, err := client.GetBucketPolicy(BucketName)
+	if err != nil {
+		return err
+	}
+	if !strings.Contains(policy, "Allow") {
+		return client.SetBucketPolicy(BucketName, DownloadBucketPolicy)
+	}
 
-	return client.SetBucketPolicy(BucketName, DownloadBucketPolicy)
+	return nil
 }
