b880f9ebd7955e7f04f63ad15f258d8163c13cbc
DONOTSAYYES <wilson.huang.qc@gmail.com>
2021-01-04 16:23:29 +0800

    auth, users, and settings integration tests

--- tests/integration/api/setting_apis_test.go
+++ tests/integration/api/setting_apis_test.go
@@ -3,149 +3,63 @@ package api_test
 import (
 	"fmt"
 	"net/http"
-	"strings"
 
-	"github.com/guonaihong/gout"
 	. "github.com/onsi/ginkgo"
 	"github.com/tidwall/gjson"
-	"github.com/tidwall/sjson"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 
-	"github.com/rancher/harvester/pkg/config"
-	"github.com/rancher/harvester/pkg/settings"
+	harvesterv1alpha1 "github.com/rancher/harvester/pkg/apis/harvester.cattle.io/v1alpha1"
 	. "github.com/rancher/harvester/tests/framework/dsl"
-	"github.com/rancher/harvester/tests/framework/fuzz"
 	"github.com/rancher/harvester/tests/framework/helper"
 )
 
-var _ = Describe("verify settings apis", func() {
+var _ = Describe("verify settings APIs", func() {
 
-	Cleanup(func() {
+	Context("operate via steve API", func() {
 
-		scaled := config.ScaledWithContext(harvester.Context)
-		settingController := scaled.HarvesterFactory.Harvester().V1alpha1().Setting()
-		settingList, err := settingController.List(metav1.ListOptions{LabelSelector: labels.FormatLabels(testResourceLabels)})
-		if err != nil {
-			GinkgoT().Logf("failed to list tested settings, %v", err)
-			return
-		}
-		for _, item := range settingList.Items {
-			if err := settingController.Delete(item.Name, &metav1.DeleteOptions{}); err != nil {
-				GinkgoT().Logf("failed to delete tested setting %s/%s, %v", item.Namespace, item.Name, err)
-			}
-		}
-
-	})
-
-	Context("operate via steve api", func() {
-
-		var settingsAPI string
+		var (
+			settingsAPI, resourceVersion, APIUIVersionVal string
+		)
 
 		BeforeEach(func() {
-
 			settingsAPI = helper.BuildAPIURL("v1", "harvester.cattle.io.settings")
-
 		})
 
-		It("should get the value from preset ENV", func() {
+		Specify("view all the settings", func() {
 
-			// NB(thxCode): in order to test the preset ENV setting,
-			// we need to inject an environment variable in the BeforeSuite.
-
-			By("given a preset setting: api-ui-version")
-			var setting = settings.APIUIVersion
-			var (
-				respCode int
-				respBody []byte
-			)
-			var err = helper.NewHTTPClient().
-				GET(fmt.Sprintf("%s/%s", settingsAPI, setting.Name)).
-				BindBody(&respBody).
-				Code(&respCode).
-				Do()
-			MustRespCodeIs(http.StatusOK, "get setting", err, respCode, respBody)
-
-			By("when modify the api-ui-version setting")
-			var updatedSettingValue = "v0.0.0"
-			respBodyUpdated, err := sjson.SetBytes(respBody, "value", updatedSettingValue)
-			MustNotError(err)
-			err = helper.NewHTTPClient().
-				PUT(fmt.Sprintf("%s/%s", settingsAPI, setting.Name)).
-				SetBody(respBodyUpdated).
-				BindBody(&respBody).
-				Code(&respCode).
-				Do()
-			MustRespCodeIs(http.StatusOK, "put setting", err, respCode, respBody)
-
-			By("then we still can get the value from preset ENV")
-			MustChanged(setting.Get(), gjson.GetBytes(respBody, "default").String(), updatedSettingValue)
+			By("check all the settings")
+			respCode, respBody, err := helper.GetObject(settingsAPI, nil)
+			MustRespCodeIs(http.StatusOK, "retrieve all the settings", err, respCode, respBody)
 		})
 
-		Specify("create a custom setting", func() {
+		Specify("edit api-ui-version", func() {
 
-			By("given a random setting")
-			var settingName = strings.ToLower(fuzz.String(5))
-			var settingValue = fuzz.String(5)
-			var setting = gout.H{
-				"metadata": map[string]interface{}{
-					"name": settingName,
-					"labels": map[string]string{
-						"test.harvester.cattle.io": "for-test",
-					},
+			By("check on api-ui-version")
+			versionAPI := fmt.Sprintf("%s/%s", settingsAPI, "api-ui-version")
+			respCode, respBody, err := helper.GetObject(versionAPI, nil)
+			MustRespCodeIs(http.StatusOK, "view api-ui-version info", err, respCode, respBody)
+
+			By("update api-ui-version's value")
+			resourceVersion = gjson.GetBytes(respBody, "metadata.resourceVersion").Str
+			APIUIVersion := harvesterv1alpha1.Setting{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Setting",
+					APIVersion: "harvester.cattle.io/v1alpha1",
 				},
-				"value": settingValue,
+				ObjectMeta: metav1.ObjectMeta{
+					Name:            "api-ui-version",
+					ResourceVersion: resourceVersion,
+				},
+				Value: "v0.0.0",
 			}
+			respCode, respBody, err = helper.PutObject(versionAPI, APIUIVersion)
+			MustRespCodeIs(http.StatusOK, "update the api-ui-version", err, respCode, respBody)
 
-			By("when create the setting")
-			var (
-				respCode int
-				respBody []byte
-			)
-			var err = helper.NewHTTPClient().
-				POST(settingsAPI).
-				SetJSON(setting).
-				BindBody(&respBody).
-				Code(&respCode).
-				Do()
-			MustRespCodeIs(http.StatusCreated, "post setting", err, respCode, respBody)
-
-			By("then succeeded")
-			MustFinallyBeTrue(func() bool {
-				var (
-					respCode int
-					respBody []byte
-				)
-				var err = helper.NewHTTPClient().
-					GET(fmt.Sprintf("%s/%s", settingsAPI, settingName)).
-					BindBody(&respBody).
-					Code(&respCode).
-					Do()
-				return CheckRespCodeIs(http.StatusOK, "get setting", err, respCode, respBody) &&
-					gjson.GetBytes(respBody, "value").String() == settingValue
-			})
-
-			By("when delete the setting")
-			err = helper.NewHTTPClient().
-				DELETE(fmt.Sprintf("%s/%s", settingsAPI, settingName)).
-				BindBody(&respBody).
-				Code(&respCode).
-				Do()
-			CheckRespCodeIs(http.StatusOK, "delete setting", err, respCode, respBody)
-
-			By("then succeeded")
-			MustFinallyBeTrue(func() bool {
-				var (
-					respCode int
-				)
-				err := helper.NewHTTPClient().
-					GET(fmt.Sprintf("%s/%s", settingsAPI, settingName)).
-					Code(&respCode).
-					Do()
-				return CheckRespCodeIs(http.StatusNotFound, "get setting", err, respCode, respBody)
-			})
+			By("check if the api-ui-version has been updated")
+			APIUIVersionVal = gjson.GetBytes(respBody, "value").Str
+			respCode, respBody, err = helper.GetObject(versionAPI, nil)
+			MustRespCodeIs(http.StatusOK, "get the updated api-ui-version", err, respCode, respBody)
+			MustEqual(APIUIVersionVal, "v0.0.0")
 		})
-
 	})
-
 })
---
+++ tests/integration/api/user_apis_test.go
@@ -0,0 +1,214 @@
+package api_test
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	ctlcorev1 "github.com/rancher/wrangler-api/pkg/generated/controllers/core/v1"
+	"github.com/tidwall/gjson"
+	"golang.org/x/crypto/bcrypt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/clientcmd"
+
+	harvesterv1alpha1 "github.com/rancher/harvester/pkg/apis/harvester.cattle.io/v1alpha1"
+	"github.com/rancher/harvester/pkg/config"
+	. "github.com/rancher/harvester/tests/framework/dsl"
+	"github.com/rancher/harvester/tests/framework/fuzz"
+	"github.com/rancher/harvester/tests/framework/helper"
+)
+
+const (
+	namespace = "harvester-system"
+)
+
+var _ = Describe("verify users APIs", func() {
+
+	var (
+		scaled                   *config.Scaled
+		secretController         ctlcorev1.SecretController
+		serviceAccountController ctlcorev1.ServiceAccountController
+	)
+
+	BeforeEach(func() {
+		scaled = harvester.Scaled()
+		secretController = scaled.CoreFactory.Core().V1().Secret()
+		serviceAccountController = scaled.CoreFactory.Core().V1().ServiceAccount()
+	})
+
+	Context("operate via steve API", func() {
+
+		var (
+			userID, userName, password, updatedPassword,
+			usersKubeAPI, usersAPI, loginAPI, resourceVersion string
+		)
+
+		userName = strings.ToLower(fuzz.String(5))
+		password = fuzz.String(6)
+
+		BeforeEach(func() {
+			usersAPI = helper.BuildAPIURL("v1", "harvester.cattle.io.users")
+			usersKubeAPI = helper.BuildAPIURL("", "apis/harvester.cattle.io/v1alpha1/users")
+			loginAPI = helper.BuildAPIURL("", "v1-public/auth?action=login")
+		})
+
+		Specify("create a user", func() {
+
+			By("given a username & password")
+			user := harvesterv1alpha1.User{
+				Username: userName,
+				Password: password,
+				IsAdmin:  true,
+			}
+
+			By("when calling harvester.cattle.io.users")
+			respCode, respBody, err := helper.PostObject(usersAPI, user)
+			MustRespCodeIs(http.StatusCreated, "create user", err, respCode, respBody)
+			userID = gjson.GetBytes(respBody, "metadata.name").Str
+			resourceVersion = gjson.GetBytes(respBody, "metadata.resourceVersion").Str
+
+		})
+
+		Specify("list users to check if the user is created or not", func() {
+
+			By("when calling harvester.cattle.io.users to get user list")
+			respCode, respBody, err := helper.GetObject(usersAPI, nil)
+			MustRespCodeIs(http.StatusOK, "get user list", err, respCode, respBody)
+
+			By("get user json")
+			selectUserJSONPath := fmt.Sprintf("data.#(username==\"%s\")", userName)
+			userJSON := gjson.GetBytes(respBody, selectUserJSONPath)
+			MustEqual(userJSON.Exists(), true)
+
+		})
+
+		Specify("login in by using local user", func() {
+
+			By("login in with the created user and invalid password")
+			login := harvesterv1alpha1.Login{
+				Username: userName,
+				Password: fuzz.String(8),
+			}
+			respCode, respBody, err := helper.PostObject(loginAPI, login)
+			MustRespCodeIs(http.StatusUnauthorized, "password incorrect", err, respCode, respBody)
+
+			By("login in with invalid username and password")
+			login = harvesterv1alpha1.Login{
+				Username: fuzz.String(6),
+				Password: fuzz.String(10),
+			}
+			respCode, respBody, err = helper.PostObject(loginAPI, login)
+			MustRespCodeIs(http.StatusUnauthorized, "user name or password incorrect", err, respCode, respBody)
+
+			By("login in with the correct username and password")
+			login = harvesterv1alpha1.Login{
+				Username: userName,
+				Password: password,
+			}
+			respCode, respBody, err = helper.PostObject(loginAPI, login)
+			MustRespCodeIs(http.StatusOK, "sign in correctly", err, respCode, respBody)
+
+		})
+
+		Specify("update a user's password", func() {
+
+			By("update the user's password")
+			updateAPI := fmt.Sprintf("%s/%s", usersAPI, userID)
+			updatedPassword = fuzz.String(8)
+			user := harvesterv1alpha1.User{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "User",
+					APIVersion: "harvester.cattle.io/v1alpha1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					ResourceVersion: resourceVersion,
+					Name:            userID,
+				},
+				Username: userName,
+				Password: updatedPassword,
+				IsAdmin:  true,
+			}
+			respCode, respBody, err := helper.PutObject(updateAPI, user)
+			MustRespCodeIs(http.StatusOK, "update user's password", err, respCode, respBody)
+
+			By("get the updated user info")
+			respCode, respBody, err = helper.GetObject(fmt.Sprintf("%s/%s", usersKubeAPI, userID), nil)
+			MustRespCodeIs(http.StatusOK, "get updated user info", err, respCode, respBody)
+
+			By("get password")
+			passwordJSON := gjson.GetBytes(respBody, "password")
+			MustEqual(passwordJSON.Exists(), true)
+
+			By("compare password")
+			err = bcrypt.CompareHashAndPassword([]byte(passwordJSON.Str), []byte(updatedPassword))
+			MustNotError(err)
+
+		})
+
+		Specify("delete a user", func() {
+
+			By("delete a user via user id")
+			respCode, respBody, err := helper.DeleteObject(fmt.Sprintf("%s/%s", usersAPI, userID))
+			MustRespCodeIs(http.StatusNoContent, "delete a user successfully", err, respCode, respBody)
+
+			By("the deleted user cannot be retrieved anymore")
+			respCode, respBody, err = helper.GetObject(fmt.Sprintf("%s/%s", usersAPI, userID), nil)
+			MustRespCodeIs(http.StatusNotFound, "retrieve",
+				err, respCode, respBody)
+
+		})
+
+		Specify("login in by using serviceAccount name", func() {
+
+			By("get the service account name")
+			sa, err := serviceAccountController.Get(namespace, "harvester", metav1.GetOptions{})
+			MustNotError(err)
+			MustEqual(len(sa.Secrets), 1)
+
+			By("get token")
+			secretName := sa.Secrets[0].Name
+			secret, err := secretController.Get(namespace, secretName, metav1.GetOptions{})
+			MustNotError(err)
+
+			By("login in with token")
+			loginToken := harvesterv1alpha1.Login{
+				Token: string(secret.Data["token"]),
+			}
+			respCode, respBody, err := helper.PostObject(loginAPI, loginToken)
+			MustRespCodeIs(http.StatusOK, "login", err, respCode, respBody)
+
+			By("use invalid service account secret to login")
+			loginInvalidToken := harvesterv1alpha1.Login{
+				Token: fuzz.String(16),
+			}
+			respCode, respBody, err = helper.PostObject(loginAPI, loginInvalidToken)
+			MustRespCodeIs(http.StatusUnauthorized, "invalid token login", err, respCode, respBody)
+		})
+
+		Specify("login in by using kubeconfig", func() {
+
+			By("get valid kubeConfig")
+			rawConfig, err := KubeClientConfig.RawConfig()
+			MustNotError(err)
+			validKubeConfig, err := clientcmd.Write(rawConfig)
+			MustNotError(err)
+
+			By("login with valid kubeConfig")
+			loginValidConfig := harvesterv1alpha1.Login{
+				KubeConfig: string(validKubeConfig),
+			}
+			respCode, respBody, err := helper.PostObject(loginAPI, loginValidConfig)
+			MustRespCodeIs(http.StatusOK, "login with kubeConfig", err, respCode, respBody)
+
+			By("login with invalid kubeConfig")
+			loginInvalidConfig := harvesterv1alpha1.Login{
+				KubeConfig: fuzz.String(20),
+			}
+			respCode, respBody, err = helper.PostObject(loginAPI, loginInvalidConfig)
+			MustRespCodeIs(http.StatusUnauthorized, "login with invalid kubeConfig", err, respCode, respBody)
+		})
+
+	})
+
+})
