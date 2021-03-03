665c61687a6403e42bd70560c3132a77926a623a
Guangbo Chen <steven.guangbo.chen@gmail.com>
2021-03-03 13:33:44 +0800

    Update generated files

--- pkg/generated/clientset/versioned/fake/register.go
+++ pkg/generated/clientset/versioned/fake/register.go
@@ -32,7 +32,7 @@ import (
 
 var scheme = runtime.NewScheme()
 var codecs = serializer.NewCodecFactory(scheme)
-var parameterCodec = runtime.NewParameterCodec(scheme)
+
 var localSchemeBuilder = runtime.SchemeBuilder{
 	cdiv1beta1.AddToScheme,
 	harvesterv1alpha1.AddToScheme,
