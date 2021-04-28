d026b5bec3ead75ac4aa94a9c29afcc2e618c229
gitlawr <lawrleegle@gmail.com>
2021-04-28 13:13:59 +0800

    Check upgrade from upgrade responder

--- pkg/controller/master/upgrade/version_syncer_test.go
+++ pkg/controller/master/upgrade/version_syncer_test.go
@@ -8,8 +8,8 @@ import (
 
 func TestGetUpgradableVersions(t *testing.T) {
 	type input struct {
-		versionMetaData string
-		currentVersion  string
+		config         CheckUpgradeResponse
+		currentVersion string
 	}
 	type output struct {
 		upgradableVersions string
@@ -24,17 +24,22 @@ func TestGetUpgradableVersions(t *testing.T) {
 			name: "test1",
 			given: input{
 				currentVersion: "v0.1.0",
-				versionMetaData: `
-[
-    {
-        "version": "v0.3.0",
-        "minUpgradableVersion": "v0.2.0"
-    },
-    {
-        "version": "v0.2.0",
-        "minUpgradableVersion": "v0.1.0"
-    }
-]`,
+				config: CheckUpgradeResponse{
+					Versions: []Version{
+						{
+							Name:                 "v0.3.0",
+							ReleaseDate:          "2021-01-01T00:00:00Z",
+							MinUpgradableVersion: "v0.2.0",
+							Tags:                 []string{"latest"},
+						},
+						{
+							Name:                 "v0.2.0",
+							ReleaseDate:          "2020-01-01T00:00:00Z",
+							MinUpgradableVersion: "v0.1.0",
+							Tags:                 []string{"v0.2-latest"},
+						},
+					},
+				},
 			},
 			expected: output{
 				upgradableVersions: "v0.2.0",
@@ -45,24 +50,69 @@ func TestGetUpgradableVersions(t *testing.T) {
 			name: "test2",
 			given: input{
 				currentVersion: "dev",
-				versionMetaData: `
-[
-  {
-	  "version": "v0.1.0",
-	  "minUpgradableVersion": "v0.1.0-rc1"
-  },
-  {
-	  "version": "v0.2.0",
-	  "minUpgradableVersion": "dev"
-  },
-  {
-	  "version": "v0.2.1",
-	  "minUpgradableVersion": "dev"
-  }
-]`,
+				config: CheckUpgradeResponse{
+					Versions: []Version{
+						{
+							Name:                 "v0.1.0",
+							ReleaseDate:          "2021-01-01T00:00:00Z",
+							MinUpgradableVersion: "v0.1.0-rc1",
+							Tags:                 []string{"v0.1.0"},
+						},
+						{
+							Name:                 "v0.2.0",
+							ReleaseDate:          "2020-01-01T00:00:00Z",
+							MinUpgradableVersion: "dev",
+							Tags:                 []string{"v0.2.0"},
+						},
+						{
+							Name:                 "v0.2.1",
+							ReleaseDate:          "2020-06-01T00:00:00Z",
+							MinUpgradableVersion: "dev",
+							Tags:                 []string{"v0.2.1", "latest"},
+						},
+					},
+				},
 			},
 			expected: output{
 				upgradableVersions: "v0.2.0,v0.2.1",
+				err:                nil,
+			},
+		},
+		{
+			name: "test3",
+			given: input{
+				currentVersion: "v0.3.0",
+				config: CheckUpgradeResponse{
+					Versions: []Version{
+						{
+							Name:        "v0.4.0",
+							ReleaseDate: "2021-01-01T00:00:00Z",
+							Tags:        []string{"latest"},
+						},
+					},
+				},
+			},
+			expected: output{
+				upgradableVersions: "v0.4.0",
+				err:                nil,
+			},
+		},
+		{
+			name: "test4",
+			given: input{
+				currentVersion: "v0.2.0",
+				config: CheckUpgradeResponse{
+					Versions: []Version{
+						{
+							Name:        "v0.2.0",
+							ReleaseDate: "2021-01-01T00:00:00Z",
+							Tags:        []string{"latest"},
+						},
+					},
+				},
+			},
+			expected: output{
+				upgradableVersions: "",
 				err:                nil,
 			},
 		},
@@ -70,7 +120,7 @@ func TestGetUpgradableVersions(t *testing.T) {
 
 	for _, tc := range testCases {
 		var actual output
-		actual.upgradableVersions, actual.err = getUpgradableVersions([]byte(tc.given.versionMetaData), tc.given.currentVersion)
+		actual.upgradableVersions, actual.err = getUpgradableVersions(tc.given.config, tc.given.currentVersion)
 		assert.Equal(t, tc.expected, actual, "case %q", tc.name)
 	}
 }
