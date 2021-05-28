70af9116a6d34a8844b23198a990eb0f9b3fa762
Kiefer Chang <kiefer.chang@suse.com>
2021-05-28 12:01:02 +0800

    Add virtualmachinetemplateversions.harvesterhci.io mutator
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

--- pkg/api/vmtemplate/schema.go
+++ pkg/api/vmtemplate/schema.go
@@ -4,7 +4,6 @@ import (
 	"github.com/rancher/apiserver/pkg/types"
 	"github.com/rancher/steve/pkg/schema"
 	"github.com/rancher/steve/pkg/server"
-	"github.com/rancher/steve/pkg/stores/proxy"
 
 	"github.com/harvester/harvester/pkg/config"
 )
@@ -15,17 +14,9 @@ const (
 )
 
 func RegisterSchema(scaled *config.Scaled, server *server.Server, options config.Options) error {
-	templates := scaled.HarvesterFactory.Harvesterhci().V1beta1().VirtualMachineTemplate()
 	templateVersionCache := scaled.HarvesterFactory.Harvesterhci().V1beta1().VirtualMachineTemplateVersion().Cache()
 	th := &templateLinkHandler{
 		templateVersionCache: templateVersionCache,
-	}
-
-	templateVersionStore := &templateVersionStore{
-		Store:                proxy.NewProxyStore(server.ClientFactory, nil, server.AccessSetLookup),
-		templateCache:        templates.Cache(),
-		templateVersionCache: templateVersionCache,
-		keyPairCache:         scaled.HarvesterFactory.Harvesterhci().V1beta1().KeyPair().Cache(),
 	}
 
 	t := []schema.Template{
@@ -35,12 +26,10 @@ func RegisterSchema(scaled *config.Scaled, server *server.Server, options config
 			Customize: func(apiSchema *types.APISchema) {
 				apiSchema.ByIDHandler = th.byIDHandler
 			},
-			Store: proxy.NewProxyStore(server.ClientFactory, nil, server.AccessSetLookup),
 		},
 		{
 			ID:        templateVersionSchemaID,
 			Formatter: versionFormatter,
-			Store:     templateVersionStore,
 		},
 	}
 
