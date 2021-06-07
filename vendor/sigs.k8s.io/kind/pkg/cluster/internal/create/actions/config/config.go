90d99bcea64d84262065696b6119e7cd0ca853ce
Kiefer Chang <kiefer.chang@suse.com>
2021-06-07 11:47:33 +0800

    Bump kind to 0.11.1
    Skip setting `net.netfilter.nf_conntrack_max` sysctl parameter.
    Setting this parameter causes Kind cluster initialization failure
    on newer kernels.
    
    Please see https://github.com/kubernetes-sigs/kind/pull/2241 for more
    information.
    
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

--- vendor/sigs.k8s.io/kind/pkg/cluster/internal/create/actions/config/config.go
+++ vendor/sigs.k8s.io/kind/pkg/cluster/internal/create/actions/config/config.go
@@ -20,6 +20,7 @@ package config
 import (
 	"bytes"
 	"fmt"
+	"net"
 	"strings"
 
 	"sigs.k8s.io/kind/pkg/cluster/constants"
@@ -47,6 +48,11 @@ func (a *Action) Execute(ctx *actions.ActionContext) error {
 	ctx.Status.Start("Writing configuration 📜")
 	defer ctx.Status.End(false)
 
+	providerInfo, err := ctx.Provider.Info()
+	if err != nil {
+		return err
+	}
+
 	allNodes, err := ctx.Nodes()
 	if err != nil {
 		return err
@@ -60,8 +66,9 @@ func (a *Action) Execute(ctx *actions.ActionContext) error {
 	// create kubeadm init config
 	fns := []func() error{}
 
+	provider := fmt.Sprintf("%s", ctx.Provider)
 	configData := kubeadm.ConfigData{
-		NodeProvider:         fmt.Sprintf("%s", ctx.Provider),
+		NodeProvider:         provider,
 		ClusterName:          ctx.Config.Name,
 		ControlPlaneEndpoint: controlPlaneEndpoint,
 		APIBindPort:          common.APIServerInternalPort,
@@ -71,15 +78,16 @@ func (a *Action) Execute(ctx *actions.ActionContext) error {
 		KubeProxyMode:        string(ctx.Config.Networking.KubeProxyMode),
 		ServiceSubnet:        ctx.Config.Networking.ServiceSubnet,
 		ControlPlane:         true,
-		IPv6:                 ctx.Config.Networking.IPFamily == "ipv6",
+		IPFamily:             ctx.Config.Networking.IPFamily,
 		FeatureGates:         ctx.Config.FeatureGates,
 		RuntimeConfig:        ctx.Config.RuntimeConfig,
+		RootlessProvider:     providerInfo.Rootless,
 	}
 
 	kubeadmConfigPlusPatches := func(node nodes.Node, data kubeadm.ConfigData) func() error {
 		return func() error {
 			data.NodeName = node.String()
-			kubeadmConfig, err := getKubeadmConfig(ctx.Config, data, node)
+			kubeadmConfig, err := getKubeadmConfig(ctx.Config, data, node, provider)
 			if err != nil {
 				// TODO(bentheelder): logging here
 				return errors.Wrap(err, "failed to generate kubeadm config content")
@@ -90,15 +98,42 @@ func (a *Action) Execute(ctx *actions.ActionContext) error {
 		}
 	}
 
+	// Populate the list of control-plane node labels and the list of worker node labels respectively.
+	// controlPlaneLabels is an array of maps (labels, read from config) associated with all the control-plane nodes.
+	// workerLabels is an array of maps (labels, read from config) associated with all the worker nodes.
+	controlPlaneLabels := []map[string]string{}
+	workerLabels := []map[string]string{}
+	for _, node := range ctx.Config.Nodes {
+		if node.Role == config.ControlPlaneRole {
+			controlPlaneLabels = append(controlPlaneLabels, node.Labels)
+		} else if node.Role == config.WorkerRole {
+			workerLabels = append(workerLabels, node.Labels)
+		} else {
+			continue
+		}
+	}
+
+	// hashMapLabelsToCommaSeparatedLabels converts labels in hashmap form to labels in a comma-separated string form like "key1=value1,key2=value2"
+	hashMapLabelsToCommaSeparatedLabels := func(labels map[string]string) string {
+		output := ""
+		for key, value := range labels {
+			output += fmt.Sprintf("%s=%s,", key, value)
+		}
+		return strings.TrimSuffix(output, ",") // remove the last character (comma) in the output string
+	}
+
 	// create the kubeadm join configuration for control plane nodes
 	controlPlanes, err := nodeutils.ControlPlaneNodes(allNodes)
 	if err != nil {
 		return err
 	}
 
-	for _, node := range controlPlanes {
+	for i, node := range controlPlanes {
 		node := node             // capture loop variable
 		configData := configData // copy config data
+		if len(controlPlaneLabels[i]) > 0 {
+			configData.NodeLabels = hashMapLabelsToCommaSeparatedLabels(controlPlaneLabels[i]) // updating the config with the respective labels to be written over the current control-plane node in consideration
+		}
 		fns = append(fns, kubeadmConfigPlusPatches(node, configData))
 	}
 
@@ -109,10 +144,13 @@ func (a *Action) Execute(ctx *actions.ActionContext) error {
 	}
 	if len(workers) > 0 {
 		// create the workers concurrently
-		for _, node := range workers {
+		for i, node := range workers {
 			node := node             // capture loop variable
 			configData := configData // copy config data
 			configData.ControlPlane = false
+			if len(workerLabels[i]) > 0 {
+				configData.NodeLabels = hashMapLabelsToCommaSeparatedLabels(workerLabels[i]) // updating the config with the respective labels to be written over the current worker node in consideration
+			}
 			fns = append(fns, kubeadmConfigPlusPatches(node, configData))
 		}
 	}
@@ -141,7 +179,7 @@ func (a *Action) Execute(ctx *actions.ActionContext) error {
 				}
 				patched, err := patch.TOML(buff.String(), ctx.Config.ContainerdConfigPatches, ctx.Config.ContainerdConfigPatchesJSON6902)
 				if err != nil {
-					return errors.Wrap(err, "failed to patch contianerd config")
+					return errors.Wrap(err, "failed to patch containerd config")
 				}
 				if err := nodeutils.WriteFile(node, containerdConfigPath, patched); err != nil {
 					return errors.Wrap(err, "failed to write patched containerd config")
@@ -166,7 +204,7 @@ func (a *Action) Execute(ctx *actions.ActionContext) error {
 
 // getKubeadmConfig generates the kubeadm config contents for the cluster
 // by running data through the template and applying patches as needed.
-func getKubeadmConfig(cfg *config.Cluster, data kubeadm.ConfigData, node nodes.Node) (path string, err error) {
+func getKubeadmConfig(cfg *config.Cluster, data kubeadm.ConfigData, node nodes.Node, provider string) (path string, err error) {
 	kubeVersion, err := nodeutils.KubeVersion(node)
 	if err != nil {
 		// TODO(bentheelder): logging here
@@ -199,11 +237,14 @@ func getKubeadmConfig(cfg *config.Cluster, data kubeadm.ConfigData, node nodes.N
 
 	data.NodeAddress = nodeAddress
 	// configure the right protocol addresses
-	if cfg.Networking.IPFamily == "ipv6" {
-		if nodeAddressIPv6 == "" {
-			return "", errors.Errorf("failed to get IPV6 address; is the docker daemon configured to use IPV6 correctly?")
+	if cfg.Networking.IPFamily == config.IPv6Family || cfg.Networking.IPFamily == config.DualStackFamily {
+		if ip := net.ParseIP(nodeAddressIPv6); ip.To16() == nil {
+			return "", errors.Errorf("failed to get IPv6 address for node %s; is %s configured to use IPv6 correctly?", node.String(), provider)
 		}
 		data.NodeAddress = nodeAddressIPv6
+		if cfg.Networking.IPFamily == config.DualStackFamily {
+			data.NodeAddress = fmt.Sprintf("%s,%s", nodeAddress, nodeAddressIPv6)
+		}
 	}
 
 	// generate the config contents
--- vendor/sigs.k8s.io/kind/pkg/internal/apis/config/types.go
+++ vendor/sigs.k8s.io/kind/pkg/internal/apis/config/types.go
@@ -85,6 +85,9 @@ type Node struct {
 	// If unset a default image will be used, see defaults.Image
 	Image string
 
+	// Labels are the labels with which the respective node will be labeled
+	Labels map[string]string
+
 	/* Advanced fields */
 
 	// ExtraMounts describes additional mount points for the node container
@@ -157,16 +160,20 @@ const (
 	IPv4Family ClusterIPFamily = "ipv4"
 	// IPv6Family sets ClusterIPFamily to ipv6
 	IPv6Family ClusterIPFamily = "ipv6"
+	// DualStackFamily sets ClusterIPFamily to dual
+	DualStackFamily ClusterIPFamily = "dual"
 )
 
 // ProxyMode defines a proxy mode for kube-proxy
 type ProxyMode string
 
 const (
-	// IPTablesMode sets ProxyMode to iptables
-	IPTablesMode ProxyMode = "iptables"
-	// IPVSMode sets ProxyMode to iptables
-	IPVSMode ProxyMode = "ipvs"
+	// IPTablesProxyMode sets ProxyMode to iptables
+	IPTablesProxyMode ProxyMode = "iptables"
+	// IPVSProxyMode sets ProxyMode to ipvs
+	IPVSProxyMode ProxyMode = "ipvs"
+	// NoneProxyMode disables kube-proxy
+	NoneProxyMode ProxyMode = "none"
 )
 
 // PatchJSON6902 represents an inline kustomize json 6902 patch
@@ -184,7 +191,7 @@ type PatchJSON6902 struct {
 // This is a close copy of the upstream cri Mount type
 // see: k8s.io/kubernetes/pkg/kubelet/apis/cri/runtime/v1alpha2
 // It additionally serializes the "propagation" field with the string enum
-// names on disk as opposed to the int32 values, and the serlialzed field names
+// names on disk as opposed to the int32 values, and the serialized field names
 // have been made closer to core/v1 VolumeMount field names
 // In yaml this looks like:
 //  containerPath: /foo
