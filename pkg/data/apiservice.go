bc40fc7914bc87091981ff9cf0143901cb87d8bd
gitlawr <lawrleegle@gmail.com>
2021-07-16 14:59:25 +0800

    Update apiservice path to avoid overwriting native steve routes

--- pkg/data/apiservice.go
+++ pkg/data/apiservice.go
@@ -22,7 +22,8 @@ func createAPIService(mgmtCtx *config.Management, namespace string) error {
 		Spec: v3.APIServiceSpec{
 			SecretName:      AggregationSecretName,
 			SecretNamespace: namespace,
-			PathPrefixes:    []string{"/v1/harvester"},
+			PathPrefixes:    []string{"/v1/harvester/"},
+			Paths:           []string{"/v1/harvester"},
 		},
 	}
 	if _, err := apiServices.Create(harvesterAggregationAPI); err != nil && !errors.IsAlreadyExists(err) {
