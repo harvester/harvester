1aff859ba3e5c68bf18af080cef4908444bc2841
gitlawr <lawrleegle@gmail.com>
2021-08-04 14:49:31 +0800

    Update VM builder

--- pkg/builder/disk.go
+++ pkg/builder/disk.go
@@ -1,14 +1,17 @@
 package builder
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/rand"
 	kubevirtv1 "kubevirt.io/client-go/api/v1"
-	cdiv1 "kubevirt.io/containerized-data-importer/pkg/apis/core/v1beta1"
+
+	"github.com/harvester/harvester/pkg/util"
 )
 
 const (
@@ -32,9 +35,8 @@ const (
 	DefaultImagePullPolicy = "IfNotPresent"
 )
 
-type DataVolumeOption struct {
+type PersistentVolumeClaimOption struct {
 	ImageID          string
-	DownloadURL      string
 	VolumeMode       corev1.PersistentVolumeMode
 	AccessMode       corev1.PersistentVolumeAccessMode
 	StorageClassName *string
@@ -118,19 +120,19 @@ func (v *VMBuilder) Volume(diskName string, volume kubevirtv1.Volume) *VMBuilder
 	return v
 }
 
-func (v *VMBuilder) ExistingDataVolume(diskName, dataVolumeName string) *VMBuilder {
+func (v *VMBuilder) ExistingPVCVolume(diskName, pvcName string) *VMBuilder {
 	return v.Volume(diskName, kubevirtv1.Volume{
 		Name: diskName,
 		VolumeSource: kubevirtv1.VolumeSource{
-			DataVolume: &kubevirtv1.DataVolumeSource{
-				Name: dataVolumeName,
+			PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+				ClaimName: pvcName,
 			},
 		},
 	})
 }
 
-func (v *VMBuilder) ExistingVolumeDisk(diskName, diskBus string, isCDRom bool, bootOrder int, dataVolumeName string) *VMBuilder {
-	return v.Disk(diskName, diskBus, isCDRom, bootOrder).ExistingDataVolume(diskName, dataVolumeName)
+func (v *VMBuilder) ExistingVolumeDisk(diskName, diskBus string, isCDRom bool, bootOrder int, pvcName string) *VMBuilder {
+	return v.Disk(diskName, diskBus, isCDRom, bootOrder).ExistingPVCVolume(diskName, pvcName)
 }
 
 func (v *VMBuilder) ContainerDiskVolume(diskName, imageName, ImagePullPolicy string) *VMBuilder {
@@ -149,60 +151,61 @@ func (v *VMBuilder) ContainerDisk(diskName, diskBus string, isCDRom bool, bootOr
 	return v.Disk(diskName, diskBus, isCDRom, bootOrder).ContainerDiskVolume(diskName, imageName, ImagePullPolicy)
 }
 
-func (v *VMBuilder) DataVolume(diskName, diskSize, dataVolumeName string, opt *DataVolumeOption) *VMBuilder {
+func (v *VMBuilder) PVCVolume(diskName, diskSize, pvcName string, opt *PersistentVolumeClaimOption) *VMBuilder {
 	if opt == nil {
-		opt = &DataVolumeOption{
-			VolumeMode: corev1.PersistentVolumeBlock,
-			AccessMode: corev1.ReadWriteMany,
+		defaultStorageClass := "longhorn"
+		opt = &PersistentVolumeClaimOption{
+			VolumeMode:       corev1.PersistentVolumeBlock,
+			AccessMode:       corev1.ReadWriteMany,
+			StorageClassName: &defaultStorageClass,
 		}
-	}
-	if dataVolumeName == "" {
-		dataVolumeName = fmt.Sprintf("%s-%s-%s", v.VirtualMachine.Name, diskName, rand.String(5))
-	}
-	// DataVolumeTemplates
-	dataVolumeTemplates := v.VirtualMachine.Spec.DataVolumeTemplates
-	dataVolumeSpecSource := cdiv1.DataVolumeSource{
-		Blank: &cdiv1.DataVolumeBlankImage{},
 	}
 
-	if opt.DownloadURL != "" {
-		dataVolumeSpecSource = cdiv1.DataVolumeSource{
-			HTTP: &cdiv1.DataVolumeSourceHTTP{
-				URL: opt.DownloadURL,
-			},
+	if pvcName == "" {
+		pvcName = fmt.Sprintf("%s-%s-%s", v.VirtualMachine.Name, diskName, rand.String(5))
+	}
+
+	var pvcs []*corev1.PersistentVolumeClaim
+	volumeClaimTemplates, ok := v.VirtualMachine.Annotations[util.AnnotationVolumeClaimTemplates]
+	if ok && volumeClaimTemplates != "" {
+		if err := json.Unmarshal([]byte(volumeClaimTemplates), &pvcs); err != nil {
+			logrus.Warnf("failed to unmarshal the volumeClaimTemplates annotation: %v", err)
 		}
 	}
-	dataVolumeTemplate := kubevirtv1.DataVolumeTemplateSpec{
+	pvc := &corev1.PersistentVolumeClaim{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: dataVolumeName,
+			Name:        pvcName,
+			Annotations: map[string]string{},
 		},
-		Spec: cdiv1.DataVolumeSpec{
-			Source: &dataVolumeSpecSource,
-			PVC: &corev1.PersistentVolumeClaimSpec{
-				AccessModes: []corev1.PersistentVolumeAccessMode{
-					opt.AccessMode,
-				},
-				Resources: corev1.ResourceRequirements{
-					Requests: corev1.ResourceList{
-						corev1.ResourceStorage: resource.MustParse(diskSize),
-					},
-				},
-				VolumeMode:       &opt.VolumeMode,
-				StorageClassName: opt.StorageClassName,
+		Spec: corev1.PersistentVolumeClaimSpec{
+			AccessModes: []corev1.PersistentVolumeAccessMode{
+				opt.AccessMode,
 			},
+			Resources: corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceStorage: resource.MustParse(diskSize),
+				},
+			},
+			VolumeMode:       &opt.VolumeMode,
+			StorageClassName: opt.StorageClassName,
 		},
 	}
 	if opt.ImageID != "" {
-		dataVolumeTemplate.Annotations = map[string]string{
-			AnnotationKeyImageID: opt.ImageID,
-		}
+		pvc.Annotations[AnnotationKeyImageID] = opt.ImageID
 	}
-	dataVolumeTemplates = append(dataVolumeTemplates, dataVolumeTemplate)
-	v.VirtualMachine.Spec.DataVolumeTemplates = dataVolumeTemplates
 
-	return v.ExistingDataVolume(diskName, dataVolumeName)
+	pvcs = append(pvcs, pvc)
+
+	toUpdateVolumeClaimTemplates, err := json.Marshal(pvcs)
+	if err != nil {
+		logrus.Warnf("failed to marshal the volumeClaimTemplates annotation: %v", err)
+	} else {
+		v.VirtualMachine.Annotations[util.AnnotationVolumeClaimTemplates] = string(toUpdateVolumeClaimTemplates)
+	}
+
+	return v.ExistingPVCVolume(diskName, pvcName)
 }
 
-func (v *VMBuilder) DataVolumeDisk(diskName, diskBus string, isCDRom bool, bootOrder int, diskSize, dataVolumeName string, opt *DataVolumeOption) *VMBuilder {
-	return v.Disk(diskName, diskBus, isCDRom, bootOrder).DataVolume(diskName, diskSize, dataVolumeName, opt)
+func (v *VMBuilder) PVCDisk(diskName, diskBus string, isCDRom bool, bootOrder int, diskSize, pvcName string, opt *PersistentVolumeClaimOption) *VMBuilder {
+	return v.Disk(diskName, diskBus, isCDRom, bootOrder).PVCVolume(diskName, diskSize, pvcName, opt)
 }
