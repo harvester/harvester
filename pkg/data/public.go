5b3da9af63d1cc534c03e8b49f2dd6c9890ada82
gitlawr <lawrleegle@gmail.com>
2021-09-08 11:18:26 +0800

    Add authed roles Make harvester settings readable to authed users Use apply to create built-in resources so that they get updated in furture changes

--- pkg/data/public.go
+++ pkg/data/public.go
@@ -1,49 +1,22 @@
 package data
 
 import (
+	"github.com/rancher/wrangler/pkg/apply"
 	v1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/harvester/harvester/pkg/config"
 )
 
 const (
 	publicNamespace = "harvester-public"
 )
 
-func createPublicNamespace(mgmtCtx *config.Management) error {
-	namespaces := mgmtCtx.CoreFactory.Core().V1().Namespace()
-	roleBindings := mgmtCtx.RbacFactory.Rbac().V1().RoleBinding()
-
-	// Create harvester-public namespace
-	if _, err := namespaces.Create(&v1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{Name: publicNamespace},
-	}); err != nil && !errors.IsAlreadyExists(err) {
-		return err
-	}
+func addPublicNamespace(apply apply.Apply) error {
 	// All authenticated users are readable in the public namespace
-	if _, err := roleBindings.Create(&rbacv1.RoleBinding{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "harvester-public",
-			Namespace: publicNamespace,
-		},
-		RoleRef: rbacv1.RoleRef{
-			APIGroup: rbacv1.GroupName,
-			Kind:     "ClusterRole",
-			Name:     "view",
-		},
-		Subjects: []rbacv1.Subject{
-			{
-				APIGroup: rbacv1.GroupName,
-				Kind:     rbacv1.GroupKind,
-				Name:     "system:authenticated",
-			},
-		},
-	}); err != nil && !errors.IsAlreadyExists(err) {
-		return err
-	}
-
-	return nil
+	return apply.
+		WithDynamicLookup().
+		WithSetID("harvester-public").
+		ApplyObjects(
+			&v1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{Name: publicNamespace},
+			})
 }
---
+++ pkg/data/rbac.go
@@ -0,0 +1,66 @@
+package data
+
+import (
+	"github.com/rancher/wrangler/pkg/apply"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func addAuthenticatedRoles(apply apply.Apply) error {
+	return apply.
+		WithDynamicLookup().
+		WithSetID("harvester-authenticated").
+		ApplyObjects(
+			&rbacv1.RoleBinding{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "harvester-public",
+					Namespace: publicNamespace,
+				},
+				RoleRef: rbacv1.RoleRef{
+					APIGroup: rbacv1.GroupName,
+					Kind:     "ClusterRole",
+					Name:     "view",
+				},
+				Subjects: []rbacv1.Subject{
+					{
+						APIGroup: rbacv1.GroupName,
+						Kind:     rbacv1.GroupKind,
+						Name:     "system:authenticated",
+					},
+				},
+			},
+			&rbacv1.ClusterRole{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "harvester-authenticated",
+				},
+				Rules: []rbacv1.PolicyRule{
+					{
+						Verbs:     []string{"get", "list", "watch"},
+						APIGroups: []string{"harvesterhci.io"},
+						Resources: []string{"settings"},
+					},
+					{
+						Verbs:         []string{"get", "watch"},
+						APIGroups:     []string{""},
+						Resources:     []string{"namespaces"},
+						ResourceNames: []string{publicNamespace},
+					},
+				},
+			},
+			&rbacv1.ClusterRoleBinding{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "harvester-authenticated",
+				},
+				Subjects: []rbacv1.Subject{{
+					Kind:     "Group",
+					APIGroup: rbacv1.GroupName,
+					Name:     "system:authenticated",
+				}},
+				RoleRef: rbacv1.RoleRef{
+					APIGroup: rbacv1.GroupName,
+					Kind:     "ClusterRole",
+					Name:     "harvester-authenticated",
+				},
+			},
+		)
+}
