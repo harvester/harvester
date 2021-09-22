69e4d65dd849b8eecdd8c30527d9a8725bdbd76d
gitlawr <lawrleegle@gmail.com>
2021-09-22 10:39:15 +0800

    Add additional-ca setting
    Support configuring additional trusted CA in harvester setting

--- pkg/webhook/resources/pod/mutator_test.go
+++ pkg/webhook/resources/pod/mutator_test.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/utils/pointer"
 
+	"github.com/harvester/harvester/pkg/util"
 	"github.com/harvester/harvester/pkg/webhook/types"
 )
 
@@ -81,6 +83,117 @@ func Test_envPatches(t *testing.T) {
 	}
 	for _, testCase := range testCases {
 		result, err := envPatches(testCase.input.targetEnvs, testCase.input.proxyEnvs, testCase.input.basePath)
+		assert.Equal(t, testCase.output, result)
+		assert.Empty(t, err)
+	}
+}
+
+func Test_volumePatch(t *testing.T) {
+
+	type input struct {
+		target []corev1.Volume
+		volume corev1.Volume
+	}
+	var testCases = []struct {
+		name   string
+		input  input
+		output string
+	}{
+		{
+			name: "add additional ca volume",
+			input: input{
+				target: []corev1.Volume{
+					{
+						Name: "foo",
+						VolumeSource: corev1.VolumeSource{
+							EmptyDir: &corev1.EmptyDirVolumeSource{},
+						},
+					},
+				},
+				volume: corev1.Volume{
+					Name: "additional-ca-volume",
+					VolumeSource: corev1.VolumeSource{
+						Secret: &corev1.SecretVolumeSource{
+							DefaultMode: pointer.Int32(400),
+							SecretName:  util.AdditionalCASecretName,
+						},
+					},
+				},
+			},
+			output: `{"op": "add", "path": "/spec/volumes/-", "value": {"name":"additional-ca-volume","secret":{"secretName":"harvester-additional-ca","defaultMode":400}}}`,
+		},
+		{
+			name: "add additional ca volume to empty volumes",
+			input: input{
+				target: []corev1.Volume{},
+				volume: corev1.Volume{
+					Name: "additional-ca-volume",
+					VolumeSource: corev1.VolumeSource{
+						Secret: &corev1.SecretVolumeSource{
+							DefaultMode: pointer.Int32(400),
+							SecretName:  util.AdditionalCASecretName,
+						},
+					},
+				},
+			},
+			output: `{"op": "add", "path": "/spec/volumes", "value": [{"name":"additional-ca-volume","secret":{"secretName":"harvester-additional-ca","defaultMode":400}}]}`,
+		},
+	}
+	for _, testCase := range testCases {
+		result, err := volumePatch(testCase.input.target, testCase.input.volume)
+		assert.Equal(t, testCase.output, result)
+		assert.Empty(t, err)
+	}
+}
+
+func Test_volumeMountPatch(t *testing.T) {
+
+	type input struct {
+		target      []corev1.VolumeMount
+		volumeMount corev1.VolumeMount
+		path        string
+	}
+	var testCases = []struct {
+		name   string
+		input  input
+		output string
+	}{
+		{
+			name: "add additional ca volume mount",
+			input: input{
+				target: []corev1.VolumeMount{
+					{
+						Name:      "foo",
+						MountPath: "/bar",
+					},
+				},
+				volumeMount: corev1.VolumeMount{
+					Name:      "additional-ca-volume",
+					MountPath: "/etc/ssl/certs/" + util.AdditionalCAFileName,
+					SubPath:   util.AdditionalCAFileName,
+					ReadOnly:  true,
+				},
+				path: "/spec/containers/0/volumeMounts",
+			},
+			output: `{"op": "add", "path": "/spec/containers/0/volumeMounts/-", "value": {"name":"additional-ca-volume","readOnly":true,"mountPath":"/etc/ssl/certs/additional-ca.pem","subPath":"additional-ca.pem"}}`,
+		},
+		{
+			name: "add additional ca volume mount to empty volumeMounts",
+			input: input{
+				target: []corev1.VolumeMount{},
+				volumeMount: corev1.VolumeMount{
+					Name:      "additional-ca-volume",
+					MountPath: "/etc/ssl/certs/" + util.AdditionalCAFileName,
+					SubPath:   util.AdditionalCAFileName,
+					ReadOnly:  true,
+				},
+				path: "/spec/containers/0/volumeMounts",
+			},
+			output: `{"op": "add", "path": "/spec/containers/0/volumeMounts", "value": [{"name":"additional-ca-volume","readOnly":true,"mountPath":"/etc/ssl/certs/additional-ca.pem","subPath":"additional-ca.pem"}]}`,
+		},
+	}
+	for _, testCase := range testCases {
+		result, err := volumeMountPatch(testCase.input.target, testCase.input.path, testCase.input.volumeMount)
 		assert.Equal(t, testCase.output, result)
 		assert.Empty(t, err)
 	}
