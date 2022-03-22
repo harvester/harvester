e44cb8c40091469384b8454e3142ec17f7cee689
Kiefer Chang <kiefer.chang@suse.com>
2022-03-22 10:52:15 +0800

    Add Prometheus resource limit and requests migration
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

--- package/upgrade/migrations/managed_charts/v1.0.0.sh
+++ package/upgrade/migrations/managed_charts/v1.0.0.sh
@@ -16,9 +16,21 @@ add_longhorn_settings()
   yq e '.spec.values.longhorn.defaultSettings.defaultDataPath = "/var/lib/harvester/defaultdisk"' $CHART_MANIFEST -i
 }
 
+adjust_prometheus_resources()
+{
+  # Increate prometheus pod limit and request (https://github.com/harvester/harvester-installer/pull/240)
+  yq e '.spec.values.prometheus.prometheusSpec.resources = {"limits": {"cpu": "1000m", "memory": "2500Mi"}, "requests": {"cpu": "750m", "memory": "1750Mi"}}' $CHART_MANIFEST -i
+
+  # Increase limits.memory (https://github.com/harvester/harvester-installer/pull/250)
+  yq e '.spec.values.prometheus-node-exporter.resources = {"limits": {"cpu": "200m", "memory": "180Mi"}, "requests": {"cpu": "100m", "memory": "30Mi"}}' $CHART_MANIFEST -i
+}
+
 case $CHART_NAME in
   harvester)
     remove_vip_config
     add_longhorn_settings
+    ;;
+  rancher-monitoring)
+    adjust_prometheus_resources
     ;;
 esac
