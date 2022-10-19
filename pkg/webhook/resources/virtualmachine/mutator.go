c72b45390dd6c15a2f8168c10db294e4b22dbd5d
Canwu Yao <yaocanwu@gmail.com>
2022-10-20 01:29:14 +0800

    Add node affinity to let the VM be scheduled to nodes with available cluster network
    Related issue: https://github.com/harvester/harvester/issues/2982

--- pkg/webhook/resources/virtualmachine/mutator.go
+++ pkg/webhook/resources/virtualmachine/mutator.go
@@ -3,7 +3,9 @@ package virtualmachine
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
+	"github.com/sirupsen/logrus"
 	admissionregv1 "k8s.io/api/admissionregistration/v1"
 	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -12,22 +14,31 @@ import (
 	kubevirtv1 "kubevirt.io/api/core/v1"
 
 	ctlharvesterv1 "github.com/harvester/harvester/pkg/generated/controllers/harvesterhci.io/v1beta1"
+	ctlcniv1 "github.com/harvester/harvester/pkg/generated/controllers/k8s.cni.cncf.io/v1"
 	"github.com/harvester/harvester/pkg/settings"
 	"github.com/harvester/harvester/pkg/util"
 	"github.com/harvester/harvester/pkg/webhook/types"
 )
 
+const (
+	networkGroup      = "network.harvesterhci.io"
+	keyClusterNetwork = networkGroup + "/clusternetwork"
+)
+
 func NewMutator(
 	setting ctlharvesterv1.SettingCache,
+	nad ctlcniv1.NetworkAttachmentDefinitionCache,
 ) types.Mutator {
 	return &vmMutator{
 		setting: setting,
+		nad:     nad,
 	}
 }
 
 type vmMutator struct {
 	types.DefaultMutator
 	setting ctlharvesterv1.SettingCache
+	nad     ctlcniv1.NetworkAttachmentDefinitionCache
 }
 
 func (m *vmMutator) Resource() types.Resource {
@@ -46,16 +57,27 @@ func (m *vmMutator) Resource() types.Resource {
 
 func (m *vmMutator) Create(request *types.Request, newObj runtime.Object) (types.PatchOps, error) {
 	vm := newObj.(*kubevirtv1.VirtualMachine)
+
+	logrus.Debugf("create VM %s/%s", vm.Namespace, vm.Name)
+
 	patchOps, err := m.patchResourceOvercommit(vm)
 	if err != nil {
 		return patchOps, err
 	}
+
+	patchOps, err = m.patchAffinity(nil, vm, patchOps)
+	if err != nil {
+		return nil, err
+	}
+
 	return patchOps, nil
 }
 
 func (m *vmMutator) Update(request *types.Request, oldObj runtime.Object, newObj runtime.Object) (types.PatchOps, error) {
 	newVm := newObj.(*kubevirtv1.VirtualMachine)
 	oldVm := oldObj.(*kubevirtv1.VirtualMachine)
+
+	logrus.Debugf("update VM %s/%s", newVm.Namespace, newVm.Name)
 
 	var patchOps types.PatchOps
 	var err error
@@ -75,6 +97,14 @@ func (m *vmMutator) Update(request *types.Request, oldObj runtime.Object, newObj
 	if needUpdateRunStrategy {
 		patchOps = patchRunStrategy(newVm, patchOps)
 	}
+
+	if needUpdateAffinity(oldVm, newVm) {
+		patchOps, err = m.patchAffinity(oldVm, newVm, patchOps)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return patchOps, nil
 }
 
@@ -216,4 +246,212 @@ func (m *vmMutator) getOvercommit() (*settings.Overcommit, error) {
 		return overcommit, err
 	}
 	return overcommit, nil
+}
+
+func needUpdateAffinity(oldVm, newVm *kubevirtv1.VirtualMachine) bool {
+	if oldVm.Spec.Template == nil || newVm.Spec.Template == nil {
+		return false
+	}
+
+	if isContainMultusNetwork(newVm.Spec.Template.Spec.Networks) {
+		return true
+	}
+	// if there are networks removed, update the affinity
+	reducedNetworks := getMultusNetworkIncrement(newVm.Spec.Template.Spec.Networks, oldVm.Spec.Template.Spec.Networks)
+	if len(reducedNetworks) != 0 {
+		return true
+	}
+
+	oldVmAffinity, newVmAffinity := oldVm.Spec.Template.Spec.Affinity, newVm.Spec.Template.Spec.Affinity
+	// the affinity.String() method already checks the nil
+	if oldVmAffinity.String() != newVmAffinity.String() {
+		return true
+	}
+
+	return false
+}
+
+func (m *vmMutator) patchAffinity(oldVm, newVm *kubevirtv1.VirtualMachine, patchOps types.PatchOps) (types.PatchOps, error) {
+	if newVm == nil || newVm.Spec.Template == nil {
+		return patchOps, nil
+	}
+
+	affinity := makeAffinityFromVMTemplate(newVm.Spec.Template)
+	nodeSelector := affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution
+
+	var oldVmNetworks []kubevirtv1.Network
+	if oldVm != nil && oldVm.Spec.Template != nil {
+		oldVmNetworks = oldVm.Spec.Template.Spec.Networks
+	}
+	newVmNetworks := newVm.Spec.Template.Spec.Networks
+	logrus.Debugf("newNetworks: %+v", newVmNetworks)
+	reducedNetworks := getMultusNetworkIncrement(newVmNetworks, oldVmNetworks)
+	logrus.Debugf("reducedNetworks: %+v", reducedNetworks)
+
+	isAdded, err := m.addNodeSelectorRequirements(newVm.Namespace, nodeSelector, newVmNetworks)
+	if err != nil {
+		return patchOps, err
+	}
+	isReduced, err := m.deleteNodeNetworkRequirements(newVm.Namespace, nodeSelector, reducedNetworks)
+	if err != nil {
+		return patchOps, err
+	}
+
+	if !(isAdded || isReduced) {
+		return patchOps, nil
+	}
+
+	bytes, err := json.Marshal(affinity)
+	if err != nil {
+		return patchOps, err
+	}
+	return append(patchOps, fmt.Sprintf(`{"op": "replace", "path": "/spec/template/spec/affinity", "value": %s}`, string(bytes))), nil
+}
+
+func (m *vmMutator) getNodeSelectorRequirementFromNetwork(defaultNamespace string, network kubevirtv1.Network) (*v1.NodeSelectorRequirement, error) {
+	if network.Multus == nil || network.Multus.NetworkName == "" {
+		return nil, nil
+	}
+
+	words := strings.Split(network.Multus.NetworkName, "/")
+	var namespace, name string
+	switch len(words) {
+	case 1:
+		namespace, name = defaultNamespace, words[0]
+	case 2:
+		namespace, name = words[0], words[1]
+	default:
+		return nil, fmt.Errorf("invalid network name %s", network.Multus.NetworkName)
+	}
+
+	nad, err := m.nad.Get(namespace, name)
+	if err != nil {
+		return nil, err
+	}
+	clusterNetwork, ok := nad.Labels[keyClusterNetwork]
+	if !ok {
+		return nil, nil
+	}
+
+	return &v1.NodeSelectorRequirement{
+		Key:      fmt.Sprintf("%s/%s", networkGroup, clusterNetwork),
+		Operator: v1.NodeSelectorOpIn,
+		Values:   []string{"true"},
+	}, nil
+}
+
+func isContainTargetNodeSelectorRequirement(term v1.NodeSelectorTerm, target v1.NodeSelectorRequirement) (int, bool) {
+	for i, expression := range term.MatchExpressions {
+		if expression.String() == target.String() {
+			return i, true
+		}
+	}
+
+	return -1, false
+}
+
+func makeAffinityFromVMTemplate(template *kubevirtv1.VirtualMachineInstanceTemplateSpec) *v1.Affinity {
+	affinity := template.Spec.Affinity
+	if affinity == nil {
+		affinity = &v1.Affinity{}
+	}
+	if affinity.NodeAffinity == nil {
+		affinity.NodeAffinity = &v1.NodeAffinity{}
+	}
+	if affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution == nil {
+		affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution = &v1.NodeSelector{}
+	}
+
+	return affinity
+}
+
+func getMultusNetworkIncrement(before, after []kubevirtv1.Network) []kubevirtv1.Network {
+	increment := make([]kubevirtv1.Network, 0, len(after))
+
+	for _, a := range after {
+		if a.Multus == nil {
+			continue
+		}
+
+		isExisting := false
+
+		for _, b := range before {
+			if b.Multus != nil && a.Multus.NetworkName == b.Multus.NetworkName {
+				isExisting = true
+				break
+			}
+		}
+
+		if !isExisting {
+			increment = append(increment, a)
+		}
+	}
+
+	return increment
+}
+
+func (m *vmMutator) addNodeSelectorRequirements(defaultNamespace string, nodeSelector *v1.NodeSelector, incrementalNetworks []kubevirtv1.Network) (bool, error) {
+	isChanged := false
+
+	for _, network := range incrementalNetworks {
+		nodeSelectorRequirement, err := m.getNodeSelectorRequirementFromNetwork(defaultNamespace, network)
+		if err != nil {
+			return false, err
+		}
+		if nodeSelectorRequirement == nil {
+			continue
+		}
+		// Since the terms of node selector are ANDed and the requirements of every term are ORed, we have to add
+		// cluster network requirements to all terms if they are existing.
+		for i, term := range nodeSelector.NodeSelectorTerms {
+			if _, ok := isContainTargetNodeSelectorRequirement(term, *nodeSelectorRequirement); !ok {
+				term.MatchExpressions = append(term.MatchExpressions, *nodeSelectorRequirement)
+				nodeSelector.NodeSelectorTerms[i] = term
+				isChanged = true
+			}
+		}
+		// If there is no term, initialize one with the cluster network requirement to prove that the cluster network
+		// requirement is added
+		if len(nodeSelector.NodeSelectorTerms) == 0 {
+			nodeSelector.NodeSelectorTerms = []v1.NodeSelectorTerm{{
+				MatchExpressions: []v1.NodeSelectorRequirement{*nodeSelectorRequirement},
+			}}
+			isChanged = true
+		}
+	}
+
+	return isChanged, nil
+}
+
+func (m *vmMutator) deleteNodeNetworkRequirements(defaultNamespace string, nodeSelector *v1.NodeSelector, reducedNetworks []kubevirtv1.Network) (bool, error) {
+	isChanged := false
+
+	for _, network := range reducedNetworks {
+		nodeSelectorRequirement, err := m.getNodeSelectorRequirementFromNetwork(defaultNamespace, network)
+		if err != nil {
+			return false, err
+		}
+		if nodeSelectorRequirement == nil {
+			continue
+		}
+		for i, term := range nodeSelector.NodeSelectorTerms {
+			if index, ok := isContainTargetNodeSelectorRequirement(term, *nodeSelectorRequirement); ok {
+				term.MatchExpressions = append(term.MatchExpressions[:index], term.MatchExpressions[index+1:]...)
+				nodeSelector.NodeSelectorTerms[i] = term
+				isChanged = true
+			}
+		}
+	}
+
+	return isChanged, nil
+}
+
+func isContainMultusNetwork(networks []kubevirtv1.Network) bool {
+	for _, network := range networks {
+		if network.Multus != nil && network.Multus.NetworkName != "" {
+			return true
+		}
+	}
+
+	return false
 }
