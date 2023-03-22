2d5a3a9aca32574a54e8be8baa44b13bea998ce2
Jian Wang <w13915984028@gmail.com>
2023-03-22 21:00:17 +0100

    add validator webhook for harvester addon
    Signed-off-by: Jian Wang <w13915984028@gmail.com>

---
+++ pkg/webhook/resources/addon/validator_test.go
@@ -0,0 +1,180 @@
+package addon
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	// corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	harvesterv1 "github.com/harvester/harvester/pkg/apis/harvesterhci.io/v1beta1"
+)
+
+func Test_validateUpdatedAddon(t *testing.T) {
+	var testCases = []struct {
+		name          string
+		oldAddon      *harvesterv1.Addon
+		newAddon      *harvesterv1.Addon
+		expectedError bool
+	}{
+		{
+			name: "user can enable addon",
+			oldAddon: &harvesterv1.Addon{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "addon1",
+				},
+				Spec: harvesterv1.AddonSpec{
+					Repo:          "repo1",
+					Chart:         "chart1",
+					Version:       "version1",
+					Enabled:       false,
+					ValuesContent: "sample",
+				},
+			},
+			newAddon: &harvesterv1.Addon{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "addon1",
+				},
+				Spec: harvesterv1.AddonSpec{
+					Repo:          "repo1",
+					Chart:         "chart1",
+					Version:       "version1",
+					Enabled:       true,
+					ValuesContent: "sample",
+				},
+			},
+			expectedError: false,
+		},
+		{
+			name: "user can disable addon",
+			oldAddon: &harvesterv1.Addon{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "addon1",
+				},
+				Spec: harvesterv1.AddonSpec{
+					Repo:          "repo1",
+					Chart:         "chart1",
+					Version:       "version1",
+					Enabled:       true,
+					ValuesContent: "sample",
+				},
+			},
+			newAddon: &harvesterv1.Addon{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "addon1",
+				},
+				Spec: harvesterv1.AddonSpec{
+					Repo:          "repo1",
+					Chart:         "chart1",
+					Version:       "version1",
+					Enabled:       false,
+					ValuesContent: "sample",
+				},
+			},
+			expectedError: false,
+		},
+		{
+			name: "user cannot change chart field",
+			oldAddon: &harvesterv1.Addon{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "addon1",
+				},
+				Spec: harvesterv1.AddonSpec{
+					Repo:          "repo1",
+					Chart:         "chart1",
+					Version:       "version1",
+					Enabled:       true,
+					ValuesContent: "sample",
+				},
+			},
+			newAddon: &harvesterv1.Addon{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "addon1",
+				},
+				Spec: harvesterv1.AddonSpec{
+					Repo:          "repo1",
+					Chart:         "chart1-changed",
+					Version:       "version1",
+					Enabled:       true,
+					ValuesContent: "sample",
+				},
+			},
+			expectedError: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		err := validateUpdatedAddon(tc.oldAddon, tc.newAddon)
+		if tc.expectedError {
+			assert.NotNil(t, err, tc.name)
+		} else {
+			assert.Nil(t, err, tc.name)
+		}
+	}
+}
+
+func Test_validateNewAddon(t *testing.T) {
+	var testCases = []struct {
+		name          string
+		newAddon      *harvesterv1.Addon
+		addonList     []*harvesterv1.Addon
+		expectedError bool
+	}{
+		{
+			name: "user can add new addon",
+			newAddon: &harvesterv1.Addon{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "addon1",
+				},
+				Spec: harvesterv1.AddonSpec{
+					Repo:          "repo1",
+					Chart:         "chart1",
+					Version:       "version1",
+					Enabled:       true,
+					ValuesContent: "sample",
+				},
+			},
+			addonList:     []*harvesterv1.Addon{},
+			expectedError: false,
+		},
+		{
+			name: "user cannot add same addon, no matter differences in version and repo fields",
+			newAddon: &harvesterv1.Addon{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "addon1",
+				},
+				Spec: harvesterv1.AddonSpec{
+					Repo:          "repo1",
+					Chart:         "chart1",
+					Version:       "version1",
+					Enabled:       true,
+					ValuesContent: "sample",
+				},
+			},
+			addonList: []*harvesterv1.Addon{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "addon1",
+					},
+					Spec: harvesterv1.AddonSpec{
+						Repo:          "repo1",
+						Chart:         "chart1",
+						Version:       "version1",
+						Enabled:       true,
+						ValuesContent: "sample",
+					},
+				},
+			},
+			expectedError: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		err := validateNewAddon(tc.newAddon, tc.addonList)
+		if tc.expectedError {
+			assert.NotNil(t, err, tc.name)
+		} else {
+			assert.Nil(t, err, tc.name)
+		}
+	}
+}
