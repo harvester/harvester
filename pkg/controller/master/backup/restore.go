a19f134cb469ba3f5b1b696439c804fb4bd0a004
Frank Yang <poan.yang@suse.com>
2023-03-23 17:16:48 +0800

    fix(volume): detach non-using volumes
    Signed-off-by: Frank Yang <poan.yang@suse.com>

--- pkg/controller/master/backup/restore.go
+++ pkg/controller/master/backup/restore.go
@@ -160,9 +160,6 @@ func (h *RestoreHandler) RestoreOnChanged(key string, restore *harvesterv1.Virtu
 	}
 
 	if isVMRestoreMissingVolumes(restore) {
-		if err := h.mountLonghornVolumes(backup); err != nil {
-			return nil, h.updateStatusError(restore, err, true)
-		}
 		return nil, h.initVolumesStatus(restore, backup)
 	}
 
@@ -345,6 +342,11 @@ func (h *RestoreHandler) reconcileResources(
 	// reconcile restoring volumes and create new PVC from CSI volumeSnapshot if not exist
 	isVolumesReady, err := h.reconcileVolumeRestores(vmRestore, backup)
 	if err != nil {
+		return nil, false, err
+	}
+
+	// mount volumes after creating PVCs, so detaching volumes controller doesn't detach the volumes
+	if err := h.mountLonghornVolumes(backup); err != nil {
 		return nil, false, err
 	}
 
