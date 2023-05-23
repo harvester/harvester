e6f6a5f419167f58891281c08ab63f60877a07eb
Chris Ho <chris.he@suse.com>
2023-05-23 18:52:41 +0800

    go mod vendor

---
+++ vendor/kubevirt.io/kubevirt/pkg/util/util.go
@@ -0,0 +1,187 @@
+package util
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	v1 "kubevirt.io/api/core/v1"
+	"kubevirt.io/client-go/log"
+)
+
+const (
+	ExtensionAPIServerAuthenticationConfigMap = "extension-apiserver-authentication"
+	RequestHeaderClientCAFileKey              = "requestheader-client-ca-file"
+	VirtShareDir                              = "/var/run/kubevirt"
+	VirtPrivateDir                            = "/var/run/kubevirt-private"
+	VirtLibDir                                = "/var/lib/kubevirt"
+	KubeletPodsDir                            = "/var/lib/kubelet/pods"
+	HostRootMount                             = "/proc/1/root/"
+	CPUManagerOS3Path                         = HostRootMount + "var/lib/origin/openshift.local.volumes/cpu_manager_state"
+	CPUManagerPath                            = HostRootMount + "var/lib/kubelet/cpu_manager_state"
+)
+
+const NonRootUID = 107
+const NonRootUserString = "qemu"
+const RootUser = 0
+
+func IsNonRootVMI(vmi *v1.VirtualMachineInstance) bool {
+	_, ok := vmi.Annotations[v1.DeprecatedNonRootVMIAnnotation]
+
+	nonRoot := vmi.Status.RuntimeUser != 0
+	return ok || nonRoot
+}
+
+func IsSRIOVVmi(vmi *v1.VirtualMachineInstance) bool {
+	for _, iface := range vmi.Spec.Domain.Devices.Interfaces {
+		if iface.SRIOV != nil {
+			return true
+		}
+	}
+	return false
+}
+
+// Check if a VMI spec requests GPU
+func IsGPUVMI(vmi *v1.VirtualMachineInstance) bool {
+	if vmi.Spec.Domain.Devices.GPUs != nil && len(vmi.Spec.Domain.Devices.GPUs) != 0 {
+		return true
+	}
+	return false
+}
+
+// Check if a VMI spec requests VirtIO-FS
+func IsVMIVirtiofsEnabled(vmi *v1.VirtualMachineInstance) bool {
+	if vmi.Spec.Domain.Devices.Filesystems != nil {
+		for _, fs := range vmi.Spec.Domain.Devices.Filesystems {
+			if fs.Virtiofs != nil {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// Check if a VMI spec requests a HostDevice
+func IsHostDevVMI(vmi *v1.VirtualMachineInstance) bool {
+	if vmi.Spec.Domain.Devices.HostDevices != nil && len(vmi.Spec.Domain.Devices.HostDevices) != 0 {
+		return true
+	}
+	return false
+}
+
+// Check if a VMI spec requests a VFIO device
+func IsVFIOVMI(vmi *v1.VirtualMachineInstance) bool {
+
+	if IsHostDevVMI(vmi) || IsGPUVMI(vmi) || IsSRIOVVmi(vmi) {
+		return true
+	}
+	return false
+}
+
+// Check if a VMI spec requests AMD SEV
+func IsSEVVMI(vmi *v1.VirtualMachineInstance) bool {
+	return vmi.Spec.Domain.LaunchSecurity != nil && vmi.Spec.Domain.LaunchSecurity.SEV != nil
+}
+
+// WantVirtioNetDevice checks whether a VMI references at least one "virtio" network interface.
+// Note that the reference can be explicit or implicit (unspecified nic models defaults to "virtio").
+func WantVirtioNetDevice(vmi *v1.VirtualMachineInstance) bool {
+	for _, iface := range vmi.Spec.Domain.Devices.Interfaces {
+		if iface.Model == "" || iface.Model == "virtio" {
+			return true
+		}
+	}
+	return false
+}
+
+// NeedVirtioNetDevice checks whether a VMI requires the presence of the "virtio" net device.
+// This happens when the VMI wants to use a "virtio" network interface, and software emulation is disallowed.
+func NeedVirtioNetDevice(vmi *v1.VirtualMachineInstance, allowEmulation bool) bool {
+	return WantVirtioNetDevice(vmi) && !allowEmulation
+}
+
+func NeedTunDevice(vmi *v1.VirtualMachineInstance) bool {
+	return (len(vmi.Spec.Domain.Devices.Interfaces) > 0) ||
+		(vmi.Spec.Domain.Devices.AutoattachPodInterface == nil) ||
+		(*vmi.Spec.Domain.Devices.AutoattachPodInterface == true)
+}
+
+// UseSoftwareEmulationForDevice determines whether to fallback to software emulation for the given device.
+// This happens when the given device doesn't exist, and software emulation is enabled.
+func UseSoftwareEmulationForDevice(devicePath string, allowEmulation bool) (bool, error) {
+	if !allowEmulation {
+		return false, nil
+	}
+
+	_, err := os.Stat(devicePath)
+	if err == nil {
+		return false, nil
+	}
+	if os.IsNotExist(err) {
+		return true, nil
+	}
+	return false, err
+}
+
+func ResourceNameToEnvVar(prefix string, resourceName string) string {
+	varName := strings.ToUpper(resourceName)
+	varName = strings.Replace(varName, "/", "_", -1)
+	varName = strings.Replace(varName, ".", "_", -1)
+	return fmt.Sprintf("%s_%s", prefix, varName)
+}
+
+// Checks if kernel boot is defined in a valid way
+func HasKernelBootContainerImage(vmi *v1.VirtualMachineInstance) bool {
+	if vmi == nil {
+		return false
+	}
+
+	vmiFirmware := vmi.Spec.Domain.Firmware
+	if (vmiFirmware == nil) || (vmiFirmware.KernelBoot == nil) || (vmiFirmware.KernelBoot.Container == nil) {
+		return false
+	}
+
+	return true
+}
+
+func HasHugePages(vmi *v1.VirtualMachineInstance) bool {
+	return vmi.Spec.Domain.Memory != nil && vmi.Spec.Domain.Memory.Hugepages != nil
+}
+
+func IsReadOnlyDisk(disk *v1.Disk) bool {
+	isReadOnlyCDRom := disk.CDRom != nil && (disk.CDRom.ReadOnly == nil || *disk.CDRom.ReadOnly == true)
+
+	return isReadOnlyCDRom
+}
+
+// AlignImageSizeTo1MiB rounds down the size to the nearest multiple of 1MiB
+// A warning or an error may get logged
+// The caller is responsible for ensuring the rounded-down size is not 0
+func AlignImageSizeTo1MiB(size int64, logger *log.FilteredLogger) int64 {
+	remainder := size % (1024 * 1024)
+	if remainder == 0 {
+		return size
+	} else {
+		newSize := size - remainder
+		if logger != nil {
+			if newSize == 0 {
+				logger.Errorf("disks must be at least 1MiB, %d bytes is too small", size)
+			} else {
+				logger.Warningf("disk size is not 1MiB-aligned. Adjusting from %d down to %d.", size, newSize)
+			}
+		}
+		return newSize
+	}
+
+}
+func CanBeNonRoot(vmi *v1.VirtualMachineInstance) error {
+	// VirtioFS doesn't work with session mode
+	if IsVMIVirtiofsEnabled(vmi) {
+		return fmt.Errorf("VirtioFS doesn't work with session mode(used by nonroot)")
+	}
+	return nil
+}
+
+func MarkAsNonroot(vmi *v1.VirtualMachineInstance) {
+	vmi.Status.RuntimeUser = 107
+}
---
+++ vendor/kubevirt.io/kubevirt/pkg/virt-config/virt-config.go
@@ -0,0 +1,290 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2017, 2018 Red Hat, Inc.
+ *
+ */
+
+package virtconfig
+
+/*
+ This module is intended for exposing the virtualization configuration that is available at the cluster-level and its default settings.
+*/
+
+import (
+	k8sv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+
+	v1 "kubevirt.io/api/core/v1"
+)
+
+const (
+	ParallelOutboundMigrationsPerNodeDefault uint32 = 2
+	ParallelMigrationsPerClusterDefault      uint32 = 5
+	BandwithPerMigrationDefault                     = "0Mi"
+	MigrationAllowAutoConverge               bool   = false
+	MigrationAllowPostCopy                   bool   = false
+	MigrationProgressTimeout                 int64  = 150
+	MigrationCompletionTimeoutPerGiB         int64  = 800
+	DefaultAMD64MachineType                         = "q35"
+	DefaultPPC64LEMachineType                       = "pseries"
+	DefaultAARCH64MachineType                       = "virt"
+	DefaultCPURequest                               = "100m"
+	DefaultMemoryOvercommit                         = 100
+	DefaultAMD64EmulatedMachines                    = "q35*,pc-q35*"
+	DefaultPPC64LEEmulatedMachines                  = "pseries*"
+	DefaultAARCH64EmulatedMachines                  = "virt*"
+	DefaultLessPVCSpaceToleration                   = 10
+	DefaultMinimumReservePVCBytes                   = 131072
+	DefaultNodeSelectors                            = ""
+	DefaultNetworkInterface                         = "bridge"
+	DefaultImagePullPolicy                          = k8sv1.PullIfNotPresent
+	DefaultAllowEmulation                           = false
+	DefaultUnsafeMigrationOverride                  = false
+	DefaultPermitSlirpInterface                     = false
+	SmbiosConfigDefaultFamily                       = "KubeVirt"
+	SmbiosConfigDefaultManufacturer                 = "KubeVirt"
+	SmbiosConfigDefaultProduct                      = "None"
+	DefaultPermitBridgeInterfaceOnPodNetwork        = true
+	DefaultSELinuxLauncherType                      = "virt_launcher.process"
+	SupportedGuestAgentVersions                     = "2.*,3.*,4.*,5.*"
+	DefaultARCHOVMFPath                             = "/usr/share/OVMF"
+	DefaultAARCH64OVMFPath                          = "/usr/share/AAVMF"
+	DefaultMemBalloonStatsPeriod             uint32 = 10
+	DefaultCPUAllocationRatio                       = 10
+	DefaultDiskVerificationMemoryLimitMBytes        = 1500
+	DefaultVirtAPILogVerbosity                      = 2
+	DefaultVirtControllerLogVerbosity               = 2
+	DefaultVirtHandlerLogVerbosity                  = 2
+	DefaultVirtLauncherLogVerbosity                 = 2
+	DefaultVirtOperatorLogVerbosity                 = 2
+
+	// Default REST configuration settings
+	DefaultVirtHandlerQPS         float32 = 5
+	DefaultVirtHandlerBurst               = 10
+	DefaultVirtControllerQPS      float32 = 20
+	DefaultVirtControllerBurst            = 30
+	DefaultVirtAPIQPS             float32 = 5
+	DefaultVirtAPIBurst                   = 10
+	DefaultVirtWebhookClientQPS           = 200
+	DefaultVirtWebhookClientBurst         = 400
+)
+
+func IsAMD64(arch string) bool {
+	if arch == "amd64" {
+		return true
+	}
+	return false
+}
+
+func IsARM64(arch string) bool {
+	if arch == "arm64" {
+		return true
+	}
+	return false
+}
+
+func IsPPC64(arch string) bool {
+	if arch == "ppc64le" {
+		return true
+	}
+	return false
+}
+
+func (c *ClusterConfig) GetMemBalloonStatsPeriod() uint32 {
+	return *c.GetConfig().MemBalloonStatsPeriod
+}
+
+func (c *ClusterConfig) AllowEmulation() bool {
+	return c.GetConfig().DeveloperConfiguration.UseEmulation
+}
+
+func (c *ClusterConfig) GetMigrationConfiguration() *v1.MigrationConfiguration {
+	return c.GetConfig().MigrationConfiguration
+}
+
+func (c *ClusterConfig) GetImagePullPolicy() (policy k8sv1.PullPolicy) {
+	return c.GetConfig().ImagePullPolicy
+}
+
+func (c *ClusterConfig) GetResourceVersion() string {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.lastValidConfigResourceVersion
+}
+
+func (c *ClusterConfig) GetMachineType() string {
+	return c.GetConfig().MachineType
+}
+
+func (c *ClusterConfig) GetCPUModel() string {
+	return c.GetConfig().CPUModel
+}
+
+func (c *ClusterConfig) GetCPURequest() *resource.Quantity {
+	return c.GetConfig().CPURequest
+}
+
+func (c *ClusterConfig) GetDiskVerification() *v1.DiskVerification {
+	return c.GetConfig().DeveloperConfiguration.DiskVerification
+}
+
+func (c *ClusterConfig) GetMemoryOvercommit() int {
+	return c.GetConfig().DeveloperConfiguration.MemoryOvercommit
+}
+
+func (c *ClusterConfig) GetEmulatedMachines() []string {
+	return c.GetConfig().EmulatedMachines
+}
+
+func (c *ClusterConfig) GetLessPVCSpaceToleration() int {
+	return c.GetConfig().DeveloperConfiguration.LessPVCSpaceToleration
+}
+
+func (c *ClusterConfig) GetMinimumReservePVCBytes() uint64 {
+	return c.GetConfig().DeveloperConfiguration.MinimumReservePVCBytes
+}
+
+func (c *ClusterConfig) GetNodeSelectors() map[string]string {
+	return c.GetConfig().DeveloperConfiguration.NodeSelectors
+}
+
+func (c *ClusterConfig) GetDefaultNetworkInterface() string {
+	return c.GetConfig().NetworkConfiguration.NetworkInterface
+}
+
+func (c *ClusterConfig) IsSlirpInterfaceEnabled() bool {
+	return *c.GetConfig().NetworkConfiguration.PermitSlirpInterface
+}
+
+func (c *ClusterConfig) GetSMBIOS() *v1.SMBiosConfiguration {
+	return c.GetConfig().SMBIOSConfig
+}
+
+func (c *ClusterConfig) IsBridgeInterfaceOnPodNetworkEnabled() bool {
+	return *c.GetConfig().NetworkConfiguration.PermitBridgeInterfaceOnPodNetwork
+}
+
+func (c *ClusterConfig) GetDefaultClusterConfig() *v1.KubeVirtConfiguration {
+	return c.defaultConfig
+}
+
+func (c *ClusterConfig) GetSELinuxLauncherType() string {
+	return c.GetConfig().SELinuxLauncherType
+}
+
+func (c *ClusterConfig) GetDefaultRuntimeClass() string {
+	return c.GetConfig().DefaultRuntimeClass
+}
+
+func (c *ClusterConfig) GetSupportedAgentVersions() []string {
+	return c.GetConfig().SupportedGuestAgentVersions
+}
+
+func (c *ClusterConfig) GetOVMFPath() string {
+	return c.GetConfig().OVMFPath
+}
+
+func (c *ClusterConfig) GetCPUAllocationRatio() int {
+	return c.GetConfig().DeveloperConfiguration.CPUAllocationRatio
+}
+
+func (c *ClusterConfig) GetMinimumClusterTSCFrequency() *int64 {
+	return c.GetConfig().DeveloperConfiguration.MinimumClusterTSCFrequency
+}
+
+func (c *ClusterConfig) GetPermittedHostDevices() *v1.PermittedHostDevices {
+	return c.GetConfig().PermittedHostDevices
+}
+
+func canSelectNode(nodeSelector map[string]string, node *k8sv1.Node) bool {
+	for key, val := range nodeSelector {
+		labelValue, exist := node.Labels[key]
+		if !exist || val != labelValue {
+			return false
+		}
+	}
+	return true
+}
+
+func (c *ClusterConfig) GetDesiredMDEVTypes(node *k8sv1.Node) []string {
+	mdevTypesConf := c.GetConfig().MediatedDevicesConfiguration
+	if mdevTypesConf == nil {
+		return []string{}
+	}
+	nodeMdevConf := mdevTypesConf.NodeMediatedDeviceTypes
+	if nodeMdevConf != nil {
+		mdevTypesMap := make(map[string]struct{})
+		for _, nodeConfig := range nodeMdevConf {
+			if canSelectNode(nodeConfig.NodeSelector, node) {
+				for _, mdevType := range nodeConfig.MediatedDevicesTypes {
+					mdevTypesMap[mdevType] = struct{}{}
+				}
+			}
+		}
+		if len(mdevTypesMap) != 0 {
+			mdevTypesList := []string{}
+			for mdevType, _ := range mdevTypesMap {
+				mdevTypesList = append(mdevTypesList, mdevType)
+			}
+			return mdevTypesList
+		}
+	}
+	return mdevTypesConf.MediatedDevicesTypes
+}
+
+func (c *ClusterConfig) GetVirtHandlerVerbosity(nodeName string) uint {
+	logConf := c.GetConfig().DeveloperConfiguration.LogVerbosity
+	if level := logConf.NodeVerbosity[nodeName]; level != 0 {
+		return level
+	}
+	return logConf.VirtHandler
+}
+
+func (c *ClusterConfig) GetVirtAPIVerbosity(nodeName string) uint {
+	logConf := c.GetConfig().DeveloperConfiguration.LogVerbosity
+	if level := logConf.NodeVerbosity[nodeName]; level != 0 {
+		return level
+	}
+	return logConf.VirtAPI
+}
+
+func (c *ClusterConfig) GetVirtControllerVerbosity(nodeName string) uint {
+	logConf := c.GetConfig().DeveloperConfiguration.LogVerbosity
+	if level := logConf.NodeVerbosity[nodeName]; level != 0 {
+		return level
+	}
+	return logConf.VirtController
+}
+
+func (c *ClusterConfig) GetVirtLauncherVerbosity() uint {
+	logConf := c.GetConfig().DeveloperConfiguration.LogVerbosity
+	return logConf.VirtLauncher
+}
+
+//GetMinCPUModel return minimal cpu which is used in node-labeller
+func (c *ClusterConfig) GetMinCPUModel() string {
+	return c.GetConfig().MinCPUModel
+}
+
+//GetObsoleteCPUModels return slice of obsolete cpus which are used in node-labeller
+func (c *ClusterConfig) GetObsoleteCPUModels() map[string]bool {
+	return c.GetConfig().ObsoleteCPUModels
+}
+
+//GetClusterCPUArch return the CPU architecture in ClusterConfig
+func (c *ClusterConfig) GetClusterCPUArch() string {
+	return c.cpuArch
+}
