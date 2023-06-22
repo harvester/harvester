6319b1ec0b13faabd70d602821932e25a9a0f99a
Jian Wang <w13915984028@gmail.com>
2023-06-22 23:20:05 +0200

    enhance addon status processing
      refactor code to run per status
      add validator to deny request when operation is ongoing
      add mutator to record user operation and timestamp
      add conditions to status to record more
      add test code
    
    Signed-off-by: Jian Wang <w13915984028@gmail.com>
    
    Co-authored-by: Gaurav Mehta <gaurav@rancher.com>
    (cherry picked from commit 0824ea6f62fc8bfa0df461465972c3d03a57cb42)

--- pkg/webhook/resources/addon/mutator.go
+++ pkg/webhook/resources/addon/mutator.go
@@ -1,14 +1,15 @@
 package addon
 
 import (
-	//	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
 
+	"github.com/sirupsen/logrus"
+
 	admissionregv1 "k8s.io/api/admissionregistration/v1"
-	corev1 "k8s.io/api/core/v1"
 
 	harvesterv1 "github.com/harvester/harvester/pkg/apis/harvesterhci.io/v1beta1"
 	ctlharvesterv1 "github.com/harvester/harvester/pkg/generated/controllers/harvesterhci.io/v1beta1"
@@ -33,8 +34,8 @@ func newResource(ops []admissionregv1.OperationType) types.Resource {
 	return types.Resource{
 		Names:          []string{string(harvesterv1.AddonResourceName)},
 		Scope:          admissionregv1.NamespacedScope,
-		APIGroup:       corev1.SchemeGroupVersion.Group,
-		APIVersion:     corev1.SchemeGroupVersion.Version,
+		APIGroup:       harvesterv1.SchemeGroupVersion.Group,
+		APIVersion:     harvesterv1.SchemeGroupVersion.Version,
 		ObjectType:     &harvesterv1.Addon{},
 		OperationTypes: ops,
 	}
@@ -59,49 +60,53 @@ func (m *addonMutator) Update(request *types.Request, oldObj runtime.Object, new
 	newAddon := newObj.(*harvesterv1.Addon)
 	oldAddon := oldObj.(*harvesterv1.Addon)
 
-	operation := "update"
+	addonOperation := "update"
 
 	if newAddon.Spec.Enabled != oldAddon.Spec.Enabled {
 		if newAddon.Spec.Enabled {
-			operation = "enable"
+			addonOperation = "enable"
 		} else {
-			operation = "disable"
+			addonOperation = "disable"
 		}
 	}
 
 	var patchOps types.PatchOps
 
-	return patchLastOperation(newAddon, patchOps, operation)
+	return patchLastOperation(newAddon, patchOps, addonOperation)
 }
 
-func patchLastOperation(addon *harvesterv1.Addon, patchOps types.PatchOps, operation string) (types.PatchOps, error) {
-	op1 := "add"
-	op2 := "add"
+func patchLastOperation(addon *harvesterv1.Addon, patchOps types.PatchOps, addonOperation string) (types.PatchOps, error) {
+	jsonOp1 := "add"
+	jsonOp2 := "add"
 	if addon.Annotations == nil {
 		addon.Annotations = make(map[string]string, 2)
 	} else {
-		if op, ok := addon.Annotations[util.AnnotationAddonLastOperation]; ok {
-			// operation not changed, e.g. update content
-			if op == operation {
-				op1 = ""
+		if lastOp, ok := addon.Annotations[util.AnnotationAddonLastOperation]; ok {
+			// new operation is same as last operation, e.g. update content
+			if lastOp == addonOperation {
+				jsonOp1 = ""
 			} else {
-				op1 = "replace"
+				jsonOp1 = "replace"
 			}
 		}
 
 		// timestamp is there
 		if _, ok := addon.Annotations[util.AnnotationAddonLastOperationTimestamp]; ok {
-			op2 = "replace"
+			jsonOp2 = "replace"
 		}
 	}
 
-	if op1 != "" {
-		// patch last operation
-		patchOps = append(patchOps, fmt.Sprintf(`{"op": "%s", "path": "/spec/annotations/%s", "value": %s}`, op1, util.AnnotationAddonLastOperation, operation))
+	if jsonOp1 != "" {
+		// patch last operation, the key should be like harvesterhci.io~1addon-last-operation instead of harvesterhci.io/addon-last-operation
+		key := strings.Replace(util.AnnotationAddonLastOperation, "/", "~1", 1)
+		patchOps = append(patchOps, fmt.Sprintf(`{"op": "%s", "path": "/metadata/annotations/%s", "value": "%s"}`, jsonOp1, key, addonOperation))
 	}
 
-	// patch last operation timestamp
-	patchOps = append(patchOps, fmt.Sprintf(`{"op": "%s", "path": "/spec/annotations/%s", "value": %s}`, op2, util.AnnotationAddonLastOperationTimestamp, time.Now().UTC().Format(time.RFC3339)))
+	// patch last operation timestamp, always update
+	key := strings.Replace(util.AnnotationAddonLastOperationTimestamp, "/", "~1", 1)
+	patchOps = append(patchOps, fmt.Sprintf(`{"op": "%s", "path": "/metadata/annotations/%s", "value": "%s"}`, jsonOp2, key, time.Now().UTC().Format(time.RFC3339)))
+
+	logrus.Infof("addon mutation result: %v", patchOps)
 
 	return patchOps, nil
 }
