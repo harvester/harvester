b8baef60588928ce033944b6955da63433d7ae52
Jian Wang <w13915984028@gmail.com>
2023-07-03 15:41:52 +0200

    Add Failed condition for UI usage and enhance validator
      Add Failed condition to record error message and UI will use it
      Silient debug related code
      Enhance validator to allow update addon annotation to adjust
       timeout when addon is in an operation
    
    Signed-off-by: Jian Wang <w13915984028@gmail.com>
    (cherry picked from commit 5effb48bdc8976e80fb30c3f062f4829b9fe4b19)

--- pkg/webhook/resources/addon/validator.go
+++ pkg/webhook/resources/addon/validator.go
@@ -83,15 +83,16 @@ func validateUpdatedAddon(newAddon *v1beta1.Addon, oldAddon *v1beta1.Addon) erro
 		return werror.NewBadRequest("chart field cannot be changed.")
 	}
 
-	if oldAddon.Status.Status == v1beta1.AddonEnabled {
+	// do not allow change Spec when an operation is onging, but allow change annotation, e.g. adjust the timeout
+	if oldAddon.Status.Status == v1beta1.AddonEnabled && isAddonSpecChanged(newAddon, oldAddon) {
 		return werror.NewBadRequest("addon is being deployed now, please wait and retry")
 	}
 
-	if oldAddon.Status.Status == v1beta1.AddonUpdating {
+	if oldAddon.Status.Status == v1beta1.AddonUpdating && isAddonSpecChanged(newAddon, oldAddon) {
 		return werror.NewBadRequest("addon is being updated now, please wait and retry")
 	}
 
-	if oldAddon.Status.Status == v1beta1.AddonDisabling {
+	if oldAddon.Status.Status == v1beta1.AddonDisabling && isAddonSpecChanged(newAddon, oldAddon) {
 		return werror.NewBadRequest("addon is being disabled now, please wait and retry")
 	}
 
@@ -99,6 +100,16 @@ func validateUpdatedAddon(newAddon *v1beta1.Addon, oldAddon *v1beta1.Addon) erro
 		return validateVClusterAddon(newAddon)
 	}
 	return nil
+}
+
+func isAddonSpecChanged(newAddon *v1beta1.Addon, oldAddon *v1beta1.Addon) bool {
+	if newAddon.Spec != oldAddon.Spec {
+		return true
+	}
+
+	// check any other potential fields
+
+	return false
 }
 
 func validateVClusterAddon(newAddon *v1beta1.Addon) error {
