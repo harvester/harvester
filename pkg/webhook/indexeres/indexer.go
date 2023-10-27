203ab1f191bb73c994b9a06df948311bff7e2191
PoAn Yang <poan.yang@suse.com>
2023-10-27 17:48:55 +0800

    fix: prevent vmrestore with delete policy if there is vmbackup snapshot
    Signed-off-by: PoAn Yang <poan.yang@suse.com>
    (cherry picked from commit 03039ce461e9316cb7f17274ce1ca04f3b93b392)

--- pkg/webhook/indexeres/indexer.go
+++ pkg/webhook/indexeres/indexer.go
@@ -9,20 +9,23 @@ import (
 )
 
 const (
-	VMBackupBySourceUIDIndex            = "harvesterhci.io/vmbackup-by-source-uid"
-	VMRestoreByTargetNamespaceAndName   = "harvesterhci.io/vmrestore-by-target-namespace-and-name"
-	VMRestoreByVMBackupNamespaceAndName = "harvesterhci.io/vmrestore-by-vmbackup-namespace-and-name"
+	VMBackupBySourceUIDIndex              = "harvesterhci.io/vmbackup-by-source-uid"
+	VMRestoreByTargetNamespaceAndName     = "harvesterhci.io/vmrestore-by-target-namespace-and-name"
+	VMRestoreByVMBackupNamespaceAndName   = "harvesterhci.io/vmrestore-by-vmbackup-namespace-and-name"
+	VMBackupSnapshotByPVCNamespaceAndName = "harvesterhci.io/vmbackup-snapshot-by-pvc-namespace-and-name"
 )
 
 func RegisterIndexers(clients *clients.Clients) {
 	vmBackupCache := clients.HarvesterFactory.Harvesterhci().V1beta1().VirtualMachineBackup().Cache()
-	vmRestoreCache := clients.HarvesterFactory.Harvesterhci().V1beta1().VirtualMachineRestore().Cache()
 	vmBackupCache.AddIndexer(VMBackupBySourceUIDIndex, vmBackupBySourceUID)
+	vmBackupCache.AddIndexer(VMBackupSnapshotByPVCNamespaceAndName, vmBackupSnapshotByPVCNamespaceAndName)
+
+	vmRestoreCache := clients.HarvesterFactory.Harvesterhci().V1beta1().VirtualMachineRestore().Cache()
 	vmRestoreCache.AddIndexer(VMRestoreByTargetNamespaceAndName, vmRestoreByTargetNamespaceAndName)
 	vmRestoreCache.AddIndexer(VMRestoreByVMBackupNamespaceAndName, vmRestoreByVMBackupNamespaceAndName)
 
-	podInformer := clients.CoreFactory.Core().V1().Pod().Cache()
-	podInformer.AddIndexer(indexeres.PodByVMNameIndex, indexeres.PodByVMName)
+	podCache := clients.CoreFactory.Core().V1().Pod().Cache()
+	podCache.AddIndexer(indexeres.PodByVMNameIndex, indexeres.PodByVMName)
 }
 
 func vmBackupBySourceUID(obj *harvesterv1.VirtualMachineBackup) ([]string, error) {
@@ -30,6 +33,19 @@ func vmBackupBySourceUID(obj *harvesterv1.VirtualMachineBackup) ([]string, error
 		return []string{string(*obj.Status.SourceUID)}, nil
 	}
 	return []string{}, nil
+}
+
+func vmBackupSnapshotByPVCNamespaceAndName(obj *harvesterv1.VirtualMachineBackup) ([]string, error) {
+	if obj.Spec.Type == harvesterv1.Backup || obj.Status == nil {
+		return []string{}, nil
+	}
+
+	result := make([]string, 0, len(obj.Status.VolumeBackups))
+	for _, volumeBackup := range obj.Status.VolumeBackups {
+		pvc := volumeBackup.PersistentVolumeClaim
+		result = append(result, fmt.Sprintf("%s/%s", pvc.ObjectMeta.Namespace, pvc.ObjectMeta.Name))
+	}
+	return result, nil
 }
 
 func vmRestoreByTargetNamespaceAndName(obj *harvesterv1.VirtualMachineRestore) ([]string, error) {
