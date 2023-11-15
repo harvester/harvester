a37d945efd794eae97ce61fcbb9f748042f5fe64
Gaurav Mehta <gaurav@rancher.com>
2023-11-15 13:51:43 +1100

    rebase changes for PR3879

--- pkg/controller/master/nodedrain/nodedrain_controller.go
+++ pkg/controller/master/nodedrain/nodedrain_controller.go
@@ -246,6 +246,28 @@ func (ndc *ControllerHandler) FindAndListVM(node *corev1.Node) ([]string, error)
 	return impactedVMDetails, nil
 }
 
+// FindAndListNonMigratableVM is called by action handler to leverage caches to find VM's which may have a cdrom or container disk
+// attached to vmi
+func (ndc *ControllerHandler) FindAndListNonMigratableVM(node *corev1.Node) ([]string, error) {
+	labelsMap := map[string]string{
+		kubevirtv1.NodeNameLabel: node.Name,
+	}
+	labelSelector := labels.SelectorFromSet(labelsMap)
+
+	vmiList, err := ndc.virtualMachineInstanceCache.List(corev1.NamespaceAll, labelSelector)
+	if err != nil {
+		return nil, fmt.Errorf("error listing VMI: %v", err)
+	}
+
+	var impactedVMI []string
+	for _, vmi := range vmiList {
+		if vmContainsCDRomOrContainerDisk(vmi) {
+			impactedVMI = append(impactedVMI, fmt.Sprintf("%s/%s", vmi.Namespace, vmi.Name))
+		}
+	}
+	return impactedVMI, nil
+}
+
 func ActionHelper(nodeCache ctlcorev1.NodeCache, virtualMachineInstanceCache ctlkubevirtv1.VirtualMachineInstanceCache,
 	longhornVolumeCache longhornv1beta1.VolumeCache, longhornReplicaCache longhornv1beta1.ReplicaCache) *ControllerHandler {
 	return &ControllerHandler{
@@ -254,4 +276,19 @@ func ActionHelper(nodeCache ctlcorev1.NodeCache, virtualMachineInstanceCache ctl
 		longhornVolumeCache:         longhornVolumeCache,
 		longhornReplicaCache:        longhornReplicaCache,
 	}
+}
+
+func vmContainsCDRomOrContainerDisk(vmi *kubevirtv1.VirtualMachineInstance) bool {
+	for _, disk := range vmi.Spec.Domain.Devices.Disks {
+		if disk.CDRom != nil {
+			return true
+		}
+	}
+
+	for _, volume := range vmi.Spec.Volumes {
+		if volume.VolumeSource.ContainerDisk != nil {
+			return true
+		}
+	}
+	return false
 }
