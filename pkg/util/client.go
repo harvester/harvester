c5472d7f1627037da1856851c84901d77db64aaa
PoAn Yang <poan.yang@suse.com>
2023-11-29 15:22:56 +0800

    fix: bump kubevirt package error
    Signed-off-by: PoAn Yang <poan.yang@suse.com>

--- pkg/util/client.go
+++ pkg/util/client.go
@@ -6,13 +6,13 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/rest"
-	"kubevirt.io/kubevirt/pkg/virt-operator/resource/generate/rbac"
+	"kubevirt.io/kubevirt/pkg/virt-operator/resource/generate/components"
 )
 
 func VirtClientUpdateVmi(ctx context.Context, client rest.Interface, managementNamespace, namespace, name string, obj runtime.Object) error {
 	return client.Put().
 		Namespace(namespace).
-		SetHeader("Impersonate-User", fmt.Sprintf("system:serviceaccount:%s:%s", managementNamespace, rbac.ControllerServiceAccountName)).
+		SetHeader("Impersonate-User", fmt.Sprintf("system:serviceaccount:%s:%s", managementNamespace, components.ControllerServiceAccountName)).
 		Resource("virtualmachineinstances").
 		Name(name).
 		Body(obj).
