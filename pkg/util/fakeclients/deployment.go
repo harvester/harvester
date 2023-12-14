a50dabbc592d8a4a984145618dbf9d7a3b52eedc
Gaurav Mehta <gaurav@rancher.com>
2023-12-14 15:59:01 +1100

    added a handler to patch capi deployment to work around an issue with 2.7.9

--- pkg/util/fakeclients/deployment.go
+++ pkg/util/fakeclients/deployment.go
@@ -13,26 +13,33 @@ import (
 type DeploymentClient func(string) appsv1type.DeploymentInterface
 
 func (c DeploymentClient) Update(deployment *appsv1.Deployment) (*appsv1.Deployment, error) {
-	panic("implement me")
+	return c(deployment.Namespace).Update(context.TODO(), deployment, metav1.UpdateOptions{})
 }
+
 func (c DeploymentClient) Get(namespace, name string, options metav1.GetOptions) (*appsv1.Deployment, error) {
 	return c(namespace).Get(context.TODO(), name, metav1.GetOptions{})
 }
+
 func (c DeploymentClient) Create(deployment *appsv1.Deployment) (*appsv1.Deployment, error) {
 	return c(deployment.Namespace).Create(context.TODO(), deployment, metav1.CreateOptions{})
 }
+
 func (c DeploymentClient) UpdateStatus(*appsv1.Deployment) (*appsv1.Deployment, error) {
 	panic("implement me")
 }
+
 func (c DeploymentClient) Delete(namespace, name string, options *metav1.DeleteOptions) error {
 	panic("implement me")
 }
+
 func (c DeploymentClient) List(namespace string, opts metav1.ListOptions) (*appsv1.DeploymentList, error) {
 	panic("implement me")
 }
+
 func (c DeploymentClient) Watch(namespace string, opts metav1.ListOptions) (watch.Interface, error) {
 	panic("implement me")
 }
+
 func (c DeploymentClient) Patch(namespace, name string, pt types.PatchType, data []byte, subresources ...string) (result *appsv1.Deployment, err error) {
 	panic("implement me")
 }
