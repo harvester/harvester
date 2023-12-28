f016e6312f17d59bb5bca42a7ae12f3acc930fa8
Jack Yu <jack.yu@suse.com>
2023-12-27 20:38:48 -0600

    feat: fetch app generalJob image information from rancher apps (#4896)
    * feat: fetch app generalJob image information from rancher APP CR
    ---------
    Signed-off-by: Jack Yu <jack.yu@suse.com>
    (cherry picked from commit 54fd0fdb48b71cb6faf62498fbe26b43ee645f76)

--- pkg/util/fakeclients/app.go
+++ pkg/util/fakeclients/app.go
@@ -7,6 +7,8 @@ import (
 	ctlcatalogv1 "github.com/rancher/rancher/pkg/generated/controllers/catalog.cattle.io/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/watch"
 
 	catalogv1type "github.com/harvester/harvester/pkg/generated/clientset/versioned/typed/catalog.cattle.io/v1"
 )
@@ -23,5 +25,39 @@ func (c AppCache) AddIndexer(indexName string, indexer ctlcatalogv1.AppIndexer) 
 	panic("implement me")
 }
 func (c AppCache) GetByIndex(indexName, key string) ([]*catalogv1.App, error) {
+	panic("implement me")
+}
+
+type AppClient func(string) catalogv1type.AppInterface
+
+func (c AppClient) Update(app *catalogv1.App) (*catalogv1.App, error) {
+	return c(app.Namespace).Update(context.TODO(), app, metav1.UpdateOptions{})
+}
+
+func (c AppClient) Get(namespace, name string, options metav1.GetOptions) (*catalogv1.App, error) {
+	return c(namespace).Get(context.TODO(), name, options)
+}
+
+func (c AppClient) Create(app *catalogv1.App) (*catalogv1.App, error) {
+	return c(app.Namespace).Create(context.TODO(), app, metav1.CreateOptions{})
+}
+
+func (c AppClient) Delete(namespace, name string, options *metav1.DeleteOptions) error {
+	panic("implement me")
+}
+
+func (c AppClient) List(namespace string, opts metav1.ListOptions) (*catalogv1.AppList, error) {
+	panic("implement me")
+}
+
+func (c AppClient) UpdateStatus(*catalogv1.App) (*catalogv1.App, error) {
+	panic("implement me")
+}
+
+func (c AppClient) Watch(namespace string, pts metav1.ListOptions) (watch.Interface, error) {
+	panic("implement me")
+}
+
+func (c AppClient) Patch(namespace, name string, pt types.PatchType, data []byte, subresources ...string) (result *catalogv1.App, err error) {
 	panic("implement me")
 }
