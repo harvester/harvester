2a939577749216e67111605cb07acdb00b20f978
Jack Yu <jack.yu@suse.com>
2024-01-03 20:04:08 +0800

    test: add test cases
    Signed-off-by: Jack Yu <jack.yu@suse.com>

---
+++ pkg/util/fakeclients/supportbundle.go
@@ -0,0 +1,110 @@
+package fakeclients
+
+import (
+	"context"
+	"time"
+
+	"github.com/rancher/wrangler/pkg/generic"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/tools/cache"
+
+	harvesterv1 "github.com/harvester/harvester/pkg/apis/harvesterhci.io/v1beta1"
+	harv1type "github.com/harvester/harvester/pkg/generated/clientset/versioned/typed/harvesterhci.io/v1beta1"
+	ctlharvesterv1 "github.com/harvester/harvester/pkg/generated/controllers/harvesterhci.io/v1beta1"
+)
+
+type SupportBundleClient func(string) harv1type.SupportBundleInterface
+
+func (c SupportBundleClient) Update(sb *harvesterv1.SupportBundle) (*harvesterv1.SupportBundle, error) {
+	return c(sb.Namespace).Update(context.TODO(), sb, metav1.UpdateOptions{})
+}
+
+func (c SupportBundleClient) Get(namespace, name string, options metav1.GetOptions) (*harvesterv1.SupportBundle, error) {
+	return c(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+}
+
+func (c SupportBundleClient) Create(sb *harvesterv1.SupportBundle) (*harvesterv1.SupportBundle, error) {
+	return c(sb.Namespace).Create(context.TODO(), sb, metav1.CreateOptions{})
+}
+
+func (c SupportBundleClient) Delete(namespace, name string, options *metav1.DeleteOptions) error {
+	return c(namespace).Delete(context.TODO(), name, metav1.DeleteOptions{})
+}
+
+func (c SupportBundleClient) List(namespace string, opts metav1.ListOptions) (*harvesterv1.SupportBundleList, error) {
+	panic("implement me")
+}
+
+func (c SupportBundleClient) UpdateStatus(*harvesterv1.SupportBundle) (*harvesterv1.SupportBundle, error) {
+	panic("implement me")
+}
+
+func (c SupportBundleClient) Watch(namespace string, opts metav1.ListOptions) (watch.Interface, error) {
+	panic("implement me")
+}
+
+func (c SupportBundleClient) Patch(namespace, name string, pt types.PatchType, data []byte, subresources ...string) (result *harvesterv1.SupportBundle, err error) {
+	panic("implement me")
+}
+
+func (c SupportBundleClient) Informer() cache.SharedIndexInformer {
+	panic("implement me")
+}
+
+func (c SupportBundleClient) GroupVersionKind() schema.GroupVersionKind {
+	panic("implement me")
+}
+
+func (c SupportBundleClient) AddGenericHandler(ctx context.Context, name string, handler generic.Handler) {
+	panic("implement me")
+}
+
+func (c SupportBundleClient) AddGenericRemoveHandler(ctx context.Context, name string, handler generic.Handler) {
+	panic("implement me")
+}
+
+func (c SupportBundleClient) Updater() generic.Updater {
+	panic("implement me")
+}
+
+func (c SupportBundleClient) OnChange(ctx context.Context, name string, sync ctlharvesterv1.SupportBundleHandler) {
+	panic("implement me")
+}
+
+func (c SupportBundleClient) OnRemove(ctx context.Context, name string, sync ctlharvesterv1.SupportBundleHandler) {
+	panic("implement me")
+}
+
+func (c SupportBundleClient) Cache() ctlharvesterv1.SupportBundleCache {
+	panic("implement me")
+}
+
+func (c SupportBundleClient) Enqueue(namespace, name string) {
+	panic("implement me")
+}
+
+func (c SupportBundleClient) EnqueueAfter(namespace, name string, duration time.Duration) {
+	// do nothing
+}
+
+type SupportBundleCache func(string) harv1type.SupportBundleInterface
+
+func (c SupportBundleCache) Get(namespace, name string) (*harvesterv1.SupportBundle, error) {
+	return c(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+}
+
+func (c SupportBundleCache) List(namespace string, selector labels.Selector) ([]*harvesterv1.SupportBundle, error) {
+	panic("implement me")
+}
+
+func (c SupportBundleCache) AddIndexer(indexName string, indexer ctlharvesterv1.SupportBundleIndexer) {
+	panic("implement me")
+}
+
+func (c SupportBundleCache) GetByIndex(indexName, key string) ([]*harvesterv1.SupportBundle, error) {
+	panic("implement me")
+}
