eb850113c8bf686e3ab557d69f762d2674321505
Zespre Chang <zespre.chang@suse.com>
2024-02-01 15:37:47 +0800

    Enhance non-live migratable VMs detection in both webhook and upgrade script (#4984)
    * feat(upgrade): vm live-migratability detection for upgrades
    
    - Add a command-line tool to detect non-live-migrable VMs, replacing the current script-based implementation in the upgrade scripts.
    - Validate upgrade object during creation, reject the request if there are any non-live migratable VMs running.
    - Remove relevant code in the node-upgrade script as its functionality is replaced by the aforementioned Golang code
    
    Signed-off-by: Zespre Chang <zespre.chang@suse.com>

---
+++ cmd/upgradehelper/vmlivemigratedetector.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	"github.com/harvester/harvester/pkg/upgradehelper/vmlivemigratedetector"
+)
+
+var (
+	shutdown bool
+)
+
+var vmLiveMigrateDetectorCmd = &cobra.Command{
+	Use:   "vm-live-migrate-detector NODENAME",
+	Short: "VM Live Migrate Detector",
+	Long: `A simple VM detector and executor for Harvester upgrades
+
+The detector accepts a node name and inferences the possible places the VMs on top of it could be live migrated to.
+If there is no place to go, it can optionally shut down the VMs.
+	`,
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := context.Context(context.Background())
+		options := vmlivemigratedetector.DetectorOptions{
+			KubeConfigPath: kubeConfigPath,
+			KubeContext:    kubeContext,
+			Shutdown:       shutdown,
+			NodeName:       args[0],
+		}
+		if err := run(ctx, options); err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	vmLiveMigrateDetectorCmd.Flags().BoolVar(&shutdown, "shutdown", false, "Shutdown non-migratable VMs")
+
+	rootCmd.AddCommand(vmLiveMigrateDetectorCmd)
+}
+
+func run(ctx context.Context, options vmlivemigratedetector.DetectorOptions) error {
+	logrus.Info("Starting VM Live Migrate Detector")
+	detector := vmlivemigratedetector.NewVMLiveMigrateDetector(options)
+	if err := detector.Init(); err != nil {
+		return err
+	}
+	return detector.Run(ctx)
+}
