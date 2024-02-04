24ffef4026d44bd83eca1a999bc78196f47c997c
Kiefer Chang <kiefer.chang@suse.com>
2024-02-04 16:36:28 +0800

    chore: remove unused codes upgrade codes
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>
    (cherry picked from commit 05c46cc4fe5102bfeecd660464f2ec47c492c9f8)

--- pkg/controller/master/upgrade/pod_controller.go
+++ pkg/controller/master/upgrade/pod_controller.go
@@ -1,12 +1,8 @@
 package upgrade
 
 import (
-	"reflect"
-
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/labels"
 
-	harvesterv1 "github.com/harvester/harvester/pkg/apis/harvesterhci.io/v1beta1"
 	ctlharvesterv1 "github.com/harvester/harvester/pkg/generated/controllers/harvesterhci.io/v1beta1"
 	upgradev1 "github.com/harvester/harvester/pkg/generated/controllers/upgrade.cattle.io/v1"
 )
@@ -43,67 +39,6 @@ func (h *podHandler) OnChanged(key string, pod *v1.Pod) (*v1.Pod, error) {
 				_, err = h.upgradeClient.Update(toUpdate)
 				return pod, err
 			}
-		}
-	}
-
-	return pod, nil
-}
-
-func (h *podHandler) syncHelmChartPod(pod *v1.Pod) (*v1.Pod, error) {
-	if pod.Status.Phase == v1.PodSucceeded {
-		return pod, nil
-	}
-
-	sets := labels.Set{
-		harvesterLatestUpgradeLabel: "true",
-	}
-	onGoingUpgrades, err := h.upgradeCache.List(h.namespace, sets.AsSelector())
-	if err != nil {
-		return pod, err
-	}
-	if len(onGoingUpgrades) == 0 {
-		return pod, nil
-	}
-	upgrade := onGoingUpgrades[0]
-	if !harvesterv1.SystemServicesUpgraded.IsUnknown(upgrade) {
-		return pod, nil
-	}
-	toUpdate := upgrade.DeepCopy()
-
-	reason, message := getPodWaitingStatus(pod)
-	setHelmChartUpgradeStatus(toUpdate, v1.ConditionUnknown, reason, message)
-
-	if !reflect.DeepEqual(upgrade, toUpdate) {
-		if _, err := h.upgradeClient.Update(toUpdate); err != nil {
-			return pod, err
-		}
-	}
-	return pod, nil
-}
-
-func (h *podHandler) syncNodeUpgradePod(pod *v1.Pod, planName string, nodeName string) (*v1.Pod, error) {
-	if pod.Status.Phase == v1.PodSucceeded {
-		return pod, nil
-	}
-	plan, err := h.planCache.Get(upgradeNamespace, planName)
-	if err != nil {
-		return pod, err
-	}
-	upgradeName, ok := plan.Labels[harvesterUpgradeLabel]
-	if !ok {
-		return pod, nil
-	}
-	upgrade, err := h.upgradeCache.Get(h.namespace, upgradeName)
-	if err != nil {
-		return pod, err
-	}
-	toUpdate := upgrade.DeepCopy()
-
-	reason, message := getPodWaitingStatus(pod)
-	setNodeUpgradeStatus(toUpdate, nodeName, StateUpgrading, reason, message)
-	if !reflect.DeepEqual(upgrade, toUpdate) {
-		if _, err := h.upgradeClient.Update(toUpdate); err != nil {
-			return pod, err
 		}
 	}
 
