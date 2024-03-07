f0f79380c085ccb2ac5c6dafcc6992cf61a70dbb
Volker Theile <vtheile@suse.com>
2024-03-07 14:25:19 +0100

    Improving Harvester Maintenance Mode
    Introduce the label `harvesterhci.io/maintain-mode-strategy` which can be added to a `VirtualMachine` resource to specify the behaviour of the VM in case of a node maintenance event. The following modes are supported:
    
    - `Migrate` (default): The VM will be live-migrated to another node in the cluster. This is the behavior of the maintenance feature as it is at the moment and will be default with this enhancement.
    - `ShutdownAndRestartAfterEnable`: Shut down and restart the VM after maintenance mode is enabled. The VM will be scheduled on a different node.
    - `ShutdownAndRestartAfterDisable`: Shut down when maintenance mode is enabled and restart the VM after maintenance mode is disabled. The VM will stay on the same node.
    - `Shutdown`: Shut down when maintenance mode is enabled. Do NOT restart the VM, it remains switched off.
    
    If the `Force` checkbox is selected in the maintenance dialog, a configured maintenance mode strategy of a VM is overridden and it is shut down and remains off regardless of the configured strategy.
    For all other VMs that do not have the label `harvesterhci.io/maintain-mode-strategy`, the current behavior of the forced maintenance mode is applied.
    
    Related to: https://github.com/harvester/harvester/issues/5069
    HEP: https://github.com/harvester/harvester/pull/5984
    Docs: https://github.com/harvester/docs/pull/523
    
    Signed-off-by: Volker Theile <vtheile@suse.com>
    (cherry picked from commit d4166eb2eb09884b3d68f753aab736a4f1d5743d)

--- pkg/util/drainhelper/helper.go
+++ pkg/util/drainhelper/helper.go
@@ -17,6 +17,7 @@ import (
 	"k8s.io/kubectl/pkg/drain"
 
 	ctlnode "github.com/harvester/harvester/pkg/controller/master/node"
+	"github.com/harvester/harvester/pkg/util"
 )
 
 // drain helper leverages k8s.io/kubectl/pkg/drain to evict pods while following longhorn
@@ -57,6 +58,7 @@ func defaultDrainHelper(ctx context.Context, cfg *rest.Config) (*drain.Helper, e
 		Out:                 logger.Writer(),
 		ErrOut:              logger.Writer(),
 		Timeout:             defaultTimeOut,
+		AdditionalFilters:   []drain.PodFilter{maintainModeStrategyFilter},
 	}, nil
 }
 
@@ -139,4 +141,19 @@ func DrainPossible(nodeCache ctlcorev1.NodeCache, node *corev1.Node) error {
 	}
 
 	return nil
+}
+
+func maintainModeStrategyFilter(pod corev1.Pod) drain.PodDeleteStatus {
+	// Ignore VMs that should not be migrated in maintenance mode. These
+	// VMs are forcibly shut down when maintenance mode is activated.
+	value, ok := pod.Labels[util.LabelMaintainModeStrategy]
+	if ok && value != util.MaintainModeStrategyMigrate {
+		logrus.WithFields(logrus.Fields{
+			"namespace": pod.Namespace,
+			"pod_name":  pod.Name,
+		}).Infof("migration of pod owned by VM %s is skipped because of the label %s",
+			pod.Labels[util.LabelVMName], util.LabelMaintainModeStrategy)
+		return drain.MakePodDeleteStatusSkip()
+	}
+	return drain.MakePodDeleteStatusOkay()
 }
