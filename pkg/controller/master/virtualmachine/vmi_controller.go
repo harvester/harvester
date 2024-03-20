07e59609f3011ed8e9359bcdd1e72efab9e44939
PoAn Yang <poan.yang@suse.com>
2024-03-20 17:57:50 +0800

    feat: remove harvesterhci.io/owned-by
    Signed-off-by: PoAn Yang <poan.yang@suse.com>

--- pkg/controller/master/virtualmachine/vmi_controller.go
+++ pkg/controller/master/virtualmachine/vmi_controller.go
@@ -9,13 +9,12 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/tools/record"
 	kubevirtv1 "kubevirt.io/api/core/v1"
 
 	"github.com/harvester/harvester/pkg/builder"
 	kubevirtctrl "github.com/harvester/harvester/pkg/generated/controllers/kubevirt.io/v1"
+	"github.com/harvester/harvester/pkg/util"
 )
 
 const (
@@ -33,82 +32,7 @@ type VMIController struct {
 	vmiClient           kubevirtctrl.VirtualMachineInstanceClient
 	nodeCache           ctlcorev1.NodeCache
 	pvcClient           ctlcorev1.PersistentVolumeClaimClient
-	pvcCache            ctlcorev1.PersistentVolumeClaimCache
 	recorder            record.EventRecorder
-}
-
-// UnsetOwnerOfPVCs erases the target VirtualMachine from the owner of the PVCs in annotation.
-//
-// When modifying the VirtualMachine's spec to remove the previously defined PVCs and recreating the VirtualMachineInstance,
-// those previously defined PVCs still hold an OwnerReference of the VirtualMachine,
-// but they are no longer used by the newly created VirtualMachineInstance.
-//
-// Since the handler of VMController has recorded the relationship in the PVC's annotation,
-// this handler will erase the target owner from the PVC's annotation to avoid logic leak.
-func (h *VMIController) UnsetOwnerOfPVCs(_ string, vmi *kubevirtv1.VirtualMachineInstance) (*kubevirtv1.VirtualMachineInstance, error) {
-	if vmi == nil || vmi.DeletionTimestamp == nil {
-		return vmi, nil
-	}
-
-	// NB(thxCode): validate the VirtualMachineInstance's OwnerReference of a VirtualMachine,
-	// and process only when it exists.
-	var vmReferred = metav1.GetControllerOfNoCopy(vmi)
-	if vmReferred == nil {
-		// doesn't process ownerless VirtualMachineInstance
-		return vmi, nil
-	}
-	var vmGVK = kubevirtv1.VirtualMachineGroupVersionKind
-	if vmReferred.APIVersion != vmGVK.GroupVersion().String() ||
-		vmReferred.Kind != vmGVK.Kind {
-		// doesn't process the VirtualMachineInstance that didn't own by VirtualMachine
-		return vmi, nil
-	}
-
-	var vm, err = h.virtualMachineCache.Get(vmi.Namespace, vmReferred.Name)
-	if err != nil {
-		if apierrors.IsNotFound(err) {
-			// NB(thxCode): in this case, the related VirtualMachine is deleted,
-			// which triggers the cascading deletion of the VirtualMachineInstance.
-			// however, the deletion of the VirtualMachineInstance is placed in the background,
-			// the VirtualMachineInstance is still deleting, but the corresponding VirtualMachine has been deleted.
-			// so we can ignores this error.
-			return vmi, nil
-		}
-		return vmi, fmt.Errorf("failed to get VirtualMachine referred by VirtualMachineInstance(%s/%s): %w", vmi.Namespace, vmi.Name, err)
-	}
-	if vm.DeletionTimestamp != nil {
-		// reconciling executed by the VirtualMachine controller and don't process in here.
-		return vmi, nil
-	}
-
-	var pvcNames = sets.String{}
-	if vmiDesired := vm.Spec.Template; vmiDesired != nil { // just a defend, the validating webhook of virt-api will make this never happen
-		pvcNames = getPVCNames(&vmiDesired.Spec)
-	}
-	var pvcNameObserved = getPVCNames(&vmi.Spec)
-
-	// unsets ownerless PVCs
-	var pvcNamespace = vmi.Namespace
-	var ownerlessPVCNames = pvcNameObserved.Difference(pvcNames).List()
-	for _, pvcName := range ownerlessPVCNames {
-		var pvc, err = h.pvcCache.Get(pvcNamespace, pvcName)
-		if err != nil {
-			if apierrors.IsNotFound(err) {
-				// NB(thxCode): ignores error, since this can't be fixed by an immediate requeue,
-				// and also doesn't block the whole logic if the PVC has already been deleted.
-				continue
-			}
-			return vmi, fmt.Errorf("failed to get PVC(%s/%s): %w", pvcNamespace, pvcName, err)
-		}
-
-		err = unsetBoundedPVCReference(h.pvcClient, pvc, vm)
-		if err != nil {
-			return vmi, fmt.Errorf("failed to revoke VitrualMachine(%s/%s) as PVC(%s/%s)'s owner: %w",
-				vm.Namespace, vm.Name, pvcNamespace, pvcName, err)
-		}
-	}
-
-	return vmi, nil
 }
 
 // ReconcileFromHostLabels handles the propagation of metadata from node labels to VirtualMachineInstance annotations.
@@ -171,4 +95,17 @@ func (h *VMIController) StopVMIfExceededQuota(_ string, vmi *kubevirtv1.VirtualM
 
 func (h *VMIController) stopVM(vm *kubevirtv1.VirtualMachine, errMsg string) error {
 	return stopVM(h.vmClient, h.recorder, vm, errMsg)
+}
+
+// removeDeprecatedFinalizer remove deprecated finalizer, so removing vm will not be blocked
+func (h *VMIController) removeDeprecatedFinalizer(_ string, vmi *kubevirtv1.VirtualMachineInstance) (*kubevirtv1.VirtualMachineInstance, error) {
+	if vmi == nil {
+		return vmi, nil
+	}
+	vmiObj := vmi.DeepCopy()
+	util.RemoveFinalizer(vmiObj, util.GetWranglerFinalizerName(deprecatedVMIUnsetOwnerOfPVCsFinalizer))
+	if !reflect.DeepEqual(vmi, vmiObj) {
+		return h.vmiClient.Update(vmiObj)
+	}
+	return vmi, nil
 }
