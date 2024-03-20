46590c9c0b47d8aedd737b54ecb044695736e9cd
PoAn Yang <poan.yang@suse.com>
2024-03-20 17:57:50 +0800

    feat: remove harvesterhci.io/owned-by
    Signed-off-by: PoAn Yang <poan.yang@suse.com>
    (cherry picked from commit 07e59609f3011ed8e9359bcdd1e72efab9e44939)

--- pkg/controller/master/virtualmachine/vm_controller.go
+++ pkg/controller/master/virtualmachine/vm_controller.go
@@ -8,12 +8,10 @@ import (
 	"time"
 
 	v1 "github.com/rancher/wrangler/pkg/generated/controllers/core/v1"
-	"github.com/rancher/wrangler/pkg/slice"
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/tools/record"
 	kubevirt "kubevirt.io/api/core"
 	kubevirtv1 "kubevirt.io/api/core/v1"
@@ -23,7 +21,6 @@ import (
 	ctlkubevirtv1 "github.com/harvester/harvester/pkg/generated/controllers/kubevirt.io/v1"
 	ctlsnapshotv1 "github.com/harvester/harvester/pkg/generated/controllers/snapshot.storage.k8s.io/v1"
 	"github.com/harvester/harvester/pkg/indexeres"
-	"github.com/harvester/harvester/pkg/ref"
 	"github.com/harvester/harvester/pkg/util"
 	rqutils "github.com/harvester/harvester/pkg/util/resourcequota"
 )
@@ -93,79 +90,6 @@ func (h *VMController) createPVCsFromAnnotation(_ string, vm *kubevirtv1.Virtual
 	}
 
 	return nil, nil
-}
-
-// SetOwnerOfPVCs records the target VirtualMachine as the owner of the PVCs in annotation.
-func (h *VMController) SetOwnerOfPVCs(vm *kubevirtv1.VirtualMachine) (*kubevirtv1.VirtualMachine, error) {
-	// add harvester finalizers
-	if !util.ContainsFinalizer(vm, harvesterUnsetOwnerOfPVCsFinalizer) {
-		vmCopy := vm.DeepCopy()
-		util.AddFinalizer(vmCopy, harvesterUnsetOwnerOfPVCsFinalizer)
-		return h.vmClient.Update(vmCopy)
-	}
-
-	pvcNames := getPVCNames(&vm.Spec.Template.Spec)
-
-	vmReferenceKey := ref.Construct(vm.Namespace, vm.Name)
-	// get all the volumes attached to this virtual machine
-	attachedPVCs, err := h.pvcCache.GetByIndex(indexeres.PVCByVMIndex, vmReferenceKey)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get attached PVCs by VM index: %w", err)
-	}
-
-	vmGVK := kubevirtv1.VirtualMachineGroupVersionKind
-	vmGK := vmGVK.GroupKind()
-
-	for _, attachedPVC := range attachedPVCs {
-		// check if it's still attached
-		if pvcNames.Has(attachedPVC.Name) {
-			continue
-		}
-
-		toUpdate := attachedPVC.DeepCopy()
-
-		// if this volume is no longer attached
-		// remove volume's annotation
-		owners, err := ref.GetSchemaOwnersFromAnnotation(attachedPVC)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get schema owners from annotation: %w", err)
-		}
-
-		isAttached := owners.Remove(vmGK, vm)
-		if isAttached {
-			if err := owners.Bind(toUpdate); err != nil {
-				return nil, fmt.Errorf("failed to apply schema owners to annotation: %w", err)
-			}
-		}
-
-		// update volume
-		if isAttached {
-			if _, err = h.pvcClient.Update(toUpdate); err != nil {
-				return nil, fmt.Errorf("failed to clean schema owners for PVC(%s/%s): %w",
-					attachedPVC.Namespace, attachedPVC.Name, err)
-			}
-		}
-	}
-
-	var pvcNamespace = vm.Namespace
-	for _, pvcName := range pvcNames.List() {
-		var pvc, err = h.pvcCache.Get(pvcNamespace, pvcName)
-		if err != nil {
-			if apierrors.IsNotFound(err) {
-				// ignores not-found error, A VM can reference a non-existent PVC.
-				continue
-			}
-			return vm, fmt.Errorf("failed to get PVC(%s/%s): %w", pvcNamespace, pvcName, err)
-		}
-
-		err = setOwnerlessPVCReference(h.pvcClient, pvc, vm)
-		if err != nil {
-			return vm, fmt.Errorf("failed to grant VitrualMachine(%s/%s) as PVC(%s/%s)'s owner: %w",
-				vm.Namespace, vm.Name, pvcNamespace, pvcName, err)
-		}
-	}
-
-	return vm, nil
 }
 
 // SyncLabelsToVmi synchronizes the labels in the VM spec to the existing VMI without re-deployment
@@ -257,61 +181,38 @@ func (h *VMController) StoreRunStrategy(_ string, vm *kubevirtv1.VirtualMachine)
 	return vm, nil
 }
 
-// OnVMRemove handle related PVC and delete related VMBackup snapshot
-func (h *VMController) OnVMRemove(vm *kubevirtv1.VirtualMachine) (*kubevirtv1.VirtualMachine, error) {
-
-	if err := h.unsetOwnerOfPVCs(vm); err != nil {
-		return vm, err
+// cleanupPVCAndSnapshot handle related PVC and delete related VMBackup snapshot
+func (h *VMController) cleanupPVCAndSnapshot(_ string, vm *kubevirtv1.VirtualMachine) (*kubevirtv1.VirtualMachine, error) {
+	if vm == nil {
+		return vm, nil
 	}
 
 	if err := h.removeVMBackupSnapshot(vm); err != nil {
 		return vm, err
 	}
 
-	// remove harvester finalizers
-	vmObj := vm.DeepCopy()
-	util.RemoveFinalizer(vmObj, harvesterUnsetOwnerOfPVCsFinalizer)
-	util.RemoveFinalizer(vmObj, oldWranglerFinalizer) // post upgrade, these are not being cleaned by wrangler so need to be managed by us
-	return h.vmClient.Update(vmObj)
-}
-
-// unsetOwnerOfPVCs erases the target VirtualMachine from the owner of the PVCs in annotation.
-func (h *VMController) unsetOwnerOfPVCs(vm *kubevirtv1.VirtualMachine) error {
-	var (
-		pvcNamespace = vm.Namespace
-		pvcNames     = getPVCNames(&vm.Spec.Template.Spec)
-		removedPVCs  = getRemovedPVCs(vm)
-	)
-	for _, pvcName := range pvcNames.List() {
-		var pvc, err = h.pvcCache.Get(pvcNamespace, pvcName)
-		if err != nil {
-			if apierrors.IsNotFound(err) {
-				// NB(thxCode): ignores error, since this can't be fixed by an immediate requeue,
-				// and also doesn't block the whole logic if the PVC has already been deleted.
-				continue
-			}
-			return fmt.Errorf("failed to get PVC(%s/%s): %w", pvcNamespace, pvcName, err)
-		}
-
-		if err := unsetBoundedPVCReference(h.pvcClient, pvc, vm); err != nil {
-			return fmt.Errorf("failed to revoke VitrualMachine(%s/%s) as PVC(%s/%s)'s owner: %w",
-				vm.Namespace, vm.Name, pvcNamespace, pvcName, err)
-		}
-
-		if slice.ContainsString(removedPVCs, pvcName) {
-			numberOfOwner, err := numberOfBoundedPVCReference(pvc)
-			if err != nil {
-				return fmt.Errorf("failed to count number of owners for PVC(%s/%s): %w", pvcNamespace, pvcName, err)
-			}
-			// We are the solely owner here, so try to delete the PVC as requested.
-			if numberOfOwner == 1 {
-				if err := h.pvcClient.Delete(pvcNamespace, pvcName, &metav1.DeleteOptions{}); err != nil {
-					return err
-				}
-			}
-		}
+	if err := h.removePVCInRemovedPVCsAnnotation(vm); err != nil {
+		return vm, err
 	}
 
+	return vm, nil
+}
+
+// removePVCInRemovedPVCsAnnotation cleanup PVCs which users want to remove with the VM.
+func (h *VMController) removePVCInRemovedPVCsAnnotation(vm *kubevirtv1.VirtualMachine) error {
+	for _, pvcName := range getRemovedPVCs(vm) {
+		pvcName = strings.TrimSpace(pvcName)
+		if pvcName == "" {
+			continue
+		}
+
+		if err := h.pvcClient.Delete(vm.Namespace, pvcName, &metav1.DeleteOptions{}); err != nil {
+			if apierrors.IsNotFound(err) {
+				continue
+			}
+			return fmt.Errorf("can't delete PVC %s/%s, err: %w", vm.Namespace, pvcName, err)
+		}
+	}
 	return nil
 }
 
@@ -364,31 +265,6 @@ func (h *VMController) removeVMBackupSnapshot(vm *kubevirtv1.VirtualMachine) err
 // getRemovedPVCs returns removed PVCs.
 func getRemovedPVCs(vm *kubevirtv1.VirtualMachine) []string {
 	return strings.Split(vm.Annotations[util.RemovedPVCsAnnotationKey], ",")
-}
-
-// getPVCNames returns a name set of the PVCs used by the VMI.
-func getPVCNames(vmiSpecPtr *kubevirtv1.VirtualMachineInstanceSpec) sets.String {
-	var pvcNames = sets.String{}
-
-	for _, volume := range vmiSpecPtr.Volumes {
-		if volume.PersistentVolumeClaim != nil && volume.PersistentVolumeClaim.ClaimName != "" {
-			pvcNames.Insert(volume.PersistentVolumeClaim.ClaimName)
-		}
-	}
-
-	return pvcNames
-}
-
-func (h *VMController) ManageOwnerOfPVCs(_ string, vm *kubevirtv1.VirtualMachine) (*kubevirtv1.VirtualMachine, error) {
-	if vm == nil || vm.Spec.Template == nil {
-		return vm, nil
-	}
-
-	if vm.DeletionTimestamp == nil {
-		return h.SetOwnerOfPVCs(vm)
-	}
-
-	return h.OnVMRemove(vm)
 }
 
 func (h *VMController) SetHaltIfInsufficientResourceQuota(_ string, vm *kubevirtv1.VirtualMachine) (*kubevirtv1.VirtualMachine, error) {
@@ -471,4 +347,18 @@ func stopVM(vms ctlkubevirtv1.VirtualMachineClient, recorder record.EventRecorde
 
 	recorder.Eventf(vm, corev1.EventTypeWarning, "InsufficientResourceQuota", "Set runStrategy to %s: %s", kubevirtv1.RunStrategyHalted, errMsg)
 	return nil
+}
+
+// removeDeprecatedFinalizer remove deprecated finalizer, so removing vm will not be blocked
+func (h *VMController) removeDeprecatedFinalizer(_ string, vm *kubevirtv1.VirtualMachine) (*kubevirtv1.VirtualMachine, error) {
+	if vm == nil {
+		return vm, nil
+	}
+	vmObj := vm.DeepCopy()
+	util.RemoveFinalizer(vmObj, deprecatedHarvesterUnsetOwnerOfPVCsFinalizer)
+	util.RemoveFinalizer(vmObj, util.GetWranglerFinalizerName(deprecatedVMUnsetOwnerOfPVCsFinalizer))
+	if !reflect.DeepEqual(vm, vmObj) {
+		return h.vmClient.Update(vmObj)
+	}
+	return vm, nil
 }
