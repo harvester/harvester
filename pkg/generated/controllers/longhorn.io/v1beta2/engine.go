6feaa9471a07b4446ee146b140d43c921e53853c
PoAn Yang <poan.yang@suse.com>
2024-05-21 16:27:38 +0800

    feat: regenerate for longhorn v1.6.2-rc2
    Signed-off-by: PoAn Yang <poan.yang@suse.com>
    (cherry picked from commit 8126f1e3109759ac312a8a332e73c771995731fb)

--- pkg/generated/controllers/longhorn.io/v1beta2/engine.go
+++ pkg/generated/controllers/longhorn.io/v1beta2/engine.go
@@ -20,6 +20,7 @@ package v1beta2
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	v1beta2 "github.com/longhorn/longhorn-manager/k8s/pkg/apis/longhorn/v1beta2"
@@ -263,10 +264,14 @@ func (c *engineCache) GetByIndex(indexName, key string) (result []*v1beta2.Engin
 	return result, nil
 }
 
+// EngineStatusHandler is executed for every added or modified Engine. Should return the new status to be updated
 type EngineStatusHandler func(obj *v1beta2.Engine, status v1beta2.EngineStatus) (v1beta2.EngineStatus, error)
 
+// EngineGeneratingHandler is the top-level handler that is executed for every Engine event. It extends EngineStatusHandler by a returning a slice of child objects to be passed to apply.Apply
 type EngineGeneratingHandler func(obj *v1beta2.Engine, status v1beta2.EngineStatus) ([]runtime.Object, v1beta2.EngineStatus, error)
 
+// RegisterEngineStatusHandler configures a EngineController to execute a EngineStatusHandler for every events observed.
+// If a non-empty condition is provided, it will be updated in the status conditions for every handler execution
 func RegisterEngineStatusHandler(ctx context.Context, controller EngineController, condition condition.Cond, name string, handler EngineStatusHandler) {
 	statusHandler := &engineStatusHandler{
 		client:    controller,
@@ -276,6 +281,8 @@ func RegisterEngineStatusHandler(ctx context.Context, controller EngineControlle
 	controller.AddGenericHandler(ctx, name, FromEngineHandlerToHandler(statusHandler.sync))
 }
 
+// RegisterEngineGeneratingHandler configures a EngineController to execute a EngineGeneratingHandler for every events observed, passing the returned objects to the provided apply.Apply.
+// If a non-empty condition is provided, it will be updated in the status conditions for every handler execution
 func RegisterEngineGeneratingHandler(ctx context.Context, controller EngineController, apply apply.Apply,
 	condition condition.Cond, name string, handler EngineGeneratingHandler, opts *generic.GeneratingHandlerOptions) {
 	statusHandler := &engineGeneratingHandler{
@@ -297,6 +304,7 @@ type engineStatusHandler struct {
 	handler   EngineStatusHandler
 }
 
+// sync is executed on every resource addition or modification. Executes the configured handlers and sends the updated status to the Kubernetes API
 func (a *engineStatusHandler) sync(key string, obj *v1beta2.Engine) (*v1beta2.Engine, error) {
 	if obj == nil {
 		return obj, nil
@@ -342,8 +350,10 @@ type engineGeneratingHandler struct {
 	opts  generic.GeneratingHandlerOptions
 	gvk   schema.GroupVersionKind
 	name  string
+	seen  sync.Map
 }
 
+// Remove handles the observed deletion of a resource, cascade deleting every associated resource previously applied
 func (a *engineGeneratingHandler) Remove(key string, obj *v1beta2.Engine) (*v1beta2.Engine, error) {
 	if obj != nil {
 		return obj, nil
@@ -353,12 +363,17 @@ func (a *engineGeneratingHandler) Remove(key string, obj *v1beta2.Engine) (*v1be
 	obj.Namespace, obj.Name = kv.RSplit(key, "/")
 	obj.SetGroupVersionKind(a.gvk)
 
+	if a.opts.UniqueApplyForResourceVersion {
+		a.seen.Delete(key)
+	}
+
 	return nil, generic.ConfigureApplyForObject(a.apply, obj, &a.opts).
 		WithOwner(obj).
 		WithSetID(a.name).
 		ApplyObjects()
 }
 
+// Handle executes the configured EngineGeneratingHandler and pass the resulting objects to apply.Apply, finally returning the new status of the resource
 func (a *engineGeneratingHandler) Handle(obj *v1beta2.Engine, status v1beta2.EngineStatus) (v1beta2.EngineStatus, error) {
 	if !obj.DeletionTimestamp.IsZero() {
 		return status, nil
@@ -368,9 +383,41 @@ func (a *engineGeneratingHandler) Handle(obj *v1beta2.Engine, status v1beta2.Eng
 	if err != nil {
 		return newStatus, err
 	}
+	if !a.isNewResourceVersion(obj) {
+		return newStatus, nil
+	}
 
-	return newStatus, generic.ConfigureApplyForObject(a.apply, obj, &a.opts).
+	err = generic.ConfigureApplyForObject(a.apply, obj, &a.opts).
 		WithOwner(obj).
 		WithSetID(a.name).
 		ApplyObjects(objs...)
+	if err != nil {
+		return newStatus, err
+	}
+	a.storeResourceVersion(obj)
+	return newStatus, nil
+}
+
+// isNewResourceVersion detects if a specific resource version was already successfully processed.
+// Only used if UniqueApplyForResourceVersion is set in generic.GeneratingHandlerOptions
+func (a *engineGeneratingHandler) isNewResourceVersion(obj *v1beta2.Engine) bool {
+	if !a.opts.UniqueApplyForResourceVersion {
+		return true
+	}
+
+	// Apply once per resource version
+	key := obj.Namespace + "/" + obj.Name
+	previous, ok := a.seen.Load(key)
+	return !ok || previous != obj.ResourceVersion
+}
+
+// storeResourceVersion keeps track of the latest resource version of an object for which Apply was executed
+// Only used if UniqueApplyForResourceVersion is set in generic.GeneratingHandlerOptions
+func (a *engineGeneratingHandler) storeResourceVersion(obj *v1beta2.Engine) {
+	if !a.opts.UniqueApplyForResourceVersion {
+		return
+	}
+
+	key := obj.Namespace + "/" + obj.Name
+	a.seen.Store(key, obj.ResourceVersion)
 }
