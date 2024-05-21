cf0b99f299eac03eb7bab54b32e23e1b3bb4f07d
Jack Yu <jack.yu@suse.com>
2024-05-21 12:08:18 +0800

    feat: add support bundle node timeout
    Signed-off-by: Jack Yu <jack.yu@suse.com>
    (cherry picked from commit 1801ed0c9359e208ddea8a8d0e7eae8a837e2f32)

--- pkg/util/supportbundle/constants.go
+++ pkg/util/supportbundle/constants.go
@@ -4,4 +4,8 @@ const (
 	// SupportBundleExpirationDefault and SupportBundleExpirationDefaultStr these two value should be changed together.
 	SupportBundleExpirationDefault    = 30
 	SupportBundleExpirationDefaultStr = "30"
+
+	// SupportBundleNodeTimeoutDefault and SupportBundleNodeTimeoutDefaultStr these two value should be changed together.
+	SupportBundleNodeTimeoutDefault    = 30
+	SupportBundleNodeTimeoutDefaultStr = "30"
 )
