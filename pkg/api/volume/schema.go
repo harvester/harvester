36b21d38f20fe0b05e49dc6dd1838eb8c8aa4285
Jack Yu <jack.yu@suse.com>
2024-07-08 15:42:00 +0800

    feat: exporting volume returns the image data in response body
    - Design a common interface for custom action API
    - Refactor the volume API
    
    Signed-off-by: Jack Yu <jack.yu@suse.com>
    (cherry picked from commit d794ee0894eec1ff99c8d593cb619ec550a4c3a6)

--- pkg/api/volume/schema.go
+++ pkg/api/volume/schema.go
@@ -9,6 +9,7 @@ import (
 	"github.com/rancher/wrangler/v3/pkg/schemas"
 
 	"github.com/harvester/harvester/pkg/config"
+	harvesterServer "github.com/harvester/harvester/pkg/server/http"
 )
 
 const (
@@ -19,7 +20,7 @@ func RegisterSchema(scaled *config.Scaled, server *server.Server, _ config.Optio
 	server.BaseSchemas.MustImportAndCustomize(ExportVolumeInput{}, nil)
 	server.BaseSchemas.MustImportAndCustomize(CloneVolumeInput{}, nil)
 	server.BaseSchemas.MustImportAndCustomize(SnapshotVolumeInput{}, nil)
-	actionHandler := ActionHandler{
+	actionHandler := &ActionHandler{
 		images:      scaled.HarvesterFactory.Harvesterhci().V1beta1().VirtualMachineImage(),
 		pvcs:        scaled.CoreFactory.Core().V1().PersistentVolumeClaim(),
 		pvcCache:    scaled.CoreFactory.Core().V1().PersistentVolumeClaim().Cache(),
@@ -30,6 +31,8 @@ func RegisterSchema(scaled *config.Scaled, server *server.Server, _ config.Optio
 		volumeCache: scaled.LonghornFactory.Longhorn().V1beta2().Volume().Cache(),
 		vmCache:     scaled.VirtFactory.Kubevirt().V1().VirtualMachine().Cache(),
 	}
+
+	handler := harvesterServer.NewHandler(actionHandler)
 
 	t := schema.Template{
 		ID: pvcSchemaID,
@@ -47,10 +50,10 @@ func RegisterSchema(scaled *config.Scaled, server *server.Server, _ config.Optio
 				},
 			}
 			s.ActionHandlers = map[string]http.Handler{
-				actionExport:       &actionHandler,
-				actionCancelExpand: &actionHandler,
-				actionClone:        &actionHandler,
-				actionSnapshot:     &actionHandler,
+				actionExport:       handler,
+				actionCancelExpand: handler,
+				actionClone:        handler,
+				actionSnapshot:     handler,
 			}
 		},
 		Formatter: Formatter,
