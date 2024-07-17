3a95964fe70e3c5ed2412eaccf31e61097952b7f
Webber Huang <webber.huang@suse.com>
2024-07-17 16:56:44 +0800

    [Scheduled VM Backup] Add unit testing
    Signed-off-by: Webber Huang <webber.huang@suse.com>

---
+++ pkg/util/fakeclients/svmbackup.go
@@ -0,0 +1,93 @@
+package fakeclients
+
+import (
+	"context"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/watch"
+
+	harvesterv1beta1 "github.com/harvester/harvester/pkg/apis/harvesterhci.io/v1beta1"
+	harvestertype "github.com/harvester/harvester/pkg/generated/clientset/versioned/typed/harvesterhci.io/v1beta1"
+	harvesterv1ctl "github.com/harvester/harvester/pkg/generated/controllers/harvesterhci.io/v1beta1"
+	"github.com/harvester/harvester/pkg/indexeres"
+	"github.com/harvester/harvester/pkg/ref"
+)
+
+type SVMBackupClient func(string) harvestertype.ScheduleVMBackupInterface
+
+func (c SVMBackupClient) Create(svmBackup *harvesterv1beta1.ScheduleVMBackup) (*harvesterv1beta1.ScheduleVMBackup, error) {
+	return c(svmBackup.Namespace).Create(context.TODO(), svmBackup, metav1.CreateOptions{})
+}
+
+func (c SVMBackupClient) Update(svmBackup *harvesterv1beta1.ScheduleVMBackup) (*harvesterv1beta1.ScheduleVMBackup, error) {
+	return c(svmBackup.Namespace).Update(context.TODO(), svmBackup, metav1.UpdateOptions{})
+}
+
+func (c SVMBackupClient) UpdateStatus(_ *harvesterv1beta1.ScheduleVMBackup) (*harvesterv1beta1.ScheduleVMBackup, error) {
+	panic("implement me")
+}
+
+func (c SVMBackupClient) Delete(namespace, name string, options *metav1.DeleteOptions) error {
+	return c(namespace).Delete(context.TODO(), name, *options)
+}
+
+func (c SVMBackupClient) Get(namespace, name string, options metav1.GetOptions) (*harvesterv1beta1.ScheduleVMBackup, error) {
+	return c(namespace).Get(context.TODO(), name, options)
+}
+
+func (c SVMBackupClient) List(namespace string, opts metav1.ListOptions) (*harvesterv1beta1.ScheduleVMBackupList, error) {
+	return c(namespace).List(context.TODO(), opts)
+}
+
+func (c SVMBackupClient) Watch(namespace string, opts metav1.ListOptions) (watch.Interface, error) {
+	return c(namespace).Watch(context.TODO(), opts)
+}
+
+func (c SVMBackupClient) Patch(namespace, name string, pt types.PatchType, data []byte, subresources ...string) (result *harvesterv1beta1.ScheduleVMBackup, err error) {
+	return c(namespace).Patch(context.TODO(), name, pt, data, metav1.PatchOptions{}, subresources...)
+}
+
+type SVMBackupCache func(string) harvestertype.ScheduleVMBackupInterface
+
+func (c SVMBackupCache) Get(namespace, name string) (*harvesterv1beta1.ScheduleVMBackup, error) {
+	return c(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+}
+
+func (c SVMBackupCache) List(namespace string, selector labels.Selector) ([]*harvesterv1beta1.ScheduleVMBackup, error) {
+	list, err := c(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: selector.String()})
+	if err != nil {
+		return nil, err
+	}
+	result := make([]*harvesterv1beta1.ScheduleVMBackup, 0, len(list.Items))
+	for i := range list.Items {
+		result = append(result, &list.Items[i])
+	}
+	return result, err
+}
+
+func (c SVMBackupCache) AddIndexer(_ string, _ harvesterv1ctl.ScheduleVMBackupIndexer) {
+	panic("implement me")
+}
+
+func (c SVMBackupCache) GetByIndex(indexName, key string) ([]*harvesterv1beta1.ScheduleVMBackup, error) {
+	switch indexName {
+	case indexeres.VMBackupBySourceVMNameIndex:
+		vmNamespace, _ := ref.Parse(key)
+		backupList, err := c(vmNamespace).List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			return nil, err
+		}
+		var backups []*harvesterv1beta1.ScheduleVMBackup
+		for i := range backupList.Items {
+			b := backupList.Items[i]
+			if b.Name == key {
+				backups = append(backups, &b)
+			}
+		}
+		return backups, nil
+	default:
+		return nil, nil
+	}
+}
