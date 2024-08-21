f712324cf733c1a2d61454149a94e8da9f66921f
PoAn Yang <poan.yang@suse.com>
2024-08-21 15:02:22 +0800

    feat: add ResourceQuota in harvesterhci.io group
    Signed-off-by: PoAn Yang <poan.yang@suse.com>
    (cherry picked from commit e10e3c41a1fe36488d5fd359dfcbceacad56888d)

--- pkg/generated/clientset/versioned/typed/harvesterhci.io/v1beta1/harvesterhci.io_client.go
+++ pkg/generated/clientset/versioned/typed/harvesterhci.io/v1beta1/harvesterhci.io_client.go
@@ -31,6 +31,7 @@ type HarvesterhciV1beta1Interface interface {
 	AddonsGetter
 	KeyPairsGetter
 	PreferencesGetter
+	ResourceQuotasGetter
 	SettingsGetter
 	SupportBundlesGetter
 	UpgradesGetter
@@ -58,6 +59,10 @@ func (c *HarvesterhciV1beta1Client) KeyPairs(namespace string) KeyPairInterface 
 
 func (c *HarvesterhciV1beta1Client) Preferences(namespace string) PreferenceInterface {
 	return newPreferences(c, namespace)
+}
+
+func (c *HarvesterhciV1beta1Client) ResourceQuotas(namespace string) ResourceQuotaInterface {
+	return newResourceQuotas(c, namespace)
 }
 
 func (c *HarvesterhciV1beta1Client) Settings() SettingInterface {
