5a7096064216789b7556b9d40bb2d7dec762675b
Jack Yu <jack.yu@suse.com>
2024-08-26 18:30:41 +0800

    test: add test case
    Signed-off-by: Jack Yu <jack.yu@suse.com>
    (cherry picked from commit abd9b8aa536c781db458ae299b2366f165c18b5a)

--- pkg/controller/master/image/vm_image_controller_test.go
+++ pkg/controller/master/image/vm_image_controller_test.go
@@ -1,63 +1,91 @@
 package image
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 	"testing"
 	"time"
 
+	longhorntypes "github.com/longhorn/longhorn-manager/types"
 	"github.com/stretchr/testify/assert"
-	storagev1 "k8s.io/api/storage/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
 	k8sfake "k8s.io/client-go/kubernetes/fake"
 
 	harvesterv1 "github.com/harvester/harvester/pkg/apis/harvesterhci.io/v1beta1"
 	"github.com/harvester/harvester/pkg/generated/clientset/versioned/fake"
+	"github.com/harvester/harvester/pkg/util"
 	"github.com/harvester/harvester/pkg/util/fakeclients"
 )
 
 func TestVMImageHandler_OnChanged(t *testing.T) {
 	type input struct {
-		key   string
-		image *harvesterv1.VirtualMachineImage
-	}
-	type output struct {
-		image *harvesterv1.VirtualMachineImage
-		sc    *storagev1.StorageClass
-		err   error
+		image   *harvesterv1.VirtualMachineImage
+		objects []runtime.Object
 	}
 	var testCases = []struct {
 		name     string
 		given    input
-		expected output
+		expected func(t *testing.T, handler *vmImageHandler, image *harvesterv1.VirtualMachineImage, err error)
 	}{
 		{
-			name: "Test case 2: Image is not nil",
+			name: "Test case 1: Create Encrypted Image",
 			given: input{
-				key: "test-key",
+				objects: []runtime.Object{
+					&harvesterv1.VirtualMachineImage{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "source-image",
+							Namespace: "default",
+						},
+						Spec: harvesterv1.VirtualMachineImageSpec{
+							SourceType:  "download",
+							URL:         "https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-standard-3.20.2-x86_64.iso",
+							DisplayName: "source-image",
+						},
+					},
+				},
 				image: &harvesterv1.VirtualMachineImage{
 					ObjectMeta: metav1.ObjectMeta{
-						Name:      "test-image",
+						Name:      "encrypted-test-image",
 						Namespace: "default",
 					},
 					Spec: harvesterv1.VirtualMachineImageSpec{
-						SourceType:  "download",
-						URL:         "https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-standard-3.20.2-x86_64.iso",
-						DisplayName: "test-image",
+						SourceType: "clone",
+						SecurityParameters: &harvesterv1.VirtualMachineImageSecurityParameters{
+							CryptoOperation:      "encrypt",
+							SourceImageName:      "source-image",
+							SourceImageNamespace: "default",
+						},
+						// After mutator, these parameters are from storage class of annotation
+						// But, we didn't have mutator here, so we just put them here
+						StorageClassParameters: map[string]string{
+							util.LonghornOptionEncrypted:          "true",
+							util.CSIProvisionerSecretNameKey:      "test-secret",
+							util.CSIProvisionerSecretNamespaceKey: "default",
+							util.CSINodeStageSecretNameKey:        "test-secret",
+							util.CSINodeStageSecretNamespaceKey:   "default",
+							util.CSINodePublishSecretNameKey:      "test-secret",
+							util.CSINodePublishSecretNamespaceKey: "default",
+							longhorntypes.OptionNumberOfReplicas:  "1",
+						},
+						DisplayName: "encrypted-test-image",
 					},
 				},
 			},
-			expected: output{
-				image: &harvesterv1.VirtualMachineImage{},
-				err:   nil,
+			expected: func(t *testing.T, handler *vmImageHandler, _ *harvesterv1.VirtualMachineImage, err error) {
+				bis, _ := handler.backingImageCache.List("longhorn-system", labels.Everything())
+				assert.Equal(t, 1, len(bis))
+				assert.Equal(t, "default/encrypted-test-image", bis[0].Annotations[util.AnnotationImageID])
+				assert.Equal(t, "test-secret", bis[0].Spec.SourceParameters["secret"])
+				assert.Equal(t, "default", bis[0].Spec.SourceParameters["secret-namespace"])
+				assert.Nil(t, err)
 			},
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			clientset := fake.NewSimpleClientset(tc.given.image)
+			clientset := fake.NewSimpleClientset(append(tc.given.objects, tc.given.image)...)
 			k8sclientset := k8sfake.NewSimpleClientset()
 
 			handler := &vmImageHandler{
@@ -73,23 +101,9 @@ func TestVMImageHandler_OnChanged(t *testing.T) {
 				pvcCache: fakeclients.PersistentVolumeClaimCache(k8sclientset.CoreV1().PersistentVolumeClaims),
 			}
 
-			actualImage, actualErr := handler.OnChanged(tc.given.key, tc.given.image)
+			image, err := handler.OnChanged("", tc.given.image)
 
-			scs, _ := k8sclientset.StorageV1().StorageClasses().List(context.TODO(), metav1.ListOptions{})
-			for _, sc := range scs.Items {
-				fmt.Println(sc.Name)
-				fmt.Println(sc.Parameters)
-			}
-
-			bis, _ := clientset.LonghornV1beta2().BackingImages("default").List(context.TODO(), metav1.ListOptions{})
-			for _, bi := range bis.Items {
-				fmt.Println(bi.Name)
-				fmt.Println(bi.Spec.SourceParameters)
-				fmt.Println(bi.Spec.SourceType)
-			}
-
-			assert.Equal(t, tc.expected.image, actualImage)
-			assert.Equal(t, tc.expected.err, actualErr)
+			tc.expected(t, handler, image, err)
 		})
 	}
 }
