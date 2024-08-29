02cb340e4fb97a3629e8eb4fb31b05561c7fb8c9
PoAn Yang <poan.yang@suse.com>
2024-08-29 18:35:41 +0800

    feat: remove utility functions for conditions
    Signed-off-by: PoAn Yang <poan.yang@suse.com>

--- pkg/webhook/resources/virtualmachinebackup/validator.go
+++ pkg/webhook/resources/virtualmachinebackup/validator.go
@@ -6,7 +6,6 @@ import (
 	ctlcorev1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/core/v1"
 	ctlstoragev1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/storage/v1"
 	admissionregv1 "k8s.io/api/admissionregistration/v1"
-	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	kubevirtv1 "kubevirt.io/api/core/v1"
@@ -86,7 +85,7 @@ func (v *virtualMachineBackupValidator) Create(_ *types.Request, newObj runtime.
 	}
 
 	validateFunc := v.validateStandardBackup
-	if newVMBackup.Status != nil {
+	if newVMBackup.Status.SourceSpec != nil {
 		validateFunc = v.validateVMBackupRecover
 	}
 
@@ -204,16 +203,12 @@ func (v *virtualMachineBackupValidator) Delete(_ *types.Request, obj runtime.Obj
 		return fmt.Errorf("can't get vmrestores from index %s with vmbackup %s/%s, err: %w", indexeres.VMRestoreByVMBackupNamespaceAndName, vmBackup.Namespace, vmBackup.Name, err)
 	}
 	for _, vmRestore := range vmRestores {
-		if vmRestore.DeletionTimestamp != nil || vmRestore.Status == nil {
+		if vmRestore.DeletionTimestamp != nil {
 			continue
 		}
-		for _, cond := range vmRestore.Status.Conditions {
-			// we use the same condition for backup and restore
-			if cond.Type == v1beta1.BackupConditionProgressing {
-				if cond.Status == corev1.ConditionTrue {
-					return fmt.Errorf("vmrestore %s/%s is in progress", vmRestore.Namespace, vmRestore.Name)
-				}
-			}
+		// we use the same condition for backup and restore
+		if v1beta1.BackupConditionProgressing.IsTrue(vmRestore) {
+			return fmt.Errorf("vmrestore %s/%s is in progress", vmRestore.Namespace, vmRestore.Name)
 		}
 	}
 	return nil
