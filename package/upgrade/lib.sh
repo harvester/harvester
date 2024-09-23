0bde44193ce1c2b2e2787237a2b31eb2263fc756
Jian Wang <jian.wang@suse.com>
2024-09-23 15:24:18 +0200

    In upgrade path bump eventrouter to v0.3.2
    Signed-off-by: Jian Wang <jian.wang@suse.com>

--- package/upgrade/lib.sh
+++ package/upgrade/lib.sh
@@ -517,8 +517,8 @@ upgrade_addon_rancher_logging_with_patch_eventrouter_image()
   if [ $EXIT_CODE != 0 ]; then
     echo "eventrouter is not found, need not patch"
   else
-    if [[ "rancher/harvester-eventrouter:v0.3.1" > $tag ]]; then
-      echo "eventrouter image is $tag, will patch to v0.3.1"
+    if [[ "rancher/harvester-eventrouter:v0.3.2" > $tag ]]; then
+      echo "eventrouter image is $tag, will patch to v0.3.2"
       fixeventrouter=true
     else
       echo "eventrouter image is updated, need not patch"
@@ -536,7 +536,7 @@ upgrade_addon_rancher_logging_with_patch_eventrouter_image()
   cat $valuesfile
 
   if [[ $fixeventrouter == true ]]; then
-    yq -e '.eventTailer.workloadOverrides.containers[0].image = "rancher/harvester-eventrouter:v0.3.1"' -i $valuesfile
+    yq -e '.eventTailer.workloadOverrides.containers[0].image = "rancher/harvester-eventrouter:v0.3.2"' -i $valuesfile
   fi
 
   # add 4 spaces to each line
