4b950a15fd6d1a0a98c7a7ac4efa112a6fe89ac3
Cooper Tseng <cooper.tseng@suse.com>
2025-02-06 18:00:33 +0800

    Fix incorrect Grafana CPU Usage panel

---
+++ package/upgrade/migrations/upgrade_manifests/v1.4.2/pre-hook.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -x
+
+patch_harvester_vm_dashboard()
+{
+    echo "Patch configmap/harvester-vm-dashboard..."
+    local patch_json=$( \
+        kubectl get -n cattle-dashboards configmap harvester-vm-dashboard -o json | \
+            jq '.data."harvester_vm_dashboard.json" | fromjson | .panels |=
+                map(
+                    if .title == "CPU Usage" then
+                        .yaxes |= map(
+                            if .format == "percent" then
+                                .format = "percentunit"
+                            else
+                                .
+                            end
+                        ) |
+                        .targets[0].expr = "topk(${count}, (avg(rate(kubevirt_vmi_vcpu_seconds_total[5m])) by (domain, name)) / 1000)"
+                    else
+                        .
+                    end
+                )' |
+            jq -r .
+    )
+
+    if [ -z "$patch_json" ]; then
+        echo "Something goes wrong, the patch json is empty... skip this patch"
+        return 0
+    fi
+
+    patch_json=$(echo '{"data":{"harvester_vm_dashboard.json": ""}}' | jq --arg arg "$patch_json" '.data."harvester_vm_dashboard.json"=$arg')
+    kubectl patch -n cattle-dashboards configmap harvester-vm-dashboard --type merge -p "$patch_json"
+}
+
+patch_harvester_vm_detail_dashboard()
+{
+    echo "Patch configmap/harvester-vm-detail-dashboard..."
+    local patch_json=$( \
+        kubectl get -n cattle-dashboards configmap harvester-vm-detail-dashboard -o json | \
+            jq '.data."harvester_vm_info_details.json" | fromjson | .panels |=
+                map(
+                    if .title == "CPU Usage" then
+                        .fieldConfig.defaults.unit = "percentunit" |
+                        .targets[0].expr = "sum(avg(rate(kubevirt_vmi_vcpu_seconds_total{namespace=\"$namespace\", name=\"$vm\"}[5m])) by (domain, name)) / 1000"
+                    else
+                        .
+                    end
+                )'
+    )
+
+    if [ -z "$patch_json" ]; then
+        echo "Something goes wrong, the patch json is empty... skip this patch"
+        return 0
+    fi
+
+    patch_json=$(echo '{"data":{"harvester_vm_info_details.json": ""}}' | jq --arg arg "$patch_json" '.data."harvester_vm_info_details.json"=$arg')
+    kubectl patch -n cattle-dashboards configmap harvester-vm-detail-dashboard --type merge -p "$patch_json"
+}
+
+# Fix incorrect unit in CPU Usage dashboard https://github.com/harvester/harvester/issues/7086
+patch_harvester_vm_dashboard
+patch_harvester_vm_detail_dashboard
