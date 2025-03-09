b80a137d9733fc5f3c8e9b65844bb8c7f782bf66
Vicente Cheng <vicente.cheng@suse.com>
2025-03-09 19:39:51 +0800

    controller/pvc: create volumeImportSource to init filesystem PVC
        - Because the dataSourceRef need the same namespace of populator
          (volumeImportSource), we need to create it on-demand.
        - Add mutator for filesystem volume with the annotation
          `harvesterhci.io/volumeForVirtualMachine: "true"`
    
    Signed-off-by: Vicente Cheng <vicente.cheng@suse.com>

--- pkg/controller/master/pvc/register.go
+++ pkg/controller/master/pvc/register.go
@@ -12,14 +12,17 @@ const (
 
 func Register(ctx context.Context, management *config.Management, _ config.Options) error {
 	dataVolume := management.CdiFactory.Cdi().V1beta1().DataVolume()
+	volImportSource := management.CdiFactory.Cdi().V1beta1().VolumeImportSource()
 	ctlpvc := management.CoreFactory.Core().V1().PersistentVolumeClaim()
 
 	pvcHandler := &pvcHandler{
-		dataVolumeClient: dataVolume,
-		pvcClient:        ctlpvc,
-		pvcController:    ctlpvc,
+		volImportSourceClient: volImportSource,
+		dataVolumeClient:      dataVolume,
+		pvcClient:             ctlpvc,
+		pvcController:         ctlpvc,
 	}
 
+	ctlpvc.OnChange(ctx, pvcControllerName, pvcHandler.createFilesystemBlankSource)
 	ctlpvc.OnRemove(ctx, pvcControllerName, pvcHandler.cleanupDataVolume)
 	return nil
 }
--- pkg/util/constants.go
+++ pkg/util/constants.go
@@ -20,6 +20,7 @@ const (
 	AnnotationSVMBackupID               = prefix + "/svmbackupId"
 	AnnotationSVMBackupSkipCronCheck    = prefix + "/svmbackupSkipCronCheck"
 	AnnotationGoldenImage               = prefix + "/goldenImage"
+	AnnotationVolForVM                  = prefix + "/volumeForVirtualMachine"
 	LabelImageDisplayName               = prefix + "/imageDisplayName"
 	LabelSetting                        = prefix + "/setting"
 	LabelVMName                         = prefix + "/vmName"
@@ -102,6 +103,8 @@ const (
 	// CDI constants
 	CSIProvisionerLVM      = "lvm.driver.harvesterhci.io"
 	CSIProvisionerLonghorn = "driver.longhorn.io"
+	KindVolumeImportSource = "VolumeImportSource"
+	ImportSourceFSBlank    = "filesystem-blank-source"
 
 	// CSI constants
 	CSIProvisionerSecretNameKey      = "csi.storage.k8s.io/provisioner-secret-name"
