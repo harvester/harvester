a103d09e43a3e2fd997452b34b6e5925acf34ea7
Vicente Cheng <vicente.cheng@suse.com>
2025-03-19 11:07:30 +0800

    controller/vmImageDownloader: add nodeAffinity if needed
        - If the VM Imager created with LVM storage class, we need to define
          the nodeAffinity to ensure the vmImageDownloader work.
    
    Signed-off-by: Vicente Cheng <vicente.cheng@suse.com>

--- pkg/controller/master/vmimagedownloader/register.go
+++ pkg/controller/master/vmimagedownloader/register.go
@@ -19,12 +19,14 @@ func Register(ctx context.Context, management *config.Management, _ config.Optio
 	vmImage := management.HarvesterFactory.Harvesterhci().V1beta1().VirtualMachineImage()
 	deployment := management.AppsFactory.Apps().V1().Deployment()
 	pvcCache := management.CoreFactory.Core().V1().PersistentVolumeClaim().Cache()
+	scCache := management.StorageFactory.Storage().V1().StorageClass().Cache()
 	clientSet := management.ClientSet
 
 	storageProfileHandler := &vmImageDownloaderHandler{
 		clientSet:                   clientSet,
 		vmImageClient:               vmImage,
 		pvcCache:                    pvcCache,
+		scCache:                     scCache,
 		deploymentClient:            deployment,
 		vmImageDownloaders:          vmImageDownloader,
 		vmImageDownloaderController: vmImageDownloader,
