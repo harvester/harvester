4a6545c91076dacd531972aea1ccccb666214730
Jack Yu <jack.yu@suse.com>
2025-04-25 14:43:37 +0800

    chore: change the rule of creating e2e issue and remove it when unlabeling [skip ci]
    Signed-off-by: Jack Yu <jack.yu@suse.com>

--- .github/workflows/bot/issue_transfer.py
+++ .github/workflows/bot/issue_transfer.py
@@ -3,8 +3,7 @@ import logging
 
 from flask import render_template
 
-from bot import repo, repo_test, \
-    GITHUB_OWNER, GITHUB_REPOSITORY, GITHUB_REPOSITORY_TEST
+from bot import repo
 
 
 template_re = re.compile('---\n.*?---\n', re.DOTALL)
@@ -28,35 +27,3 @@ class IssueTransfer:
         if not found:
             logging.info('pre-merge checklist does not exist, creating a new one')
             self.__issue.create_comment(render_template('pre-merge.md'))
-
-    def create_e2e_issue(self):
-        require_e2e = True
-        labels = self.__issue.get_labels()
-        for label in labels:
-            if label.name == 'not-require/test-plan':
-                require_e2e = False
-                break
-        if require_e2e:
-            found = False
-            for comment in self.__comments:
-                if comment.body.startswith('Automation e2e test issue:'):
-                    logging.info('Automation e2e test issue already exists, not creating a new one')
-                    found = True
-                    break
-            if not found:
-                logging.info('Automation e2e test issue does not exist, creating a new one')
-
-                issue_link = '{}/{}#{}'.format(GITHUB_OWNER, GITHUB_REPOSITORY, self.__issue.number)
-                issue_test_title = '[e2e] {}'.format(self.__issue.title)
-                issue_test_template_content = repo_test.get_contents(
-                    ".github/ISSUE_TEMPLATE/test.md").decoded_content.decode()
-                issue_test_body = template_re.sub("\n", issue_test_template_content, count=1)
-                issue_test_body += '\nrelated issue: {}'.format(issue_link)
-                issue_test = repo_test.create_issue(title=issue_test_title, body=issue_test_body)
-
-                issue_test_link = '{}/{}#{}'.format(GITHUB_OWNER, GITHUB_REPOSITORY_TEST, issue_test.number)
-
-                # link test issue in Harvester issue
-                self.__issue.create_comment('Automation e2e test issue: {}'.format(issue_test_link))
-        else:
-            logging.info('label require/automation-e2e does not exists, not creating test issue')
