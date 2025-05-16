34f60cf4754c564f31c0aac88dcc68ff4afa19fa
Gaurav Mehta <gaurav.mehta@suse.com>
2025-05-16 15:22:44 +1000

    PR introduces a new setting and associated handler to reconcile baseline setting against all non whitelisted namespaces on the cluster
    It also includes a validator to ensure that PSS specific namespace label
    updates can only be performed by the harvester service account when the
    setting is enabled
    
    If setting is enabled then attempts to apply namespace level overrides
    will fail and this can only be managed via the setting update.
    
    Signed-off-by: Gaurav Mehta <gaurav.mehta@suse.com>
    
    update pss to use constants from k8s.io/pod-security-admission/api
    
    Signed-off-by: Gaurav Mehta <gaurav.mehta@suse.com>
    
    additional changes to pss setting handler to include checking for audit/warn labels, plus improve validation/reconcile logic
    
    Signed-off-by: Gaurav Mehta <gaurav.mehta@suse.com>

--- pkg/webhook/resources/namespace/validator.go
+++ pkg/webhook/resources/namespace/validator.go
@@ -3,6 +3,7 @@ package namespace
 import (
 	"encoding/json"
 	"fmt"
+	"slices"
 
 	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
 	"github.com/sirupsen/logrus"
@@ -11,22 +12,31 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
+	psaApi "k8s.io/pod-security-admission/api"
 
 	harvestercorev1 "github.com/harvester/harvester/pkg/generated/controllers/core/v1"
+	harvesterctlv1beta1 "github.com/harvester/harvester/pkg/generated/controllers/harvesterhci.io/v1beta1"
+	"github.com/harvester/harvester/pkg/settings"
 	"github.com/harvester/harvester/pkg/util"
 	rqutils "github.com/harvester/harvester/pkg/util/resourcequota"
 	"github.com/harvester/harvester/pkg/webhook/types"
 )
 
-func NewValidator(resourceQuotaCache harvestercorev1.ResourceQuotaCache) types.Validator {
+func NewValidator(resourceQuotaCache harvestercorev1.ResourceQuotaCache, settingsCache harvesterctlv1beta1.SettingCache) types.Validator {
 	return &namespaceValidator{
 		resourceQuotaCache: resourceQuotaCache,
+		settingsCache:      settingsCache,
 	}
 }
+
+// account name passed by harvester
+// can be used to filter non controller operations
+const harvesterSAName = "system:serviceaccount:harvester-system:harvester"
 
 type namespaceValidator struct {
 	types.DefaultValidator
 	resourceQuotaCache harvestercorev1.ResourceQuotaCache
+	settingsCache      harvesterctlv1beta1.SettingCache
 }
 
 func (v *namespaceValidator) Resource() types.Resource {
@@ -42,10 +52,21 @@ func (v *namespaceValidator) Resource() types.Resource {
 	}
 }
 
-func (v *namespaceValidator) Update(_ *types.Request, oldObj runtime.Object, newObj runtime.Object) error {
+func (v *namespaceValidator) Update(req *types.Request, oldObj runtime.Object, newObj runtime.Object) error {
 	oldNamespace := oldObj.(*corev1.Namespace)
 	newNamespace := newObj.(*corev1.Namespace)
 
+	// perform resource quota validation
+	if err := v.resourceQuotaValidation(oldNamespace, newNamespace); err != nil {
+		return err
+	}
+
+	// perform pss validation to ensure only harvester controller can apply pss labels
+	// when setting is enabled
+	return v.validatePSSOnNamespaceUpdate(req, oldNamespace, newNamespace)
+}
+
+func (v *namespaceValidator) resourceQuotaValidation(oldNamespace, newNamespace *corev1.Namespace) error {
 	rqOld := oldNamespace.Annotations[util.CattleAnnotationResourceQuota]
 	rqNew := newNamespace.Annotations[util.CattleAnnotationResourceQuota]
 	// if no change, skip
@@ -115,4 +136,110 @@ func (v *namespaceValidator) checkIfNewResourceQuotaIsSufficient(rq *corev1.Reso
 	}
 
 	return nil
+}
+
+// validate PSS label is only being applied by the harvester controller, attempts to change this
+// from non controller service account will be blocked if the setting is enabled
+func (v *namespaceValidator) validatePSSOnNamespaceUpdate(req *types.Request, oldNamespace, newNamespace *corev1.Namespace) error {
+	// harvesterSA managed namespace, so allow all updates
+	if isUserHarvesterSA(req) {
+		return nil
+	}
+
+	// check if namespace is whitelisted as part of default Harvester whitelist
+	if isNamespaceWhitelisted(util.DefaultHarvesterNamespaceWhiteList, newNamespace.Name) {
+		return nil
+	}
+
+	settingObj, err := v.settingsCache.Get(settings.ClusterPodSecurityStandardSettingName)
+	if err != nil {
+		return fmt.Errorf("error fetching setting %s from cache: %v", settings.ClusterPodSecurityStandardSettingName, err)
+	}
+
+	pssSetting, err := settings.GetPodSecuritySetting(settingObj)
+	if err != nil {
+		return err
+	}
+
+	// setting is not enabled, nothing need to be done
+	if !pssSetting.Enabled {
+		return nil
+	}
+
+	// verify if namespace is part of whiltelisted namespaces available from setting
+	if isNamespaceWhitelisted(util.GetWhitelistedNamespacesList(pssSetting), newNamespace.Name) {
+		return nil
+	}
+
+	if oldNamespace.Labels == nil {
+		oldNamespace.Labels = make(map[string]string)
+	}
+
+	if newNamespace.Labels == nil {
+		newNamespace.Labels = make(map[string]string)
+	}
+
+	// verify pss label is not being changed
+	for _, key := range []string{psaApi.EnforceLevelLabel, psaApi.EnforceVersionLabel, psaApi.AuditLevelLabel, psaApi.AuditVersionLabel, psaApi.WarnLevelLabel, psaApi.WarnVersionLabel, util.HarvesterManagedPSSKey} {
+		if newNamespace.Labels[key] != oldNamespace.Labels[key] {
+			return fmt.Errorf("%s is enabled, PSS level can only be changed by cluster admin via the setting", settings.ClusterPodSecurityStandardSettingName)
+		}
+
+	}
+
+	return nil
+}
+
+// Create ensures user cannot apply PSS label during namespace creation there by ignoring the underlying cluster level
+// settings
+func (v *namespaceValidator) Create(req *types.Request, newObj runtime.Object) error {
+	newNamespace := newObj.(*corev1.Namespace)
+
+	// harvesterSA managed namespace, so allow all updates
+	if isUserHarvesterSA(req) {
+		return nil
+	}
+
+	// check if namespace is whitelisted as part of default Harvester whitelist
+	if isNamespaceWhitelisted(util.DefaultHarvesterNamespaceWhiteList, newNamespace.Name) {
+		return nil
+	}
+
+	settingObj, err := v.settingsCache.Get(settings.ClusterPodSecurityStandardSettingName)
+	if err != nil {
+		return fmt.Errorf("error fetching setting %s from cache: %v", settings.ClusterPodSecurityStandardSettingName, err)
+	}
+
+	pssSetting, err := settings.GetPodSecuritySetting(settingObj)
+	if err != nil {
+		return err
+	}
+
+	// setting is not enabled, nothing need to be done
+	if !pssSetting.Enabled {
+		return nil
+	}
+
+	// verify if namespace is part of whiltelisted namespaces available from setting
+	if isNamespaceWhitelisted(util.GetWhitelistedNamespacesList(pssSetting), newNamespace.Name) {
+		return nil
+	}
+
+	for _, key := range []string{psaApi.EnforceLevelLabel, psaApi.EnforceVersionLabel, psaApi.AuditLevelLabel, psaApi.AuditVersionLabel, psaApi.WarnLevelLabel, psaApi.WarnVersionLabel, util.HarvesterManagedPSSKey} {
+		if _, ok := newNamespace.Labels[key]; ok {
+			return fmt.Errorf("%s is enabled, PSS level cannot be set during namespace creation, please remove PSS related labels from the namespace manifest and try again", settings.ClusterPodSecurityStandardSettingName)
+		}
+
+	}
+
+	return nil
+}
+
+// isNamespaceWhitelisted checks if namespace is whitelisted
+func isNamespaceWhitelisted(namespaceList []string, namespace string) bool {
+	return slices.Contains(namespaceList, namespace)
+}
+
+func isUserHarvesterSA(req *types.Request) bool {
+	return req.Username() == harvesterSAName
 }
