44f484d5ed49a3f5dbc431622a78dc3340181dbf
Luiz Felipe Rosa <luizrosa8@proton.me>
2025-06-25 19:00:54 -0300

    feat: add new machine validator under webhook
    Signed-off-by: Luiz Felipe Rosa <luizrosa8@proton.me>

---
+++ pkg/webhook/resources/machine/validator.go
@@ -0,0 +1,40 @@
+package machine
+
+import (
+	admissionregv1 "k8s.io/api/admissionregistration/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/harvester/harvester/pkg/webhook/types"
+)
+
+func NewValidator(client client.Client) types.Validator {
+	return &machineValidator{
+		client: client,
+	}
+}
+
+type machineValidator struct {
+	types.DefaultValidator
+	client client.Client
+}
+
+func (v *machineValidator) Resource() types.Resource {
+	return types.Resource{
+		Names:      []string{"machines"},
+		Scope:      admissionregv1.ClusterScope,
+		APIGroup:   corev1.SchemeGroupVersion.Group,
+		APIVersion: corev1.SchemeGroupVersion.Version,
+		ObjectType: &corev1.Node{},
+		OperationTypes: []admissionregv1.OperationType{
+			admissionregv1.Create,
+		},
+	}
+}
+
+func (v *machineValidator) Update(_ *types.Request, oldObj runtime.Object, newObj runtime.Object) error {
+
+	return nil
+}
