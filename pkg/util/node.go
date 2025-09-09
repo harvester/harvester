27e5a52496db3aa0362a497df08f5be8e39520b1
Volker Theile <vtheile@suse.com>
2025-09-09 08:42:22 +0200

    Make use of status conditions when activating maintenance mode
    Related to: https://github.com/harvester/harvester/issues/9022
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- pkg/util/node.go
+++ pkg/util/node.go
@@ -13,3 +13,31 @@ func ExcludeWitnessNodes(nodes []*corev1.Node) []*corev1.Node {
 	}
 	return nonWitnessNodes
 }
+
+func AddOrUpdateConditionToNode(node *corev1.Node, newCondition corev1.NodeCondition) {
+	found := false
+	for i := range node.Status.Conditions {
+		c := &node.Status.Conditions[i]
+		if c.Type == newCondition.Type {
+			found = true
+			c.Status = newCondition.Status
+			c.LastHeartbeatTime = newCondition.LastHeartbeatTime
+			c.LastTransitionTime = newCondition.LastTransitionTime
+			c.Reason = newCondition.Reason
+			c.Message = newCondition.Message
+		}
+	}
+	if !found {
+		node.Status.Conditions = append(node.Status.Conditions, newCondition)
+	}
+}
+
+func RemoveConditionFromNode(node *corev1.Node, condType corev1.NodeConditionType) {
+	newConditions := make([]corev1.NodeCondition, 0, len(node.Status.Conditions))
+	for _, c := range node.Status.Conditions {
+		if c.Type != condType {
+			newConditions = append(newConditions, c)
+		}
+	}
+	node.Status.Conditions = newConditions
+}
---
+++ pkg/util/node_test.go
@@ -0,0 +1,81 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func Test_AddOrUpdateConditionToNode_Add(t *testing.T) {
+	node := &corev1.Node{
+		Status: corev1.NodeStatus{
+			Conditions: []corev1.NodeCondition{},
+		},
+	}
+	AddOrUpdateConditionToNode(node, corev1.NodeCondition{
+		Type:               NodeConditionTypeMaintenanceMode,
+		Status:             corev1.ConditionTrue,
+		LastHeartbeatTime:  metav1.Now(),
+		LastTransitionTime: metav1.Now(),
+		Reason:             NodeConditionReasonCompleted,
+		Message:            "Draining the node is completed",
+	})
+	assert.Len(t, node.Status.Conditions, 1)
+}
+
+func Test_AddOrUpdateConditionToNode_Update(t *testing.T) {
+	node := &corev1.Node{
+		Status: corev1.NodeStatus{
+			Conditions: []corev1.NodeCondition{
+				{
+					Type:               NodeConditionTypeMaintenanceMode,
+					Status:             corev1.ConditionTrue,
+					LastHeartbeatTime:  metav1.Now(),
+					LastTransitionTime: metav1.Now(),
+					Reason:             NodeConditionReasonRunning,
+					Message:            "Draining the node is running",
+				},
+			},
+		},
+	}
+	AddOrUpdateConditionToNode(node, corev1.NodeCondition{
+		Type:               NodeConditionTypeMaintenanceMode,
+		Status:             corev1.ConditionTrue,
+		LastHeartbeatTime:  metav1.Now(),
+		LastTransitionTime: metav1.Now(),
+		Reason:             NodeConditionReasonCompleted,
+		Message:            "Draining the node is completed",
+	})
+	assert.Len(t, node.Status.Conditions, 1)
+	assert.Equal(t, node.Status.Conditions[0].Reason, NodeConditionReasonCompleted)
+}
+
+func Test_RemoveConditionFromNode(t *testing.T) {
+	node := &corev1.Node{
+		Status: corev1.NodeStatus{
+			Conditions: []corev1.NodeCondition{
+				{
+					Type:               NodeConditionTypeMaintenanceMode,
+					Status:             corev1.ConditionTrue,
+					LastHeartbeatTime:  metav1.Now(),
+					LastTransitionTime: metav1.Now(),
+					Reason:             NodeConditionReasonCompleted,
+					Message:            "Draining the node is completed",
+				},
+				{
+					Type:               "foo",
+					Status:             corev1.ConditionFalse,
+					LastHeartbeatTime:  metav1.Now(),
+					LastTransitionTime: metav1.Now(),
+					Reason:             "bar",
+					Message:            "Qui aromata regit, universum regit",
+				},
+			},
+		},
+	}
+	RemoveConditionFromNode(node, NodeConditionTypeMaintenanceMode)
+	assert.Len(t, node.Status.Conditions, 1)
+}
