b1ddc7da2cd7d474e3cb198d6fe048ee36ee716f
Volker Theile <vtheile@suse.com>
2025-09-09 08:42:22 +0200

    Make use of status conditions when activating maintenance mode
    Related to: https://github.com/harvester/harvester/issues/9022
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- pkg/controller/master/node/maintain_controller.go
+++ pkg/controller/master/node/maintain_controller.go
@@ -3,10 +3,12 @@ package node
 import (
 	"context"
 	"fmt"
+	"reflect"
 
 	ctlcorev1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/core/v1"
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	kubevirtv1 "kubevirt.io/api/core/v1"
 
@@ -57,7 +59,20 @@ func (h *maintainNodeHandler) OnNodeChanged(_ string, node *corev1.Node) (*corev
 	if node == nil || node.DeletionTimestamp != nil {
 		return node, nil
 	}
+
 	if maintenanceStatus, ok := node.Annotations[MaintainStatusAnnotationKey]; !ok || maintenanceStatus != MaintainStatusRunning {
+		if !ok {
+			// Make sure the maintenance mode status condition is cleaned up
+			// when there is no "harvesterhci.io/maintain-status" annotation.
+			nodeCopy := node.DeepCopy()
+			util.RemoveConditionFromNode(nodeCopy, util.NodeConditionTypeMaintenanceMode)
+			if !reflect.DeepEqual(node.Status.Conditions, nodeCopy.Status.Conditions) {
+				if _, err := h.nodes.UpdateStatus(nodeCopy); err != nil {
+					return node, fmt.Errorf("failed to clean up %q condition: %w", util.NodeConditionTypeMaintenanceMode, err)
+				}
+				return nodeCopy, nil
+			}
+		}
 		return node, nil
 	}
 
@@ -109,9 +124,24 @@ func (h *maintainNodeHandler) OnNodeChanged(_ string, node *corev1.Node) (*corev
 		}
 	}
 
-	toUpdate := node.DeepCopy()
-	toUpdate.Annotations[MaintainStatusAnnotationKey] = MaintainStatusComplete
-	return h.nodes.Update(toUpdate)
+	nodeCopy := node.DeepCopy()
+
+	nodeCopy.Annotations[MaintainStatusAnnotationKey] = MaintainStatusComplete
+	nodeCopy, err = h.nodes.Update(nodeCopy)
+	if err != nil {
+		return node, err
+	}
+
+	util.AddOrUpdateConditionToNode(nodeCopy, corev1.NodeCondition{
+		Type:               util.NodeConditionTypeMaintenanceMode,
+		Status:             corev1.ConditionTrue,
+		LastHeartbeatTime:  metav1.Now(),
+		LastTransitionTime: metav1.Now(),
+		Reason:             util.NodeConditionReasonCompleted,
+		Message:            "Draining the node is completed",
+	})
+
+	return h.nodes.UpdateStatus(nodeCopy)
 }
 
 // OnNodeRemoved Ensure that all "harvesterhci.io/maintain-mode-strategy-node-name"
