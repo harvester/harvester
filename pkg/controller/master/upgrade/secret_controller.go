173fa1d38b3bbd7aee910b4b16c4136a3d017a39
Zespre Chang <zespre.chang@suse.com>
2025-11-19 22:29:55 +0800

    feat(upgrade): leave unpause record in the pause map
    Unpaused node records are kept in the pause map annotation for
    references. The machine-plan secret enqueue criteria is adjust as a
    result. Now it will ensure only those machine-plan secrets whose nodes
    are in paused state and are requested to be unpaused are enqueued.
    
    Signed-off-by: Zespre Chang <zespre.chang@suse.com>
    (cherry picked from commit 3553a196e2e0b7789f00bd2e28a189a6b5d2574f)

--- pkg/controller/master/upgrade/secret_controller.go
+++ pkg/controller/master/upgrade/secret_controller.go
@@ -2,7 +2,6 @@ package upgrade
 
 import (
 	"fmt"
-	"reflect"
 	"time"
 
 	jobV1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/batch/v1"
@@ -83,43 +82,39 @@ func (h *secretHandler) OnChanged(_ string, secret *v1.Secret) (*v1.Secret, erro
 
 	switch upgrade.Status.NodeStatuses[nodeName].State {
 	case nodeStateImagesPreloaded:
-		if secret.Annotations[rke2PreDrainAnnotation] != secret.Annotations[preDrainAnnotation] {
-			if shouldPauseNodeUpgrade(upgrade, nodeName) {
-				logrus.Infof("Pause pre-drain job creation for node %s", nodeName)
-				setNodeUpgradeStatus(upgradeCpy, nodeName, nodeStateUpgradePaused, "AdministrativelyPaused", "Node upgrade paused as requested by the user")
-				logrus.Infof("Update upgrade %s/%s", upgrade.Namespace, upgrade.Name)
-				_, err := h.upgradeClient.Update(upgradeCpy)
-				return secret, err
-			}
-			if err := checkEligibleToDrain(upgrade, nodeName); err != nil {
-				return nil, err
-			}
-			logrus.Debugf("Create pre-drain job on %s", nodeName)
-			if err := h.createHookJob(upgrade, nodeName, upgradeJobTypePreDrain, nodeStatePreDraining); err != nil {
-				return nil, err
-			}
+		if isPreDrained(secret) {
+			break
 		}
-	case nodeStateUpgradePaused:
-		if secret.Annotations[rke2PreDrainAnnotation] != secret.Annotations[preDrainAnnotation] {
-			if shouldPauseNodeUpgrade(upgrade, nodeName) {
-				logrus.Debugf("Continue pausing pre-drain job creation for node %s", nodeName)
-				return secret, nil
-			}
-			logrus.Infof("Unpause pre-drain job creation for node %s", nodeName)
-			if err := clearNodePause(upgradeCpy, nodeName); err != nil {
-				return secret, err
-			}
-			setNodeUpgradeStatus(upgradeCpy, nodeName, nodeStateImagesPreloaded, "", "")
-			if reflect.DeepEqual(upgradeCpy, upgrade) {
-				return secret, nil
-			}
+		if shouldPauseNodeUpgrade(upgrade, nodeName) {
+			logrus.Infof("Pause pre-drain job creation for node %s", nodeName)
+			setNodeUpgradeStatus(upgradeCpy, nodeName, nodeStateUpgradePaused, "AdministrativelyPaused", "Node upgrade paused as requested by the user")
 			logrus.Infof("Update upgrade %s/%s", upgrade.Namespace, upgrade.Name)
-			if _, err := h.upgradeClient.Update(upgradeCpy); err != nil {
-				return secret, err
-			}
-			h.secretController.EnqueueAfter(secret.Namespace, secret.Name, 5*time.Second)
+			_, err := h.upgradeClient.Update(upgradeCpy)
 			return secret, err
 		}
+		if err := checkEligibleToDrain(upgrade, nodeName); err != nil {
+			return nil, err
+		}
+		logrus.Debugf("Create pre-drain job on %s", nodeName)
+		if err := h.createHookJob(upgrade, nodeName, upgradeJobTypePreDrain, nodeStatePreDraining); err != nil {
+			return nil, err
+		}
+	case nodeStateUpgradePaused:
+		if isPreDrained(secret) {
+			break
+		}
+		if shouldPauseNodeUpgrade(upgrade, nodeName) {
+			logrus.Debugf("Continue pausing pre-drain job creation for node %s", nodeName)
+			return secret, nil
+		}
+		logrus.Infof("Unpause pre-drain job creation for node %s", nodeName)
+		setNodeUpgradeStatus(upgradeCpy, nodeName, nodeStateImagesPreloaded, "", "")
+		logrus.Infof("Update upgrade %s/%s", upgrade.Namespace, upgrade.Name)
+		if _, err := h.upgradeClient.Update(upgradeCpy); err != nil {
+			return secret, err
+		}
+		h.secretController.EnqueueAfter(secret.Namespace, secret.Name, 5*time.Second)
+		return secret, err
 	case nodeStatePreDrained:
 		if secret.Annotations[rke2PostDrainAnnotation] != secret.Annotations[postDrainAnnotation] {
 			if err := checkEligibleToDrain(upgrade, nodeName); err != nil {
@@ -189,4 +184,8 @@ func checkEligibleToDrain(upgrade *harvesterv1.Upgrade, nodeName string) error {
 		return fmt.Errorf("%s is in \"%s\" state so %s is not allowed to run any kind of job", name, status, nodeName)
 	}
 	return nil
+}
+
+func isPreDrained(secret *v1.Secret) bool {
+	return secret.Annotations[rke2PreDrainAnnotation] == secret.Annotations[preDrainAnnotation]
 }
