243638c51e562bcf7c570b70e77396b959592fc5
Cooper Tseng <cooper.tseng@suse.com>
2025-11-25 15:23:08 +0800

    Address comments
    Signed-off-by: Cooper Tseng <cooper.tseng@suse.com>

--- pkg/image/cdi/common.go
+++ pkg/image/cdi/common.go
@@ -140,22 +140,27 @@ func fetchImageSize(url string) (int64, error) {
 	}
 	defer rsp.Body.Close()
 
-	var contentLengthStr string
-	if cl := rsp.Header.Get("Content-Length"); cl != "" {
-		contentLengthStr = cl
-	} else if xgcl := rsp.Header.Get("x-goog-stored-content-length"); xgcl != "" {
-		// Fallback to x-goog-stored-content-length if Content-Length is not available
-		contentLengthStr = xgcl
-	} else {
-		// if both headers are missing, return an error
-		return 0, ErrHeaderContentLengthNotFound
+	cl, err := getContentLength(rsp)
+	if err != nil {
+		return 0, err
 	}
 
-	contentLength, err := strconv.ParseInt(contentLengthStr, 10, 64)
+	contentLength, err := strconv.ParseInt(cl, 10, 64)
 	if err != nil {
 		return 0, err
 	}
 	return contentLength, nil
+}
+
+func getContentLength(rsp *http.Response) (string, error) {
+	if cl := rsp.Header.Get("Content-Length"); cl != "" {
+		return cl, nil
+	} else if xgcl := rsp.Header.Get("x-goog-stored-content-length"); xgcl != "" {
+		// fallback to x-goog-stored-content-length if Content-Length is not available
+		return xgcl, nil
+	}
+	// if both headers are missing, return an error
+	return "", ErrHeaderContentLengthNotFound
 }
 
 func fetchImageVirtualSize(url string) (int64, error) {
