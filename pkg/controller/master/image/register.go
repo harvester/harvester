5cd4ce874f315c438d9696682166ecd8ed77c978
Cooper Tseng <cooper.tseng@suse.com>
2025-12-03 19:55:58 +0800

    Fix harvester crash when performing cdi vmimage download
    Signed-off-by: Cooper Tseng <cooper.tseng@suse.com>
    (cherry picked from commit 51b8b75d5c1211a3cba535464b5fd0c39b96f8b4)

--- pkg/controller/master/image/register.go
+++ pkg/controller/master/image/register.go
@@ -25,7 +25,10 @@ func Register(ctx context.Context, management *config.Management, _ config.Optio
 	secrets := management.CoreFactory.Core().V1().Secret()
 	ctlcdi := management.CdiFactory.Cdi().V1beta1().DataVolume()
 
-	vmio := common.GetVMIOperator(vmi, nil, http.Client{Timeout: 15 * time.Second})
+	vmio, err := common.GetVMIOperator(vmi, vmi.Cache(), http.Client{Timeout: 15 * time.Second})
+	if err != nil {
+		return err
+	}
 	backends := map[harvesterv1.VMIBackend]backend.Backend{
 		harvesterv1.VMIBackendBackingImage: backingimage.GetBackend(
 			ctx, sc, sc.Cache(),
