c9cd422ba301f42c853e0b665cf03ae8aedb7d15
Jian Wang <jian.wang@suse.com>
2026-02-18 16:23:00 +0100

    Minor updates
    Signed-off-by: Jian Wang <jian.wang@suse.com>

--- pkg/controller/master/migration/utils.go
+++ pkg/controller/master/migration/utils.go
@@ -6,30 +6,16 @@ import (
 	"github.com/harvester/harvester/pkg/util"
 )
 
-// when migration is done or aborted, vmi.Status.MigrationState has following conditions
-func IsVmiMigrationDone(vmi *kubevirtv1.VirtualMachineInstance) bool {
+// when migration is done or aborted, vmi.Status.MigrationState has Completed or Failed
+func IsVmiResetHarvesterMigrationAnnotationRequired(vmi *kubevirtv1.VirtualMachineInstance) bool {
 	if vmi == nil || vmi.Status.MigrationState == nil {
 		return false
 	}
-
-	return vmi.Status.MigrationState.Completed || vmi.Status.MigrationState.Failed
-	//return isVmiMigrationDone(vmi)
+	return (vmi.Status.MigrationState.Completed || vmi.Status.MigrationState.Failed) &&
+		vmi.Annotations[util.AnnotationMigrationUID] == string(vmi.Status.MigrationState.MigrationUID)
 }
 
-// when migration is done or aborted, vmi.Status.MigrationState has following conditions
-// caller ensures vmi and vmi.Status.MigrationState are not nil
-func isVmiMigrationDone(vmi *kubevirtv1.VirtualMachineInstance) bool {
-	// migrationState := vmi.Status.MigrationState
-	//return migrationState.StartTimestamp != nil && migrationState.EndTimestamp != nil && (migrationState.Completed || migrationState.Failed)
-	return vmi.Status.MigrationState.Completed || vmi.Status.MigrationState.Failed
-}
-
-// caller ensures vmi and vmi.Status.MigrationState are not nil
-func isVmiWithHarvesterMigrationAnnotation(vmi *kubevirtv1.VirtualMachineInstance) bool {
-	return vmi.Annotations[util.AnnotationMigrationUID] == string(vmi.Status.MigrationState.MigrationUID)
-}
-
-// migration is done or aborted
+// migration is succeeded or failed (aborted)
 func IsVmimMigrationDone(vmim *kubevirtv1.VirtualMachineInstanceMigration) bool {
 	if vmim == nil {
 		return false
