e76698dfb65cf883ccd40f68181db9600d9bff53
Vicente Cheng <vicente.cheng@suse.com>
2026-03-26 05:05:46 +0800

    controller: remove the volumeForVirtualMachine annotation
        - Now we can create PVC through DataVolume on the UI, so we don't
          need this trick for the filesystem volume.
    
    Signed-off-by: Vicente Cheng <vicente.cheng@suse.com>

--- pkg/controller/master/pvc/register.go
+++ pkg/controller/master/pvc/register.go
@@ -12,17 +12,12 @@ const (
 
 func Register(ctx context.Context, management *config.Management, _ config.Options) error {
 	dataVolume := management.CdiFactory.Cdi().V1beta1().DataVolume()
-	volImportSource := management.CdiFactory.Cdi().V1beta1().VolumeImportSource()
 	ctlpvc := management.CoreFactory.Core().V1().PersistentVolumeClaim()
 
 	pvcHandler := &pvcHandler{
-		volImportSourceClient: volImportSource,
-		dataVolumeClient:      dataVolume,
-		pvcClient:             ctlpvc,
-		pvcController:         ctlpvc,
+		dataVolumeClient: dataVolume,
 	}
 
-	ctlpvc.OnChange(ctx, pvcControllerName, pvcHandler.createFilesystemBlankSource)
 	ctlpvc.OnRemove(ctx, pvcControllerName, pvcHandler.cleanupDataVolume)
 	return nil
 }
--- pkg/webhook/resources/persistentvolumeclaim/mutator.go
+++ pkg/webhook/resources/persistentvolumeclaim/mutator.go
@@ -1,7 +1,6 @@
 package persistentvolumeclaim
 
 import (
-	"encoding/json"
 	"fmt"
 
 	v1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/core/v1"
@@ -11,7 +10,6 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	cdicommon "kubevirt.io/containerized-data-importer/pkg/controller/common"
 	"kubevirt.io/kubevirt/pkg/apimachinery/patch"
 
 	"github.com/harvester/harvester/pkg/apis/harvesterhci.io/v1beta1"
@@ -51,8 +49,6 @@ func (m *pvcMutator) Create(_ *types.Request, newObj runtime.Object) (types.Patc
 	pvc := newObj.(*corev1.PersistentVolumeClaim)
 
 	logrus.Debugf("create PVC %s/%s with mutator", pvc.Namespace, pvc.Name)
-	patchOPs := []string{}
-	isGoldenImage := false
 
 	// check pvc is related to the vm image
 	patchOp, err := m.patchGoldenImageAnnotation(pvc)
@@ -60,27 +56,10 @@ func (m *pvcMutator) Create(_ *types.Request, newObj runtime.Object) (types.Patc
 		return nil, err
 	}
 
-	if patchOp != "" || apierrors.IsAlreadyExists(err) {
-		isGoldenImage = true
-		if patchOp != "" {
-			patchOPs = append(patchOPs, patchOp)
-		}
-	}
-
-	// patch populator
-	if !isGoldenImage {
-		patchOp, err = m.patchDataSource(pvc)
-		if err != nil {
-			return nil, err
-		}
-		patchOPs = append(patchOPs, patchOp)
-	}
-
-	// no need to patch
-	if len(patchOPs) == 0 {
+	if patchOp == "" {
 		return nil, nil
 	}
-	return patchOPs, nil
+	return types.PatchOps{patchOp}, nil
 }
 
 func (m *pvcMutator) patchGoldenImageAnnotation(pvc *corev1.PersistentVolumeClaim) (string, error) {
@@ -106,47 +85,4 @@ func (m *pvcMutator) patchGoldenImageAnnotation(pvc *corev1.PersistentVolumeClai
 
 	// patch annotation
 	return fmt.Sprintf(`{"op": "replace", "path": "/metadata/annotations/%s", "value": "true"}`, patch.EscapeJSONPointer(util.AnnotationGoldenImage)), nil
-}
-
-func (m pvcMutator) patchDataSource(pvc *corev1.PersistentVolumeClaim) (string, error) {
-	// do no-op if volume mode is block
-	if pvc.Spec.VolumeMode != nil && *pvc.Spec.VolumeMode == corev1.PersistentVolumeBlock {
-		logrus.Debugf("PVC %s/%s is block volume, skip patch", pvc.Namespace, pvc.Name)
-		return "", nil
-	}
-	annotations := pvc.GetAnnotations()
-	if v, find := annotations[util.AnnotationVolForVM]; !find || v != "true" {
-		logrus.Debugf("PVC %s/%s is not for VM, skip patch", pvc.Namespace, pvc.Name)
-		return "", nil
-	}
-
-	// do not replace any existing data source, such as csi snapshot, with the
-	// filesystem-blank-source VolumeImportSource. if set, the 'blank'
-	// VolumeImportSource would wipe any restore or prepopulated data.
-	if pvc.Spec.DataSourceRef != nil || pvc.Spec.DataSource != nil {
-		dataSource := ""
-		if pvc.Spec.DataSourceRef != nil {
-			dataSource = fmt.Sprintf("%+v", pvc.Spec.DataSourceRef)
-		} else if pvc.Spec.DataSource != nil {
-			dataSource = fmt.Sprintf("%+v", pvc.Spec.DataSource)
-		}
-		pvcName := fmt.Sprintf("%s/%s", pvc.Namespace, pvc.Name)
-		logrus.Debugf("PVC %s has existing data source: %s, skip patch", pvcName, dataSource)
-		return "", nil
-	}
-
-	cdiAPIGroup := cdicommon.AnnAPIGroup
-	dataSourceRef := corev1.TypedLocalObjectReference{
-		APIGroup: &cdiAPIGroup,
-		Kind:     util.KindVolumeImportSource,
-		Name:     util.ImportSourceFSBlank,
-	}
-	dataSourceRefVal, err := json.Marshal(dataSourceRef)
-	if err != nil {
-		logrus.Warnf("failed to marshal data source ref: %v, err: %v", dataSourceRef, err)
-		return "", err
-	}
-
-	// patch annotation
-	return fmt.Sprintf(`{"op": "replace", "path": "/spec/dataSourceRef", "value": %s}`, string(dataSourceRefVal)), nil
 }
