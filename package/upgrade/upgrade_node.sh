harvester/harvester

commit e62990dd2f85f49dcaa55930ac21cef6af397540

fix: bind mount /lib64 to get updated glibc in upgrade container
This terrifying thing is necessary because when elemental-toolkit is
built against SL Micro 6.1, it uses a newer version of glibc than is
available on SLE Micro 5.5 hosts, which means `elemental upgrade` fails
with "/tmp/elemental: /lib64/libc.so.6: version `GLIBC_2.32' not found
(required by /tmp/elemental)" when run in the host chroot.

We can work around this by bind mounting /lib64 from the bci-base:15.7
upgrade container over the host's /lib64, before invoking `elemental
upgrade`.  But, we only want to do this if we really know we need to,
because in subsequent upgrades (once the host is already running SL
Micro 6.1), /lib64 is actually a symlink to /usr/lib64, and bind
mounting over that breaks other things.  Also, it's not necessary
anymore anyway, because the host OS at that point already has
the new glibc.

Related-to: https://github.com/harvester/harvester/issues/7025

Signed-off-by: Tim Serong <tserong@suse.com>

package/upgrade/upgrade_node.sh
@@ -594,11 +594,26 @@ EOF
 
   elemental_upgrade_log="${UPGRADE_TMP_DIR#"$HOST_DIR"}/elemental-upgrade-$(date +%Y%m%d%H%M%S).log"
   local ret=0
+  # elemental-toolkit built for SL Micro 6.1 needs a newer glibc than
+  # is available on SLE Micro 5.5 hosts.  We can work around this by
+  # bind mounting /lib64 from this newer container into the host
+  # environment, but we only want to do this if we know there's a
+  # problem (if the host is already running SL Micro 6.1, then bind
+  # mounting /lib64 from a SLE 15 SP7 container will actually break
+  # things).
+  local glibc_too_old=$(chroot $HOST_DIR /tmp/elemental version 2>&1 | grep 'GLIBC.*not found')
+  if [ -n "$glibc_too_old" ]; then
+    echo "GLIBC on host is too old for new elemental build; bind mounting /lib64 to fix"
+    mount -o bind /lib64 $HOST_DIR/lib64
+  fi
   chroot $HOST_DIR /tmp/elemental upgrade \
     --logfile "$elemental_upgrade_log" \
     --directory ${tmp_rootfs_mount#"$HOST_DIR"} \
     --config-dir ${tmp_elemental_config_dir#"$HOST_DIR"} \
     --debug || ret=$?
+  if [ -n "$glibc_too_old" ]; then
+    umount $HOST_DIR/lib64
+  fi
   if [ "$ret" != 0 ]; then
     echo "elemental upgrade failed with return code: $ret"
     cat "$HOST_DIR$elemental_upgrade_log"

commit e6110d54c2de773e3e00cc085b4e1cce3d515c83

fix: bind mount /lib during upgrade (needed for ARM)
The earlier fix to bind mount /lib64 during upgrade to ensure an updated
glibc is available in the upgrade container worked on x86_64, but isn't
enough on aarch64.  We need to also bring in /lib.  This is because, on
SL Micro 5.5 on aarch64, /lib/ld-linux-aarch64.so.1 is a symlink to
/lib64/ld-2.31.so, so if we bind mount /lib64 from the upgrade container
over the host, we lose that link target and no binaries will run inside
the chroot.  Bind mounting /lib as well brings in an updated version of
/lib/ld-linux-aarch64.so.1, which on SL Micro 6.1 is just a regular
file, not a symlink.  Adding /lib  causes no trouble on x86_64 and saves
us from having to complicate things with architecture specific
conditionals.

Related-to: https://github.com/harvester/harvester/issues/10286
Fixes: e62990dd2f85f49dcaa55930ac21cef6af397540

Signed-off-by: Tim Serong <tserong@suse.com>

package/upgrade/upgrade_node.sh
@@ -712,6 +712,8 @@ EOF
   if [ -n "$glibc_too_old" ]; then
     echo "GLIBC on host is too old for new elemental build; bind mounting /lib64 to fix"
     mount -o bind /lib64 $HOST_DIR/lib64
+    # /lib is also necessary for ARM
+    mount -o bind /lib $HOST_DIR/lib
   fi
   chroot $HOST_DIR /tmp/elemental upgrade \
     --logfile "$elemental_upgrade_log" \
@@ -720,6 +722,7 @@ EOF
     --debug || ret=$?
   if [ -n "$glibc_too_old" ]; then
     umount $HOST_DIR/lib64
+    umount $HOST_DIR/lib
   fi
   if [ "$ret" != 0 ]; then
     echo "elemental upgrade failed with return code: $ret"