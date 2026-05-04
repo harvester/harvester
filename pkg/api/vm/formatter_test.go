768d8573a352daa532f3dc8ebae56c8e875b7547
Cooper Tseng <cooper.tseng@suse.com>
2026-05-04 17:03:26 +0800

    Enable backup actions for vm created with longhorn v2
    Signed-off-by: Cooper Tseng <cooper.tseng@suse.com>

---
+++ pkg/api/vm/formatter_test.go
@@ -0,0 +1,168 @@
+package vm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	storagev1 "k8s.io/api/storage/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	kubevirtv1 "kubevirt.io/api/core/v1"
+
+	harvesterv1 "github.com/harvester/harvester/pkg/apis/harvesterhci.io/v1beta1"
+	"github.com/harvester/harvester/pkg/generated/clientset/versioned/fake"
+	"github.com/harvester/harvester/pkg/settings"
+	"github.com/harvester/harvester/pkg/util"
+	"github.com/harvester/harvester/pkg/util/fakeclients"
+)
+
+func TestCanDoBackup(t *testing.T) {
+	longhornSC := "longhorn"
+	lvmSC := "lvm"
+	backupTarget := `{"type":"s3","endpoint":"https://s3.amazonaws.com","bucketName":"test","bucketRegion":"us-west-1"}`
+
+	tests := []struct {
+		name           string
+		pvcNames       []string                        // PVC names to attach to VM
+		pvcs           []*corev1.PersistentVolumeClaim // PVCs to create
+		storageClasses []*storagev1.StorageClass       // Storage classes to create
+		expected       bool
+	}{
+		{
+			name:     "explicit Longhorn SC - allow backup",
+			pvcNames: []string{"pvc1"},
+			pvcs:     []*corev1.PersistentVolumeClaim{newTestPVC("pvc1", &longhornSC)},
+			storageClasses: []*storagev1.StorageClass{
+				newTestStorageClass(longhornSC, util.CSIProvisionerLonghorn, false),
+			},
+			expected: true,
+		},
+		{
+			name:     "explicit LVM SC - block backup",
+			pvcNames: []string{"pvc1"},
+			pvcs:     []*corev1.PersistentVolumeClaim{newTestPVC("pvc1", &lvmSC)},
+			storageClasses: []*storagev1.StorageClass{
+				newTestStorageClass(lvmSC, util.CSIProvisionerLVM, false),
+			},
+			expected: false,
+		},
+		{
+			name:     "default Longhorn SC - allow backup",
+			pvcNames: []string{"pvc1"},
+			pvcs:     []*corev1.PersistentVolumeClaim{newTestPVC("pvc1", nil)},
+			storageClasses: []*storagev1.StorageClass{
+				newTestStorageClass(longhornSC, util.CSIProvisionerLonghorn, true),
+			},
+			expected: true,
+		},
+		{
+			name:     "default LVM SC - block backup",
+			pvcNames: []string{"pvc1"},
+			pvcs:     []*corev1.PersistentVolumeClaim{newTestPVC("pvc1", nil)},
+			storageClasses: []*storagev1.StorageClass{
+				newTestStorageClass(lvmSC, util.CSIProvisionerLVM, true),
+			},
+			expected: false,
+		},
+		{
+			name:           "no SC and no default - block backup",
+			pvcNames:       []string{"pvc1"},
+			pvcs:           []*corev1.PersistentVolumeClaim{newTestPVC("pvc1", nil)},
+			storageClasses: []*storagev1.StorageClass{},
+			expected:       false,
+		},
+		{
+			name:     "mixed volumes (Longhorn + LVM) - block backup",
+			pvcNames: []string{"pvc1", "pvc2"},
+			pvcs: []*corev1.PersistentVolumeClaim{
+				newTestPVC("pvc1", &longhornSC),
+				newTestPVC("pvc2", &lvmSC),
+			},
+			storageClasses: []*storagev1.StorageClass{
+				newTestStorageClass(longhornSC, util.CSIProvisionerLonghorn, false),
+				newTestStorageClass(lvmSC, util.CSIProvisionerLVM, false),
+			},
+			expected: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Build objects for fake clientset
+			objs := make([]runtime.Object, 0, 1+len(tt.pvcs)+len(tt.storageClasses))
+			objs = append(objs, &harvesterv1.Setting{
+				ObjectMeta: metav1.ObjectMeta{Name: settings.BackupTargetSettingName},
+				Value:      backupTarget,
+			})
+			for _, pvc := range tt.pvcs {
+				objs = append(objs, pvc)
+			}
+			for _, sc := range tt.storageClasses {
+				objs = append(objs, sc)
+			}
+
+			clientset := fake.NewSimpleClientset(objs...)
+			vf := &vmformatter{
+				pvcCache:     fakeclients.PersistentVolumeClaimCache(clientset.CoreV1().PersistentVolumeClaims),
+				scCache:      fakeclients.StorageClassCache(clientset.StorageV1().StorageClasses),
+				settingCache: fakeclients.HarvesterSettingCache(clientset.HarvesterhciV1beta1().Settings),
+			}
+
+			vm := newTestVM(tt.pvcNames...)
+			vmi := newTestVMI()
+			result := vf.canDoBackup(vm, vmi)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+// Test helpers
+func newTestVM(pvcNames ...string) *kubevirtv1.VirtualMachine {
+	volumes := make([]kubevirtv1.Volume, 0, len(pvcNames))
+	for i, name := range pvcNames {
+		volumes = append(volumes, kubevirtv1.Volume{
+			Name: "disk-" + string(rune('0'+i)),
+			VolumeSource: kubevirtv1.VolumeSource{
+				PersistentVolumeClaim: &kubevirtv1.PersistentVolumeClaimVolumeSource{
+					PersistentVolumeClaimVolumeSource: corev1.PersistentVolumeClaimVolumeSource{
+						ClaimName: name,
+					},
+				},
+			},
+		})
+	}
+	return &kubevirtv1.VirtualMachine{
+		ObjectMeta: metav1.ObjectMeta{Name: "test-vm", Namespace: "default"},
+		Spec: kubevirtv1.VirtualMachineSpec{
+			Template: &kubevirtv1.VirtualMachineInstanceTemplateSpec{
+				Spec: kubevirtv1.VirtualMachineInstanceSpec{Volumes: volumes},
+			},
+		},
+	}
+}
+
+func newTestVMI() *kubevirtv1.VirtualMachineInstance {
+	return &kubevirtv1.VirtualMachineInstance{
+		ObjectMeta: metav1.ObjectMeta{Name: "test-vm", Namespace: "default"},
+		Status:     kubevirtv1.VirtualMachineInstanceStatus{Phase: kubevirtv1.Running},
+	}
+}
+
+func newTestPVC(name string, scName *string) *corev1.PersistentVolumeClaim {
+	return &corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: "default"},
+		Spec:       corev1.PersistentVolumeClaimSpec{StorageClassName: scName},
+	}
+}
+
+func newTestStorageClass(name, provisioner string, isDefault bool) *storagev1.StorageClass {
+	sc := &storagev1.StorageClass{
+		ObjectMeta:  metav1.ObjectMeta{Name: name},
+		Provisioner: provisioner,
+	}
+	if isDefault {
+		sc.Annotations = map[string]string{util.AnnotationIsDefaultStorageClassName: "true"}
+	}
+	return sc
+}
