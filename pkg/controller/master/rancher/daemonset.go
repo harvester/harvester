425b98058b04f3e5ae08909ba3509e6ceec94fc4
Gaurav Mehta <gaurav.mehta@suse.com>
2026-05-05 21:43:53 +1000

    tidy up reconcile of tlsstore during upgrade
    Signed-off-by: Gaurav Mehta <gaurav.mehta@suse.com>

--- pkg/controller/master/rancher/daemonset.go
+++ pkg/controller/master/rancher/daemonset.go
@@ -2,11 +2,20 @@ package rancher
 
 import (
 	"fmt"
+	"reflect"
 
+	"github.com/harvester/harvester/pkg/settings"
 	"github.com/harvester/harvester/pkg/util"
 	"github.com/sirupsen/logrus"
 	appsv1 "k8s.io/api/apps/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/discovery/cached/memory"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
 	"k8s.io/utils/ptr"
 )
 
@@ -39,8 +48,61 @@ func (h *Handler) reconcileIngressResources(_ string, ds *appsv1.DaemonSet) (*ap
 			if err := h.registerExposeService(); err != nil {
 				return ds, fmt.Errorf("error reconciling rancher expose service: %w", err)
 			}
-
 		}
+
+		// restMapping will be successful when the CRD exists
+		restMapping, err := findGVR(util.TraefikTLSStoreGVK, h.RestConfig)
+		if err != nil {
+			return ds, fmt.Errorf("error finding gvr for traefik tlsstore: %w", err)
+		}
+		_, err = h.DynamicClient.Resource(restMapping.Resource).Namespace(util.CattleSystemNamespaceName).Get(h.ctx, util.DefaultTraefikTLSStoreName, metav1.GetOptions{})
+		if err == nil {
+			return ds, nil
+		}
+
+		if apierrors.IsNotFound(err) {
+			logrus.Infof("default traefik tlsstore not found, triggering update of tlscertificate setting to re-apply default tlsstore with new cert")
+			if err := h.triggerTLSCertificateSettingUpdate(); err != nil {
+				return ds, fmt.Errorf("error triggering tlscertificate setting update: %w", err)
+			}
+		}
+		return ds, err
 	}
 	return ds, nil
+}
+
+func (h *Handler) triggerTLSCertificateSettingUpdate() error {
+	tlsCertificateSetting, err := h.SettingCache.Get(settings.SSLCertificatesSettingName)
+	if err != nil {
+		return fmt.Errorf("error looking up ssl certificate setting: %w", err)
+	}
+
+	tlsCertificateSettingCopy := tlsCertificateSetting.DeepCopy()
+	// update the setting value with the same value to trigger the change handler
+	delete(tlsCertificateSettingCopy.Annotations, util.AnnotationHash)
+	tlsCertificateSettingCopy.Annotations[util.AnnotationRancherControllerReconciled] = "true"
+	if !reflect.DeepEqual(tlsCertificateSettingCopy, tlsCertificateSetting) {
+		if _, err := h.SettingClient.Update(tlsCertificateSettingCopy); err != nil {
+			return fmt.Errorf("error removing annotation hash from ssl certificate setting to trigger tls store update: %w", err)
+		}
+	}
+	return nil
+}
+
+// lookup TLSStore using dynamic client
+// since CRD will be created in upgrade path after the dynamic client
+// has been initialised we need to use the rest mapper to lookup objects
+func findGVR(gvk schema.GroupVersionKind, cfg *rest.Config) (*meta.RESTMapping, error) {
+	// DiscoveryClient queries API server about the resources
+	dc, err := discovery.NewDiscoveryClientForConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+	mapper := restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(dc))
+
+	newMapping, err := mapper.RESTMapping(gvk.GroupKind(), gvk.Version)
+	if err != nil {
+		return nil, err
+	}
+	return newMapping, nil
 }
