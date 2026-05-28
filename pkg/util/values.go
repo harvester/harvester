9d28509f592c4568bbb72623648f478ec8279434
Kiefer Chang <kiefer.chang@suse.com>
2026-05-28 09:26:34 +0800

    feat: Port PutValue for generic data
    The function is useful for setting a nest map value.
    
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>
    (cherry picked from commit c0b69edf06e536206015940312ae8b3286dd2730)

--- pkg/util/values.go
+++ pkg/util/values.go
@@ -1,7 +1,7 @@
 package util
 
-// GetValue retrieve value from map[string]interface{} by keys
-// example: GetValue(map[string]interface{}{"a": map[string]interface{}{"b": "c"}}, "a", "b") -> "c"
+// GetValue retrieves a value from a nested map[string]interface{} by a sequence of keys.
+// example: GetValue(map[string]interface{}{"a": map[string]interface{}{"b": "c"}}, "a", "b") -> "c", true
 func GetValue(data map[string]interface{}, keys ...string) (interface{}, bool) {
 	for i, key := range keys {
 		if i == len(keys)-1 {
@@ -12,4 +12,33 @@ func GetValue(data map[string]interface{}, keys ...string) (interface{}, bool) {
 	}
 
 	return nil, false
+}
+
+// PutValue sets a value in a nested map[string]interface{} by a sequence of keys,
+// creating intermediate maps as needed. If an intermediate key exists but is not a map, the call is a no-op.
+// example: PutValue(data, "c", "a", "b") sets data["a"]["b"] = "c"
+func PutValue(data map[string]interface{}, val interface{}, keys ...string) {
+	if data == nil {
+		return
+	}
+
+	for i, key := range keys {
+		if i == len(keys)-1 {
+			data[key] = val
+		} else {
+			newData, ok := data[key]
+			if ok {
+				newMap, ok := newData.(map[string]interface{})
+				if ok {
+					data = newMap
+				} else {
+					return
+				}
+			} else {
+				newMap := map[string]interface{}{}
+				data[key] = newMap
+				data = newMap
+			}
+		}
+	}
 }
