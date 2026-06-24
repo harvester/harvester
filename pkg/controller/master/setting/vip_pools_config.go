b400c7e4b8c4cd0e4d494e02bc36b91cca46f726
Martin Dekov <martin.dekov@suse.com>
2026-06-24 11:15:31 +0300

    Revert vip-pools changes due to deprecation
    Signed-off-by: Martin Dekov <martin.dekov@suse.com>

--- pkg/controller/master/setting/vip_pools_config.go
+++ pkg/controller/master/setting/vip_pools_config.go
@@ -81,62 +81,14 @@ func ValidateCIDRs(pools map[string]string) error {
 				}
 			case 2:
 				for _, ipAddr := range ipRange {
-					if ip := net.ParseIP(ipAddr); ip == nil {
+					if ip := net.ParseIP(ipAddr); ip == nil || ip.To4() == nil {
 						return fmt.Errorf("invalid IP value %s of %s", pool, ns)
 					}
-				}
-				if err := validateIPRangeSameFamily(ipRange[0], ipRange[1], pool, ns); err != nil {
-					return err
 				}
 			default:
 				return fmt.Errorf("invalid IP Range value %s of %s", pool, ns)
 			}
 		}
-		// TODO(ipv6): remove once dual-stack ordering is no longer required
-		if err := validateIPv4BeforeIPv6(cidrOrIPRanges, pool, ns); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// validateIPRangeSameFamily rejects a start-end range where one endpoint is
-// IPv4 and the other is IPv6.
-func validateIPRangeSameFamily(start, end, pool, ns string) error {
-	a, b := net.ParseIP(start), net.ParseIP(end)
-	if (a.To4() == nil) != (b.To4() == nil) {
-		return fmt.Errorf("invalid IP Range value %s of %s: cannot mix IPv4 and IPv6 addresses", pool, ns)
-	}
-	return nil
-}
-
-// validateIPv4BeforeIPv6 enforces that all IPv4 entries precede all IPv6
-// entries within a comma-separated pool value.
-//
-// TODO(ipv6): remove this function once the installer and UI can express
-// dual-stack pools without an ordering constraint.
-func validateIPv4BeforeIPv6(cidrOrIPRanges []string, pool, ns string) error {
-	seenIPv6 := false
-	for _, entry := range cidrOrIPRanges {
-		ipRange := strings.Split(entry, "-")
-		var representative net.IP
-		if len(ipRange) == 2 {
-			representative = net.ParseIP(ipRange[0])
-		} else {
-			var err error
-			representative, _, err = net.ParseCIDR(entry)
-			if err != nil {
-				return fmt.Errorf("invalid CIDR value %s of %s, error: %s", entry, ns, err.Error())
-			}
-		}
-		if representative == nil {
-			continue
-		}
-		isIPv6 := representative.To4() == nil
-		if seenIPv6 && !isIPv6 {
-			return fmt.Errorf("invalid Pool value %s of %s: IPv4 entries must precede IPv6 entries", pool, ns)
-		}
-		seenIPv6 = seenIPv6 || isIPv6
 	}
 	return nil
 }
